9a63609ff49f2657d60ca5145e7d33e3c4ab11e4
Glenn Proctor <gp1@sanger.ac.uk>
2003-03-19 16:51:41 +0000

    Removed merge conflict.

--- src/org/ensembl/healthcheck/TestRunner.java
+++ src/org/ensembl/healthcheck/TestRunner.java
@@ -32,6 +32,7 @@ import java.util.*;
 import java.util.logging.*;
 import java.sql.*;
 import java.io.*;
+import java.util.regex.*;
 
 import junit.framework.*;
 
@@ -39,12 +40,18 @@ import org.ensembl.healthcheck.util.*;
 
 public class TestRunner {
   
+<<<<<<< TestRunner.java
+  private static String version = "$Id$";
+=======
   private static String version = "$Id$";
   private ArrayList allTests;            // will hold an instance of each test
+>>>>>>> 1.4
   private ArrayList groupsToRun;
   private Properties dbProps;
+  private String commandLineRegexp = null;
+  private boolean forceDatabases = false;
   
-  private static Logger logger = Logger.getLogger("org.ensembl.healthcheck.TestRunner");
+  private static Logger logger = Logger.getLogger("HealthCheckLogger");
   
   // -------------------------------------------------------------------------
   /** Creates a new instance of TestRunner */
@@ -71,9 +78,7 @@ public class TestRunner {
     
     //tr.showDatabaseList();
     
-    tr.findAllTests();
-    
-    tr.runAllTests();
+    tr.runAllTests(tr.findAllTests());
     
   } // main
   
@@ -106,10 +111,33 @@ public class TestRunner {
     } else {
       
       for (int i=0; i < args.length; i++) {
-	groupsToRun.add(args[i]);
-	System.out.println("Will run tests in group " + args[i]);
+	
+	if (args[i].equals("-h")) {
+	  
+	  printUsage();
+	  System.exit(0);
+	  
+	} else if (args[i].equals("-d")) {
+	  
+	  i++;
+	  commandLineRegexp = args[i];
+	  System.out.println("Will pre-filter database names on " + commandLineRegexp);
+	  
+	} else if (args[i].equals("-force")) {
+	  
+	  forceDatabases = true;
+	  System.out.println("Will use ONLY databases specified by -d");
+	  
+	} else {
+	  groupsToRun.add(args[i]);
+	  System.out.println("Will run tests in group " + args[i]);
+	}
       }
       
+      if (forceDatabases && commandLineRegexp == null) {
+	System.err.println("You have requested -force but not specified a database name regular expression with -d");
+	System.exit(1);
+      }
     }
     
   } // parseCommandLine
@@ -131,7 +159,15 @@ public class TestRunner {
   
   private void printUsage() {
     
-    System.out.println("\nUsage: TestRunner {group1} {group2} ...\n");
+    System.out.println("\nUsage: TestRunner {-d regexp} {-force} {group1} {group2} ...\n");
+    System.out.println("Options:");
+    System.out.println("  -d regexp  Use the given regular expression to decide which databases to use.");
+    System.out.println("  -force     Run the named tests on the databases matched by -d, without ");
+    System.out.println("             taking into account the regular expressions built into the tests themselves.");
+    System.out.println("  group1     Names of groups of test cases to run.");
+    System.out.println("             Note each test case is in a group of its own with the name of the test case.");
+    System.out.println("             This allows individual tests to be run if required.");
+    System.out.println("");
     
   } // printUsage
   
@@ -146,7 +182,7 @@ public class TestRunner {
   
   // -------------------------------------------------------------------------
   
-  public String[] getListOfDatabaseNames(String regexp) {
+  public String[] getListOfDatabaseNames(String regexp, String preFilterRegexp) {
     
     Connection conn;
     
@@ -162,7 +198,7 @@ public class TestRunner {
       
       logger.fine("Opened connection to " + dbProps.getProperty("databaseURL", "kaka.sanger.ac.uk") + " as " + dbProps.getProperty("user", "anonymous"));
       
-      databaseNames = DBUtils.listDatabases(conn, regexp);
+      databaseNames = DBUtils.listDatabases(conn, regexp, preFilterRegexp);
       
       if (databaseNames.length == 0) {
 	logger.warning("No database names matched");
@@ -185,11 +221,11 @@ public class TestRunner {
   
   // -------------------------------------------------------------------------
   
-  private void showDatabaseList(String regexp) {
+  private void showDatabaseList(String regexp, String preFilterRegexp) {
     
     logger.fine("Listing databases matching " + regexp + " :\n");
     
-    String[] databaseList = getListOfDatabaseNames(regexp);
+    String[] databaseList = getListOfDatabaseNames(regexp, preFilterRegexp);
     
     for (int i = 0; i < databaseList.length; i++) {
       logger.fine("\t" + databaseList[i]);
@@ -199,20 +235,98 @@ public class TestRunner {
   
   // -------------------------------------------------------------------------
   
-  private void findAllTests() {
+  private ArrayList findAllTests() {
     
-    allTests = new ArrayList();
+    ArrayList allTests = new ArrayList();
     
-    // find all classes that extend org.ensembl.healthcheck.EnsTestCase
-    
+    // --------------------------------
+    // look for tests in the same directory as this class
     String thisClassName = this.getClass().getName();
     String packageName = thisClassName.substring(0, thisClassName.lastIndexOf("."));
     String directoryName = packageName.replace('.', File.separatorChar);
-    
     logger.finest("Package name: " + packageName + " Directory name: " + directoryName);
-    File f = new File(System.getProperty("user.dir") + File.separator + "src" + File.separator + directoryName);
+    String fullDirectory = System.getProperty("user.dir") + File.separator + "src" + File.separator + directoryName;
+    allTests.addAll(findTestsInDirectory(fullDirectory, packageName));
     
-    System.out.println("f.getName(): " + f.getPath());
+    // --------------------------------
+    
+    
+    
+    // --------------------------------
+    logger.finer("Found " + allTests.size() + " test case classes.");
+    
+    /*Iterator it = allTests.iterator();
+    while (it.hasNext()) {
+      EnsTestCase tc = (EnsTestCase)it.next();
+      System.out.println("#####" + tc.getTestName());
+    }
+     */
+    
+    return allTests;
+    
+  } // findAllTests
+  
+  // -------------------------------------------------------------------------
+  
+  private void runAllTests(ArrayList allTests) {
+    
+    Pattern pattern;
+    Matcher matcher;
+    
+    // check if allTests() has been populated; if not, run findAllTests()
+    if (allTests == null) {
+      allTests = findAllTests();
+    }
+    
+    if (allTests.size() == 0) {
+      logger.warning("Warning: no tests found!");
+      return;
+    }
+    
+    Iterator it = allTests.iterator();
+    while (it.hasNext()) {
+      
+      EnsTestCase testCase = (EnsTestCase)it.next();
+      
+      if (testCase.inGroups(groupsToRun)) {
+	logger.info("\tRunning test of type " + testCase.getClass().getName());
+	if (commandLineRegexp != null) {
+	  testCase.setPreFilterRegexp(commandLineRegexp);
+	}
+	
+	if (forceDatabases) {
+	  // override built-in database regexp with the one specified on the command line
+	  testCase.setDatabaseRegexp(commandLineRegexp);
+	}
+	TestResult tr = testCase.run();
+	System.out.println("\n" + tr.getName() + " " + tr.getResult() + " " + tr.getMessage() + "\n");
+	// TBC
+      }
+    }
+    
+  } // runAllTests
+  
+  // -------------------------------------------------------------------------
+  
+  public DatabaseConnectionIterator getDatabaseConnectionIterator(String[] databaseNames) {
+    
+    return new DatabaseConnectionIterator(dbProps.getProperty("driver"),
+    dbProps.getProperty("databaseURL"),
+    dbProps.getProperty("user"),
+    dbProps.getProperty("password"),
+    databaseNames);
+    
+  } // getDatabaseConnectionIterator
+  
+  // -------------------------------------------------------------------------
+  
+  public ArrayList findTestsInDirectory(String dir, String packageName) {
+    
+    ArrayList tests = new ArrayList();
+    
+    File f = new File(dir);
+    
+    // find all classes that extend org.ensembl.healthcheck.EnsTestCase
     ClassFileFilenameFilter cnff = new ClassFileFilenameFilter();
     File[] classFiles = f.listFiles(cnff);
     logger.finer("Examining " + classFiles.length + " class files ...");
@@ -239,68 +353,21 @@ public class TestRunner {
 	e.printStackTrace();
       }
       
-      if (obj instanceof org.ensembl.healthcheck.EnsTestCase && !allTests.contains(obj)) {
+      if (obj instanceof org.ensembl.healthcheck.EnsTestCase && !tests.contains(obj)) {
 	((EnsTestCase)obj).init(this);
-	allTests.add(obj); // note we store an INSTANCE of the test, not just its name
+	tests.add(obj); // note we store an INSTANCE of the test, not just its name
 	logger.info("Added test case " + obj.getClass().getName());
       }
       
     } // for classFiles
     
-    logger.finer("Found " + allTests.size() + " test case classes.");
+    return tests;
     
-    /*Iterator it = allTests.iterator();
-    while (it.hasNext()) {
-      EnsTestCase tc = (EnsTestCase)it.next();
-      System.out.println("#####" + tc.getTestName());
-    }
-     */
-    
-  } // findAllTests
+  } // findAllTestsByPath
   
   // -------------------------------------------------------------------------
   
-  private void runAllTests() {
-    
-    // check if allTests() has been populated; if not, run findAllTests()
-    if (allTests == null) {
-      findAllTests();
-    }
-    
-    if (allTests.size() == 0) {
-      logger.warning("Warning: no tests found!");
-      return;
-    }
-    
-    Iterator it = allTests.iterator();
-    while (it.hasNext()) {
-      
-      EnsTestCase testCase = (EnsTestCase)it.next();
-      
-      if (testCase.inGroups(groupsToRun)) {
-	logger.info("\tRunning test of type " + testCase.getClass().getName());
-	TestResult tr = testCase.run();
-	System.out.println("\n" + tr.getName() + " " + tr.getResult() + " " + tr.getMessage() + "\n");
-	// TBC
-      }
-      
-    }
-    
-  } // runAllTests
   
-  // -------------------------------------------------------------------------
-  
-  public DatabaseConnectionIterator getDatabaseConnectionIterator(String[] databaseNames) {
-    
-    return new DatabaseConnectionIterator(dbProps.getProperty("driver"),
-    dbProps.getProperty("databaseURL"),
-    dbProps.getProperty("user"),
-    dbProps.getProperty("password"),
-    databaseNames);
-    
-  } // getDatabaseConnectionIterator
-  
-  // -------------------------------------------------------------------------
   
 } // TestRunner
 
