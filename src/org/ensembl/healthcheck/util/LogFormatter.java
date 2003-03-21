b684259f9307da6bb514a273e602d37e88eacd21
Glenn Proctor <gp1@sanger.ac.uk>
2003-03-21 11:41:54 +0000

    *** empty log message ***

--- src/org/ensembl/healthcheck/util/LogFormatter.java
+++ src/org/ensembl/healthcheck/util/LogFormatter.java
@@ -1,18 +1,34 @@
-package org.ensembl.healthcheck.util;
+/*
+  Copyright (C) 2003 EBI, GRL
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
+ * <p>Title: LogFormatter</p>
+ * <p>Description: This custom formatter formats parts of a log record to a single line.</p>
  * <p>Copyright: Copyright (c) 2003</p>
  * <p>Organisation: EMBL</p>
  * @author Glenn Proctor <glenn@ebi.ac.uk>
  * @version $Revision$
  */
 
+package org.ensembl.healthcheck.util;
+
 import java.util.logging.*;
 import java.util.*;
-
-// This custom formatter formats parts of a log record to a single line
 
 public class LogFormatter extends Formatter {
 
@@ -24,6 +40,5 @@ public class LogFormatter extends Formatter {
     buf.append('\n');
     return buf.toString();
   }
-
 
 } // LogFormatter
--- src/org/ensembl/healthcheck/util/MyStreamHandler.java
+++ src/org/ensembl/healthcheck/util/MyStreamHandler.java
@@ -18,7 +18,7 @@
 
 /**
  * <p>Title: MyStreamHandler.java</p>
- * <p>Description: </p>
+ * <p>Description: Custom stream handler for logging.</p>
  * <p>Copyright: Copyright (c) 2003</p>
  * <p>Organisation: EMBL</p>
  * <p>Created on March 12, 2003, 11:30 AM</p>
@@ -36,7 +36,9 @@ public class MyStreamHandler extends Handler {
   
   OutputStream outputStream;
   
-  /** Creates a new instance of MyStreamHandler */
+  /** 
+   * Creates a new instance of MyStreamHandler 
+   */
   public MyStreamHandler(OutputStream os, Formatter formatter) {
     this.outputStream = os;
     setFormatter(formatter);
