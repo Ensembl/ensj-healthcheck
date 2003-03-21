26b8bd23c0365eff5c974b0124fe64bbd21d585b
Glenn Proctor <gp1@sanger.ac.uk>
2003-03-21 11:50:41 +0000

    Created.

---
+++ src/org/ensembl/healthcheck/BlankNonNullExternalDBTestCase.java
@@ -0,0 +1,63 @@
+/*
+  Copyright (C) 2003 EBI, GRL
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/**
+ * <p>Title: BlankEnumDBNameExternalDBTestCase.java</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Organisation: EMBL</p>
+ * <p>Created on March 21, 2003, 8:58 AM</p>
+ * @author Glenn Proctor <glenn@ebi.ac.uk>
+ * @version $Revision$
+ */
+
+package org.ensembl.healthcheck;
+
+import java.sql.*;
+import java.util.*;
+import org.ensembl.healthcheck.util.*;
+
+public class BlankNonNullExternalDBTestCase extends EnsTestCase {
+  
+  /** Creates a new instance of BlankEnumDBNameExternalDBTestCase */
+  public BlankNonNullExternalDBTestCase() {
+    databaseRegexp = ".*_core_\\d.*";
+  }
+  
+  /**
+   * Check for any columns in the external_db table that are marked as NON NULL but contain null or blank values.
+   */
+  TestResult run() {
+    DatabaseConnectionIterator it = testRunner.getDatabaseConnectionIterator(getAffectedDatabases(databaseRegexp));
+    
+    boolean result = true;
+    
+    while (it.hasNext()) {
+      
+      Connection con = (Connection)it.next();
+      int blanks = checkBlankNonNull(con, "external_db");
+      if (blanks > 0) {
+	result = false;
+      }
+    }
+    
+    return new TestResult(getShortTestName(), result, "");
+    
+  }
+  
+} // BlankNonNullExternalDBTestCase
