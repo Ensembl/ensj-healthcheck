b98e2227b5dc81af9f0459fbff68526a6c03dc2c
Glenn Proctor <gp1@sanger.ac.uk>
2003-03-24 16:30:11 +0000

    Created.

---
+++ src/org/ensembl/healthcheck/TextTestRunner.java
@@ -0,0 +1,159 @@
+/*
+  Copyright (C) 2003 EBI, GRL
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.ensembl.healthcheck;
+
+/**
+ * <p>Title: TextTestRunner.java</p>
+ * <p>Description: Subclass of TestRunner intended for running tests from the command line.</p>
+ * <p>Copyright: Copyright (c) 2003</p>
+ * <p>Organisation: EMBL</p>
+ * <p>Created on March 21, 2003, 3:15 PM</p>
+ * @author Glenn Proctor <glenn@ebi.ac.uk>
+ * @version $Revision$
+ */
+
+
+import java.util.*;
+import java.util.logging.*;
+import java.sql.*;
+import java.io.*;
+import java.util.regex.*;
+
+import junit.framework.*;
+
+import org.ensembl.healthcheck.util.*;
+
+public class TextTestRunner extends TestRunner {
+  
+  private String commandLineRegexp = null;
+  private boolean forceDatabases = false;
+  
+  private static Logger logger = Logger.getLogger("HealthCheckLogger");
+  
+  private static String version = "$Id$";
+  
+  // -------------------------------------------------------------------------
+  /**
+   * Command-line run method.
+   * @param args The command-line arguments.
+   */
+  public static void main(String[] args) {
+    
+    TextTestRunner ttr = new TextTestRunner();
+    
+    System.out.println(ttr.getVersion());
+    
+    ttr.parseCommandLine(args);
+    
+    ttr.setupLogging();
+    
+    ttr.readPropertiesFile();
+    
+    ttr.runAllTests(ttr.findAllTests(), ttr.commandLineRegexp, ttr.forceDatabases);
+    
+  } // main
+  
+  // -------------------------------------------------------------------------
+  
+  private void printUsage() {
+    
+    System.out.println("\nUsage: TextTestRunner {-d regexp} {-force} {group1} {group2} ...\n");
+    System.out.println("Options:");
+    System.out.println("  -d regexp  Use the given regular expression to decide which databases to use.");
+    System.out.println("  -force     Run the named tests on the databases matched by -d, without ");
+    System.out.println("             taking into account the regular expressions built into the tests themselves.");
+    System.out.println("  group1     Names of groups of test cases to run.");
+    System.out.println("             Note each test case is in a group of its own with the name of the test case.");
+    System.out.println("             This allows individual tests to be run if required.");
+    System.out.println("");
+    
+  } // printUsage
+  
+  // -------------------------------------------------------------------------
+  /**
+   * Get the version information (inserted by CVS).
+   * @return The version info as inserted by CVS.
+   */
+  public String getVersion() {
+    
+    // strip off first and last few chars of version since these are only used by CVS
+    return version.substring(5, version.length()-2);
+    
+  } // getVersion
+  
+  // -------------------------------------------------------------------------
+  
+  
+  private void parseCommandLine(String[] args) {
+    
+    if (args.length == 0) {
+      
+      printUsage();
+      System.exit(1);
+      
+    } else {
+      
+      for (int i=0; i < args.length; i++) {
+	
+	if (args[i].equals("-h")) {
+	  
+	  printUsage();
+	  System.exit(0);
+	  
+	} else if (args[i].equals("-d")) {
+	  
+	  i++;
+	  commandLineRegexp = args[i];
+	  System.out.println("Will pre-filter database names on " + commandLineRegexp);
+	  
+	} else if (args[i].equals("-force")) {
+	  
+	  forceDatabases = true;
+	  System.out.println("Will use ONLY databases specified by -d");
+	  
+	} else {
+	  groupsToRun.add(args[i]);
+	  System.out.println("Will run tests in group " + args[i]);
+	}
+      }
+      
+      if (forceDatabases && commandLineRegexp == null) {
+	System.err.println("You have requested -force but not specified a database name regular expression with -d");
+	System.exit(1);
+      }
+    }
+    
+  } // parseCommandLine
+  
+  // -------------------------------------------------------------------------
+  
+  private void setupLogging() {
+    
+    logger.setUseParentHandlers(false); // stop parent logger getting the message
+    Handler myHandler = new MyStreamHandler(System.out, new LogFormatter());
+    logger.addHandler(myHandler);
+    logger.setLevel(Level.FINEST);
+    logger.info("Set logging level to " + logger.getLevel().getName());
+    
+  } // setupLogging
+    
+  // -------------------------------------------------------------------------
+  
+} // TextTestRunner
+
