0d1601a3eed34d3e4bb5b95b34bec4776d148c9d
Glenn Proctor <gp1@sanger.ac.uk>
2003-04-03 10:04:57 +0000

    Updated to work on acari

--- compile-healthcheck.sh
+++ compile-healthcheck.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-javac -classpath "lib/junit.jar:src/" src/org/ensembl/healthcheck/*.java
+javac -classpath "build/" -d build src/org/ensembl/healthcheck/util/*.java src/org/ensembl/healthcheck/testcase/*.java src/org/ensembl/healthcheck/*.java
+
--- run-healthcheck-gui.sh
+++ run-healthcheck-gui.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-java -classpath "lib/junit.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner "$*"
+java -classpath "lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner $*
