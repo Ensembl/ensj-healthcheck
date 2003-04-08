3f54aa6fc9b59545feb0ab6a523220b6ccd18330
Glenn Proctor <gp1@sanger.ac.uk>
2003-04-08 15:52:45 +0000

    Removed main(); added various text-updating methods

--- src/org/ensembl/healthcheck/TestInfoWindow.java
+++ src/org/ensembl/healthcheck/TestInfoWindow.java
@@ -1,18 +1,35 @@
 /*
- * TestInfoWindow.java
- *
- * Created on April 2, 2003, 10:59 AM
+  Copyright (C) 2003 EBI, GRL
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package org.ensembl.healthcheck;
 
+import java.util.*;
+
 /**
- *
- * @author  glenn
+ * Dialog that displays information about a test that is running (or has just run).
  */
 public class TestInfoWindow extends javax.swing.JDialog {
   
-  /** Creates new form TestInfoWindow */
+  /** Creates new form TestInfoWindow
+   * @param parent The JFrame opening this dialog.
+   * @param title The title of the dialog.
+   * @param modal Whether or not this window is modal.
+   */
   public TestInfoWindow(java.awt.Frame parent, String title, boolean modal) {
     super(parent, modal);
     this.setTitle(title);
@@ -53,13 +70,6 @@ public class TestInfoWindow extends javax.swing.JDialog {
     dispose();
   }//GEN-LAST:event_closeDialog
   
-  /**
-   * @param args the command line arguments
-   */
-  public static void main(String args[]) {
-    new TestInfoWindow(new javax.swing.JFrame(), "TestInfoWindow", true).show();
-  }
-  
   
   // Variables declaration - do not modify//GEN-BEGIN:variables
   private javax.swing.JPanel basePanel;
@@ -67,12 +77,49 @@ public class TestInfoWindow extends javax.swing.JDialog {
   private javax.swing.JTextArea infoTextArea;
   // End of variables declaration//GEN-END:variables
  
+  // -------------------------------------------------------------------------
+  /**
+   * Append text to the information TextArea.
+   * @param s The text to append.
+   */
   public void append(String s) {
     
     infoTextArea.append(s);
     
   } // append
   
+  // -------------------------------------------------------------------------
+  /** 
+   * Set the text of the information TextArea to a particular String.
+   * @param s The text to set.
+   */
+  public void setText(String s) {
+  
+    infoTextArea.setText(s);
+      
+  } // setText
+  
+  // -------------------------------------------------------------------------
+  /**
+   * Set the information TextArea to the contents of a list of Strings.
+   * @param list The List of Strings to display.
+   */
+  public void setText(List list) {
+  
+    Iterator it = list.iterator();
+    while(it.hasNext()) {
+      
+     ReportLine line = (ReportLine)it.next();
+     infoTextArea.append(line.getMessage()); // @todo e.g. colour by level?
+     
+    }
+    
+  } // setText
+  
+  // -------------------------------------------------------------------------
+ /**
+  * Clear the information TextArea
+  */
   public void clear() {
     
    infoTextArea.setText("");
