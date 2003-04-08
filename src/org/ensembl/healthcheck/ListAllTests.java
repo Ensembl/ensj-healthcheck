21d809a59bd465728e09ff4f993dc75e8b3d54b4
Glenn Proctor <gp1@sanger.ac.uk>
2003-04-08 15:48:49 +0000

    Removed unused constructor.

--- src/org/ensembl/healthcheck/ListAllTests.java
+++ src/org/ensembl/healthcheck/ListAllTests.java
@@ -8,7 +8,7 @@
  
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNUsql
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
  
   You should have received a copy of the GNU Lesser General Public
@@ -29,12 +29,6 @@ import org.ensembl.healthcheck.testcase.*;
 public class ListAllTests extends TestRunner {
   
   String groupToList = "";
-  
-  /**
-   * Creates a new instance of ListAllTests
-   */
-  public ListAllTests() {
-  }
   
   // -------------------------------------------------------------------------
   /**
