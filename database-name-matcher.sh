49e0fedc393618e7d9d48d697f8a06dc84f6d3c1
Glenn Proctor <gp1@sanger.ac.uk>
2003-04-09 15:03:22 +0000

    Added ensj.jar to classpath

--- database-name-matcher.sh
+++ database-name-matcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath "lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.DatabaseNameMatcher "$*"
+java -classpath "lib/ensj.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.DatabaseNameMatcher "$*"
--- run-healthcheck-gui.sh
+++ run-healthcheck-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner $*
+java -classpath "lib/ensj.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner $*
