7efd5317728126b8da74fb8159ec9203582657c1
Glenn Proctor <gp1@sanger.ac.uk>
2003-04-11 13:06:27 +0000

    Added ensj jars

--- run-healthcheck-gui.sh
+++ run-healthcheck-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/ensj.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner $*
+java -classpath "lib/ensj.jar:lib:/ecp1_0beta.jar:lib/log4j-1.2.6.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner $*
--- run-healthcheck.sh
+++ run-healthcheck.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.TextTestRunner $*
+java -classpath "lib/ensj.jar:lib:/ecp1_0beta.jar:lib/log4j-1.2.6.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.TextTestRunner $*
