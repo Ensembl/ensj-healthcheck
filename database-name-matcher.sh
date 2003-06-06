a59c9bf8d57082d3121c8834e60c32fa1f085dcf
Glenn Proctor <gp1@sanger.ac.uk>
2003-06-06 10:04:35 +0000

    Updated for new version of mySQL JDBC driver

--- database-name-matcher.sh
+++ database-name-matcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/ensj.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.DatabaseNameMatcher "$*"
+java -classpath "lib/ensj.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:build/" org.ensembl.healthcheck.DatabaseNameMatcher "$*"
--- list-tests-in-group.sh
+++ list-tests-in-group.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/ensj.jar:lib/junit.jar:lib/mysql-connector-java-2.0.14-bin.jar:build/" org.ensembl.healthcheck.ListAllTests $*
+java -classpath "lib/ensj.jar:lib/junit.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:build/" org.ensembl.healthcheck.ListAllTests $*
