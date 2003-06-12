ef63ec844388bb975bb524de24fa7dee20be2243
Glenn Proctor <gp1@sanger.ac.uk>
2003-06-12 08:24:55 +0000

    Added -noschemainfo command line option.

--- src/org/ensembl/healthcheck/TextTestRunner.java
+++ src/org/ensembl/healthcheck/TextTestRunner.java
@@ -1,16 +1,16 @@
 /*
   Copyright (C) 2003 EBI, GRL
-
+ 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
+ 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
+ 
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -31,45 +31,48 @@ import java.util.regex.*;
  * Subclass of TestRunner intended for running tests from the command line.
  */
 public class TextTestRunner extends TestRunner {
-
+  
   private static String version = "$Id$";
   private boolean forceDatabases = false;
   private boolean debug = false;
-
+  private boolean buildSchemaInfo = true;
+  
   // -------------------------------------------------------------------------
-
+  
   /**
    * Command-line run method.
    * @param args The command-line arguments.
    */
   public static void main(String[] args) {
-
+    
     TextTestRunner ttr = new TextTestRunner();
-
+    
     System.out.println(ttr.getVersion());
-
+    
     ttr.parseCommandLine(args);
-
+    
     ttr.setupLogging();
-
+    
     ttr.readPropertiesFile();
-
-    ttr.buildSchemaList();
+    
+    if (ttr.buildSchemaInfo) {
+      ttr.buildSchemaList();
+    }
     
     ttr.runAllTests(ttr.findAllTests(), ttr.forceDatabases);
-
+    
     ConnectionPool.closeAll();
-
+    
     ttr.printReportsByTest(ttr.outputLevel);
-
+    
     ttr.printReportsByDatabase(ttr.outputLevel);
-
+    
   }
-
+  
   // main
   // -------------------------------------------------------------------------
   private void printUsage() {
-
+    
     System.out.println("\nUsage: TextTestRunner {-d regexp} {-force} {-output none|problem|correct|summary|info|all} {group1} {group2} ...\n");
     System.out.println("Options:");
     System.out.println("  -d regexp       Use the given regular expression to decide which databases to use.");
@@ -87,11 +90,12 @@ public class TextTestRunner extends TestRunner {
     System.out.println("  -debug          Print debugging info (for developers only)");
     System.out.println("  -repair         If appropriate, carry out repair methods on test cases that support it");
     System.out.println("  -showrepair     Like -repair, but the repair is NOT carried out, just reported.");
+    System.out.println("  -noschemainfo   Do not cache schema info at startup. Quicker, but may cause some tests not to work. Use with caution.");
     System.out.println("  group1          Names of groups of test cases to run.");
     System.out.println("                  Note each test case is in a group of its own with the name of the test case.");
     System.out.println("                  This allows individual tests to be run if required.");
     System.out.println("");
-
+    
   }
   
   /**
@@ -99,36 +103,36 @@ public class TextTestRunner extends TestRunner {
    * @return The version.
    */
   public String getVersion() {
-
+    
     // strip off first and last few chars of version since these are only used by CVS
     return version.substring(5, version.length() - 2);
-
+    
   }
-
+  
   // -------------------------------------------------------------------------
   private void parseCommandLine(String[] args) {
-
+    
     if (args.length == 0) {
-
+      
       printUsage();
       System.exit(1);
-
+      
     } else {
-
+      
       for (int i = 0; i < args.length; i++) {
-
+        
         if (args[i].equals("-h")) {
-
+          
           printUsage();
           System.exit(0);
-
+          
         } else if (args[i].equals("-output")) {
-
+          
           setOutputLevel(args[++i]);
           System.out.println("Set output level to " + outputLevel);
-
+          
         } else if (args[i].equals("-debug")) {
-
+          
           debug = true;
           
         } else if (args[i].equals("-repair")) {
@@ -136,66 +140,71 @@ public class TextTestRunner extends TestRunner {
           doRepair = true;
           
         } else if (args[i].equals("-showrepair")) {
-
+          
           showRepair = true;
           
         } else if (args[i].equals("-d")) {
-
+          
           i++;
           preFilterRegexp = args[i];
           System.out.println("Will pre-filter database names on " + preFilterRegexp);
-
+          
         } else if (args[i].equals("-force")) {
-
+          
           forceDatabases = true;
           System.out.println("Will use ONLY databases specified by -d");
-
+          
+        } else if (args[i].equals("-noschemainfo")) {
+          
+          buildSchemaInfo = false;
+          System.out.println("Will NOT cache schema info at startup");
+          
         } else if (args[i].equals("-config")) {
-
+          
           i++;
           propertiesFileName = args[i];
           System.out.println("Will read properties from " + propertiesFileName);
-
+          
         } else {
-
+          
           groupsToRun.add(args[i]);
           System.out.println("Will run tests in group " + args[i]);
-
+          
         }
-
+        
       }
-
+      
       if (forceDatabases && (preFilterRegexp == null)) {
-
+        
         System.err.println("You have requested -force but not specified a database name regular expression with -d");
         System.exit(1);
-
+        
       }
-
+      
     }
-
+    
   }
-
+  
   // parseCommandLine
   // -------------------------------------------------------------------------
   private void setupLogging() {
-
+    
     logger.setUseParentHandlers(false);// stop parent logger getting the message
-
+    
     Handler myHandler = new MyStreamHandler(System.out, new LogFormatter());
-
+    
     logger.addHandler(myHandler);
     logger.setLevel(Level.WARNING);// default - only print important messages
-
+    
     if (debug) {
-
+      
       logger.setLevel(Level.FINEST);
-
+      
     }
-
+    
     //logger.info("Set logging level to " + logger.getLevel().getName());
   }
-
+  
   // setupLogging
   // -------------------------------------------------------------------------
 }
