ff3b0d3f3ddf0ab14d05caecc230cf7646183ee4
Glenn Proctor <gp1@sanger.ac.uk>
2003-06-25 15:15:46 +0000

    Created

---
+++ src/org/ensembl/healthcheck/testcase/EmptyTablesTestCase.java
@@ -0,0 +1,81 @@
+/*
+  Copyright (C) 2003 EBI, GRL
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.ensembl.healthcheck.testcase;
+
+import java.sql.*;
+import java.util.*;
+
+import org.ensembl.healthcheck.*;
+import org.ensembl.healthcheck.util.*;
+
+/**
+ * Check that all tables have data.
+ */
+public class EmptyTablesTestCase extends EnsTestCase {
+    
+    /**
+     * Creates a new instance of EmptyTablesTestCase
+     */
+    public EmptyTablesTestCase() {
+        
+        addToGroup("db_constraints");
+        setDescription("Checks that all tables have data");
+        setHintLongRunning(true);
+        
+    }
+    
+    /**
+     * For each schema, check that every table has more than 0 rows.
+     */
+    public TestResult run() {
+        
+        boolean result = true;
+        
+        // DatabaseConnectionIterator it = getMatchingSchemaIterator();
+        DatabaseConnectionIterator it = getDatabaseConnectionIterator();
+        
+        while (it.hasNext()) {
+            
+            Connection con = (Connection)it.next();
+            
+            SchemaInfo si = SchemaManager.getSchema(con);
+            
+            List tables = si.getTables();
+            
+            Iterator tableIterator = tables.iterator();
+            while(tableIterator.hasNext()) {
+                
+                String table = (String)tableIterator.next();
+                logger.finest("Checking that " + table + " has rows");
+                
+                if (!tableHasRows(con, table)) {
+                 
+                    ReportManager.problem(this, con, table + " has zero rows");
+                    
+                }
+            }
+            
+            
+        } // while connection
+        
+        return new TestResult(getShortTestName(), result);
+        
+    } // run
+    
+} // EmptyTablesTestCase
