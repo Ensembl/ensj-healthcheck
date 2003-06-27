d4a070d1d3a044037c6567aa4ad2d49fa2ca6644
Glenn Proctor <gp1@sanger.ac.uk>
2003-06-27 12:19:30 +0000

    Created.

---
+++ src/org/ensembl/healthcheck/util/TestComparator.java
@@ -0,0 +1,44 @@
+/*
+  Copyright (C) 2003 EBI, GRL
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.ensembl.healthcheck.util;
+
+import java.util.*;
+
+import org.ensembl.healthcheck.testcase.*;
+
+/**
+ * Implementation of Comparator for comparing EnsTestCase objects.
+ */
+
+public class TestComparator implements Comparator {
+  
+  /** 
+   * Implementation of Compare interface. 
+   * Compares on test name.
+   */
+  public int compare(Object o1, Object o2) {
+    
+    String n1 = ((EnsTestCase)o1).getShortTestName();
+    String n2 = ((EnsTestCase)o2).getShortTestName();
+ 
+    return n1.compareTo(n2);
+    
+  }
+  
+}
