419338a3bcf77180dc072f3aa37792faf313b0be
Glenn Proctor <gp1@sanger.ac.uk>
2003-09-26 09:27:21 +0000

    Fixed setting of result flag to correct PASSED/FAILED report.

--- src/org/ensembl/healthcheck/testcase/AccessionTestCase.java
+++ src/org/ensembl/healthcheck/testcase/AccessionTestCase.java
@@ -74,6 +74,7 @@ public class AccessionTestCase extends EnsTestCase {
         // check that there is at least one hit
         int hits = getRowCount(con, "SELECT COUNT(*) FROM protein_feature pf, analysis a WHERE a.logic_name='" + logicNames.get(key) + "' AND a.analysis_id=pf.analysis_id");
         if (hits < 1) {
+          result = false;
           ReportManager.problem(this, con, "No proteins with " + logicNames.get(key) + " hits; this is only a problem for *_core_* databases");
         } else {
           ReportManager.correct(this, con, hits  + " proteins with " + logicNames.get(key) + "  hits");
@@ -82,6 +83,7 @@ public class AccessionTestCase extends EnsTestCase {
         // check format of hits
         int badFormat = getRowCount(con, "SELECT COUNT(*) FROM protein_feature pf, analysis a WHERE a.logic_name='" + logicNames.get(key) + "' AND a.analysis_id=pf.analysis_id AND pf.hit_id NOT LIKE '" + format.get(key) + "'");
         if (badFormat > 0) {
+          result = false;
           ReportManager.problem(this, con, badFormat + " " + logicNames.get(key) + " hit IDs are not in the correct format");
         } else {
           ReportManager.correct(this, con, "All " + logicNames.get(key) + " hits are in the correct format");
@@ -92,6 +94,7 @@ public class AccessionTestCase extends EnsTestCase {
       // check for protein features with no hit_id
       int nullHitIDs = getRowCount(con, "SELECT COUNT(*) FROM protein_feature WHERE hit_id IS NULL OR hit_id=''");
       if (nullHitIDs > 0) {
+        result = false;
         ReportManager.problem(this, con, nullHitIDs + " protein features have null or blank hit_ids");
       } else {
         ReportManager.correct(this, con, "No protein features have null or blank hit_ids");
