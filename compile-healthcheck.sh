720045b6021fc58f2b38fe6c0ee1eac532e23e3a
Glenn Proctor <gp1@sanger.ac.uk>
2004-01-14 13:52:54 +0000

    Now automatically creates build/ directory if required. Copied from branch

--- compile-healthcheck.sh
+++ compile-healthcheck.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# create build directory for class files if it's not there already
+if [ ! -d build ]; then
+  mkdir build
+fi
+
 javac -classpath "lib/ensj.jar:build/" -d build src/org/ensembl/healthcheck/util/*.java src/org/ensembl/healthcheck/testcase/*.java src/org/ensembl/healthcheck/*.java
 
