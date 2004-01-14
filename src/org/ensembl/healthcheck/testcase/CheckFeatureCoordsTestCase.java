8b5dc1785bc59de48e1c527776dc77d2dbb057d9
Glenn Proctor <gp1@sanger.ac.uk>
2004-01-14 16:25:59 +0000

    Bug fix - was trying to read columns from ResultSet that had not been defined in the SELECT

--- src/org/ensembl/healthcheck/testcase/CheckFeatureCoordsTestCase.java
+++ src/org/ensembl/healthcheck/testcase/CheckFeatureCoordsTestCase.java
@@ -28,88 +28,102 @@ import org.ensembl.healthcheck.util.*;
  * Check that feature co-ords make sense.
  */
 public class CheckFeatureCoordsTestCase extends EnsTestCase {
-  
-  /**
-   * Creates a new instance of CheckFeatureCoordsTestCase
-   */
-  public CheckFeatureCoordsTestCase() {
-    addToGroup("post_genebuild");
-    setDescription("Check that feature co-ords (DNA and protein) make sense.");
-  }
-  
-  /**
-   * Iterate over each affected database and perform various checks.
-   * @return Result.
-   */
-  public TestResult run() {
-    
-    boolean result = true;
-    
-    String[] featureTables = { "dna_align_feature", "protein_align_feature", "exon", "repeat_feature",
-    "prediction_transcript", "prediction_exon", "simple_feature", "marker_feature", "misc_feature",
-    "qtl_feature", "karyotype", "gene", "transcript" };
-    
-    
-    for( int tableIndex = 0; tableIndex < featureTables.length; tableIndex++ ) {
-      String tableName = featureTables[tableIndex];
-      
-      DatabaseConnectionIterator it = getDatabaseConnectionIterator();
-      
-      while (it.hasNext()) {
-        
-        Connection con = (Connection)it.next();
-        
-        logger.info("Checking " + tableName + " for " + DBUtils.getShortDatabaseName(con) + " ...");
-        
-        String sql = "SELECT f.seq_region_start, f.seq_region_end, s.length " +
-        "FROM " + tableName + " f, seq_region s " +
-        "WHERE s.seq_region_id = f.seq_region_id " +
-        "AND ( f.seq_region_start > f.seq_region_end " +
-        " OR f.seq_region_start < 1 " +
-        " OR f.seq_region_end > s.length )";
-        try {
-          Statement stmt = con.createStatement();
-          ResultSet rs = stmt.executeQuery(sql);
-          int smallStart = 0;
-          int swapCoords = 0;
-          int largeEnd = 0;
-          
-          while( rs.next() ) {
-            int id = rs.getInt( 1 );
-            int start = rs.getInt( 2 );
-            int end = rs.getInt( 3 );
-            int len = rs.getInt( 4 );
-            
-            if( end < start && swapCoords < 10 ) {
-              ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start > seq_region_end");
-              swapCoords++;
-            }
-            
-            if( end > len && largeEnd < 10 ) {
-              ReportManager.warning(this, con, tableName + " " + id + " has seq_region_end > seq_region_length");
-              largeEnd++;
-            }
-            
-            if( start < 1 && smallStart < 10 ) {
-              ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start < 1" );
-              smallStart++;
-            }
-          }
-          
-          if( smallStart + largeEnd + swapCoords > 0 ) {
-            ReportManager.problem( this, con, ( tableName + " table has coordinate mistakes" ));
-            result = false;
-          } else {
-            ReportManager.correct( this, con, ( tableName + " coordinates seem correct" ));
-          }
-        } catch( SQLException e ) {
-          e.printStackTrace();
-        }
-      }
-    }
-    
-    return new TestResult(getShortTestName(), result);
-    
-  } // run
-  
+
+	/**
+	 * Creates a new instance of CheckFeatureCoordsTestCase
+	 */
+	public CheckFeatureCoordsTestCase() {
+		addToGroup("post_genebuild");
+		setDescription("Check that feature co-ords (DNA and protein) make sense.");
+	}
+
+	/**
+	 * Iterate over each affected database and perform various checks.
+	 * @return Result.
+	 */
+	public TestResult run() {
+
+		boolean result = true;
+
+		String[] featureTables =
+			{
+				"gene",
+				"dna_align_feature",
+				"protein_align_feature",
+				"exon",
+				"repeat_feature",
+				"prediction_transcript",
+				"prediction_exon",
+				"simple_feature",
+				"marker_feature",
+				"misc_feature",
+				"qtl_feature",
+				"karyotype",
+				"transcript" };
+
+		for (int tableIndex = 0; tableIndex < featureTables.length; tableIndex++) {
+			String tableName = featureTables[tableIndex];
+
+			DatabaseConnectionIterator it = getDatabaseConnectionIterator();
+
+			while (it.hasNext()) {
+
+				Connection con = (Connection)it.next();
+
+				logger.info("Checking " + tableName + " for " + DBUtils.getShortDatabaseName(con) + " ...");
+
+				String sql =
+					"SELECT f.seq_region_id, f.seq_region_start, f.seq_region_end, s.length "
+						+ "FROM "
+						+ tableName
+						+ " f, seq_region s "
+						+ "WHERE s.seq_region_id = f.seq_region_id "
+						+ "AND ( f.seq_region_start > f.seq_region_end "
+						+ " OR f.seq_region_start < 1 "
+						+ " OR f.seq_region_end > s.length )";
+				try {
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery(sql);
+					int smallStart = 0;
+					int swapCoords = 0;
+					int largeEnd = 0;
+
+					while (rs.next()) {
+						int id = rs.getInt(1);
+						int start = rs.getInt(2);
+						int end = rs.getInt(3);
+						int len = rs.getInt(4);
+
+						if (end < start && swapCoords < 10) {
+							ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start > seq_region_end");
+							swapCoords++;
+						}
+
+						if (end > len && largeEnd < 10) {
+							ReportManager.warning(this, con, tableName + " " + id + " has seq_region_end > seq_region_length");
+							largeEnd++;
+						}
+
+						if (start < 1 && smallStart < 10) {
+							ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start < 1");
+							smallStart++;
+						}
+					}
+
+					if (smallStart + largeEnd + swapCoords > 0) {
+						ReportManager.problem(this, con, (tableName + " table has coordinate mistakes"));
+						result = false;
+					} else {
+						ReportManager.correct(this, con, (tableName + " coordinates seem correct"));
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return new TestResult(getShortTestName(), result);
+
+	} // run
+
 } // CheckFeatureCoordsTestCase
