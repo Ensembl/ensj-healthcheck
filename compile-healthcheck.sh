b4e42231fd324a27fdd034b6f0b859ffbf305d05
Glenn Proctor <gp1@sanger.ac.uk>
2004-01-26 10:29:32 +0000

    Added /scratch/j2sdk1.4.2/bin explicitly to java/javac commands to remove dependency on path being set correctly for scripts to run

--- compile-healthcheck.sh
+++ compile-healthcheck.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-javac -classpath "lib/ensj.jar:build/" -d build src/org/ensembl/healthcheck/util/*.java src/org/ensembl/healthcheck/testcase/*.java src/org/ensembl/healthcheck/*.java
+# create build directory for class files if it's not there already
+if [ ! -d build ]; then
+  mkdir build
+fi
+
+$JAVA_HOME/bin/javac -classpath "lib/ensj.jar:build/" -d build src/org/ensembl/healthcheck/util/*.java src/org/ensembl/healthcheck/testcase/*.java src/org/ensembl/healthcheck/*.java
 
--- database-name-matcher.sh
+++ database-name-matcher.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -classpath "lib/ensj.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:build/" org.ensembl.healthcheck.DatabaseNameMatcher "$*"
+$JAVA_HOME/bin/java -classpath "lib/ensj.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:build/" org.ensembl.healthcheck.DatabaseNameMatcher "$*"
