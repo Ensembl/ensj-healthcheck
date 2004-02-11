fa4f7e798ff1f25b284c6423af8a888b74b26bd1
Glenn Proctor <gp1@sanger.ac.uk>
2004-02-11 13:17:33 +0000

    Updated to take into account the fact that all bytecode is now in ensj-healthcheck.jar rather than individual .class files in build/

--- build-test-library.sh
+++ build-test-library.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$JAVA_HOME/bin/java -classpath "lib/ensj.jar:lib/junit.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:build/" org.ensembl.healthcheck.BuildTestLibrary doc/external_web_pages/testlist_template.html 
+$JAVA_HOME/bin/java -classpath "lib/ensj-healthcheck.jar:lib/ensj.jar:lib/junit.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.BuildTestLibrary doc/external_web_pages/testlist_template.html 
--- run-healthcheck-gui.sh
+++ run-healthcheck-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$JAVA_HOME/bin/java -classpath "lib/ensj.jar:lib:/ecp1_0beta.jar:lib/log4j-1.2.6.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:build/" org.ensembl.healthcheck.GuiTestRunner $*
+$JAVA_HOME/bin/java -classpath "lib/ensj-healthcheck.jar:lib/ensj.jar:lib:/ecp1_0beta.jar:lib/log4j-1.2.6.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.GuiTestRunner $*
