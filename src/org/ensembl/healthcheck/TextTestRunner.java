0041a0f7f0de4cfa5089b2f8c9b9cde3e93c9db3
Glenn Proctor <gp1@sanger.ac.uk>
2004-02-25 12:31:28 +0000

    Modifed to reflect change in Reporter.finishTestCase signature

--- src/org/ensembl/healthcheck/TextTestRunner.java
+++ src/org/ensembl/healthcheck/TextTestRunner.java
@@ -1,20 +1,21 @@
 /*
-  Copyright (C) 2004 EBI, GRL
+ Copyright (C) 2004 EBI, GRL
  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
  
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
  
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+
 package org.ensembl.healthcheck;
 
 import org.ensembl.healthcheck.util.*;
@@ -29,16 +30,22 @@ import java.util.logging.*;
 public class TextTestRunner extends TestRunner implements Reporter {
 
 	private static String version = "$Id$";
+
 	private boolean forceDatabases = false;
+
 	private boolean debug = false;
+
 	public ArrayList outputBuffer;
+
 	private String lastDatabase = "";
+
 	private int outputLineLength = 65;
 
 	// -------------------------------------------------------------------------
 
 	/**
 	 * Command-line run method.
+	 * 
 	 * @param args The command-line arguments.
 	 */
 	public static void main(String[] args) {
@@ -70,28 +77,33 @@ public class TextTestRunner extends TestRunner implements Reporter {
 		System.out.println("Options:");
 		System.out.println("  -d regexp       Use the given regular expression to decide which databases to use.");
 		System.out.println("  -force          Run the named tests on the databases matched by -d, without ");
-		System.out.println("                  taking into account the regular expressions built into the tests themselves.");
+		System.out
+				.println("                  taking into account the regular expressions built into the tests themselves.");
 		System.out.println("  -h              This message.");
 		System.out.println("  -output level   Set output level; level can be one of ");
 		System.out.println("                  none      nothing is printed");
 		System.out.println("                  problem   only problems are reported");
 		System.out.println("                  correct   only correct results (and problems) are reported");
-		System.out.println("                  summary   only summary info (and problems, and correct reports) are reported");
+		System.out
+				.println("                  summary   only summary info (and problems, and correct reports) are reported");
 		System.out.println("                  info      info (and problem, correct, summary) messages reported");
 		System.out.println("                  all       everything is printed");
-		System.out.println("  -config file    Read config from file (in ensj-healthcheck dir) rather than database.properties");
+		System.out
+				.println("  -config file    Read config from file (in ensj-healthcheck dir) rather than database.properties");
 		System.out.println("  -debug          Print debugging info (for developers only)");
 		System.out.println("  -repair         If appropriate, carry out repair methods on test cases that support it");
 		System.out.println("  -showrepair     Like -repair, but the repair is NOT carried out, just reported.");
-		System.out.println("  -length n       Break output lines at n columns; default is " + outputLineLength + ". 0 means never break");
-		System.out.println(
-			"  -refreshschemas Rebuild the stored schema info; this is rather slow as every schema must be examined, but should be used when a schema structure change has occurred.");
+		System.out.println("  -length n       Break output lines at n columns; default is " + outputLineLength
+				+ ". 0 means never break");
+		System.out
+				.println("  -refreshschemas Rebuild the stored schema info; this is rather slow as every schema must be examined, but should be used when a schema structure change has occurred.");
 		System.out.println("  group1          Names of groups of test cases to run.");
-		System.out.println("                  Note each test case is in a group of its own with the name of the test case.");
+		System.out
+				.println("                  Note each test case is in a group of its own with the name of the test case.");
 		System.out.println("                  This allows individual tests to be run if required.");
 		System.out.println("");
-		System.out.println(
-			"If no tests or test groups are specified, and a database regular expression is given with -d, the matching databases are shown. ");
+		System.out
+				.println("If no tests or test groups are specified, and a database regular expression is given with -d, the matching databases are shown. ");
 		System.out.println("");
 		System.out.println("Currently available tests:");
 
@@ -99,7 +111,7 @@ public class TextTestRunner extends TestRunner implements Reporter {
 		Collections.sort(tests, new TestComparator());
 		Iterator it = tests.iterator();
 		while (it.hasNext()) {
-			EnsTestCase test = (EnsTestCase)it.next();
+			EnsTestCase test = (EnsTestCase) it.next();
 			System.out.print(test.getShortTestName() + " ");
 		}
 
@@ -109,6 +121,7 @@ public class TextTestRunner extends TestRunner implements Reporter {
 
 	/**
 	 * Return the CVS version string for this class.
+	 * 
 	 * @return The version.
 	 */
 	public String getVersion() {
@@ -156,7 +169,8 @@ public class TextTestRunner extends TestRunner implements Reporter {
 
 					i++;
 					preFilterRegexp = args[i];
-					// System.out.println("Will pre-filter database names on " + preFilterRegexp);
+					// System.out.println("Will pre-filter database names on " +
+					// preFilterRegexp);
 
 				} else if (args[i].equals("-force")) {
 
@@ -167,12 +181,14 @@ public class TextTestRunner extends TestRunner implements Reporter {
 
 					i++;
 					PROPERTIES_FILE = args[i];
-					// System.out.println("Will read properties from " + propertiesFileName);
+					// System.out.println("Will read properties from " +
+					// propertiesFileName);
 
 				} else if (args[i].equals("-length")) {
 
 					outputLineLength = Integer.parseInt(args[++i]);
-					//System.out.println("Will use output line length of " + outputLineLength);
+					//System.out.println("Will use output line length of " +
+					// outputLineLength);
 
 				} else {
 
@@ -256,7 +272,8 @@ public class TextTestRunner extends TestRunner implements Reporter {
 				break;
 		}
 
-		outputBuffer.add("    " + level + ":  " + lineBreakString(reportLine.getMessage(), outputLineLength, "              "));
+		outputBuffer.add("    " + level + ":  "
+				+ lineBreakString(reportLine.getMessage(), outputLineLength, "              "));
 	}
 
 	public void startTestCase(EnsTestCase testCase) {
@@ -267,18 +284,14 @@ public class TextTestRunner extends TestRunner implements Reporter {
 		System.out.flush();
 	}
 
-	public void finishTestCase(EnsTestCase testCase, TestResult result) {
+	public void finishTestCase(EnsTestCase testCase, boolean result) {
 
-		if (result.getResult()) {
-			System.out.println(" PASSED");
-		} else {
-			System.out.println(" FAILED");
-		}
+		System.out.println(result ? " PASSED" : " FAILED");
 
 		lastDatabase = "";
 		Iterator it = outputBuffer.iterator();
 		while (it.hasNext()) {
-			System.out.println((String)it.next());
+			System.out.println((String) it.next());
 		}
 		outputBuffer.clear();
 
@@ -292,7 +305,8 @@ public class TextTestRunner extends TestRunner implements Reporter {
 
 		int lastSpace = mesg.lastIndexOf(" ", maxLen);
 		if (lastSpace > 15) {
-			return mesg.substring(0, lastSpace) + "\n" + indent + lineBreakString(mesg.substring(lastSpace + 1), maxLen, indent);
+			return mesg.substring(0, lastSpace) + "\n" + indent
+					+ lineBreakString(mesg.substring(lastSpace + 1), maxLen, indent);
 		} else {
 			return mesg.substring(0, maxLen) + "\n" + indent + lineBreakString(mesg.substring(maxLen), maxLen, indent);
 		}
