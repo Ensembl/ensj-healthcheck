da2b96c3e6361fe9a00f1c6ba9f464e18fbd59a8
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-04 17:00:34 +0000

    Added test for filterArray

--- src/org/ensembl/healthcheck/test/UtilsTest.java
+++ src/org/ensembl/healthcheck/test/UtilsTest.java
@@ -1,4 +1,3 @@
-
 package org.ensembl.healthcheck.test;
 
 import java.io.File;
@@ -12,52 +11,63 @@ import junit.framework.*;
  * @author glenn
  */
 public class UtilsTest extends TestCase {
-  
-  public UtilsTest(java.lang.String testName) {
-    super(testName);
-  }
-  
-  public static void main(java.lang.String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
-  
-  public static Test suite() {
-    TestSuite suite = new TestSuite(UtilsTest.class);
-    
-    return suite;
-  }
-  
-  // -----------------------------------------------------------------
-  
-  /** Test of readPropertiesFile method, of class org.ensembl.healthcheck.util.Utils. */
-  public void testReadPropertiesFile() {
- 
-    assertNotNull(Utils.readSimplePropertiesFile("database.properties"));
 
-}
-  
-// -----------------------------------------------------------------
+	public UtilsTest(java.lang.String testName) {
+		super(testName);
+	}
 
-  public void testgetSubDirs() {
-  	
-  	String startDir = System.getProperty("user.dir");
-  	System.out.println("Checking subdirectories of " + startDir);
-  	String[] subdirs = Utils.getSubDirs(startDir);
-  	assertNotNull(subdirs);
-  	assertTrue("Fewer than expected subdirectories", subdirs.length > 3);
-  	for (int i = 0; i < subdirs.length; i++) {
-  		assertNotNull(subdirs[i]);
-  		File f = new File(subdirs[i]);
-  		assertTrue("Got a file where we should have only got directories", f.isDirectory());
-  		System.out.println("\t" + subdirs[i]);
-  	}
-  	
-  	subdirs = Utils.getSubDirs("/some/madeup/dir");
-  	assertNotNull(subdirs);
-  	assertTrue(subdirs.length == 0);
-  	
-  }
-  
-  // -----------------------------------------------------------------
-  
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(UtilsTest.class);
+
+		return suite;
+	}
+
+	// -----------------------------------------------------------------
+
+	/** Test of readPropertiesFile method, of class org.ensembl.healthcheck.util.Utils. */
+	public void testReadPropertiesFile() {
+
+		assertNotNull(Utils.readSimplePropertiesFile("database.properties"));
+
+	}
+
+	// -----------------------------------------------------------------
+
+	public void testgetSubDirs() {
+
+		String startDir = System.getProperty("user.dir");
+		System.out.println("Checking subdirectories of " + startDir);
+		String[] subdirs = Utils.getSubDirs(startDir);
+		assertNotNull(subdirs);
+		assertTrue("Fewer than expected subdirectories", subdirs.length > 3);
+		for (int i = 0; i < subdirs.length; i++) {
+			assertNotNull(subdirs[i]);
+			File f = new File(subdirs[i]);
+			assertTrue("Got a file where we should have only got directories", f.isDirectory());
+			System.out.println("\t" + subdirs[i]);
+		}
+
+		subdirs = Utils.getSubDirs("/some/madeup/dir");
+		assertNotNull(subdirs);
+		assertTrue(subdirs.length == 0);
+
+	}
+
+	// -----------------------------------------------------------------
+
+	public void testFilterArray() {
+
+		String[] source = { "a", "b", "c", "d", "e", "f" };
+		String[] remove = { "c", "e" };
+		Object[] filtered = Utils.filterArray(source, remove);
+		assertEquals(filtered.length, 4);
+
+	}
+
+	// -----------------------------------------------------------------
+
 }
