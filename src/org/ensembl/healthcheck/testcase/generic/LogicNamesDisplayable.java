01ff0977a8fb1aeca4a325b4033bb2ae30e0cfb3
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-09 11:58:46 +0000

    Now allows logic names defined in any of the supporting feature tables, as well as those defined in logicnames.txt

--- src/org/ensembl/healthcheck/testcase/generic/LogicNamesDisplayable.java
+++ src/org/ensembl/healthcheck/testcase/generic/LogicNamesDisplayable.java
@@ -24,158 +24,199 @@ import org.ensembl.healthcheck.*;
 import org.ensembl.healthcheck.util.*;
 
 /**
- * Check that the logic names in the analysis table are displayable. Currently reads the
- * list of displayable logc names from a text file. Current set of logic names is stored
- * at http://www.ensembl.org/Docs/wiki/html/EnsemblDocs/LogicNames.html
+ * Check that the logic names in the analysis table are displayable. Currently reads the list of
+ * displayable logc names from a text file. Current set of logic names is stored at
+ * http://www.ensembl.org/Docs/wiki/html/EnsemblDocs/LogicNames.html
  */
 public class LogicNamesDisplayable extends SingleDatabaseTestCase {
 
-	// a list of the tables to check the analysis_id in
-	private String[] featureTables =
-		{ "gene", "prediction_transcript", "dna_align_feature", "marker_feature", "protein_feature", "qtl_feature", "repeat_feature", "simple_feature", "protein_align_feature" };
+    // a list of the tables to check the analysis_id in
+    private String[] featureTables = { "gene", "prediction_transcript", "dna_align_feature", "marker_feature", "protein_feature",
+            "qtl_feature", "repeat_feature", "simple_feature", "protein_align_feature"};
 
-	private static final String LOGIC_NAMES_FILE = "logicnames.txt";
-	private static final boolean CASE_SENSITIVE = false;
+    private static final String LOGIC_NAMES_FILE = "logicnames.txt";
 
-	/**
-		 * Creates a new instance of LogicNamesDisplayable
-		 */
-	public LogicNamesDisplayable() {
+    private static final boolean CASE_SENSITIVE = false;
 
-		addToGroup("post_genebuild");
-		addToGroup("release");
+    /**
+     * Creates a new instance of LogicNamesDisplayable
+     */
+    public LogicNamesDisplayable() {
 
-		setDescription("Checks that all logic names in analysis are displayable");
-		setHintLongRunning(true);
+        addToGroup("post_genebuild");
+        addToGroup("release");
 
-	}
+        setDescription("Checks that all logic names in analysis are displayable");
+        setHintLongRunning(true);
 
-	public boolean run(DatabaseRegistryEntry dbre) {
+    }
 
-		boolean result = true;
+    public boolean run(DatabaseRegistryEntry dbre) {
 
-		try {
-			Connection con = dbre.getConnection();
-			result &= checkLogicNames(con);
-			result &= checkProteinFeatureAnalysis(con);
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+        boolean result = true;
 
-		return result;
+        try {
+            Connection con = dbre.getConnection();
+            result &= checkLogicNames(con);
+            result &= checkProteinFeatureAnalysis(con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-	} // run
+        return result;
 
-	/**
-		 * Looks at analysis IDs in feature tables and checks the logic names they are
-		 * associated with will be displayed by the web code. The list of valid logic names is
-		 * currently at http://www.ensembl.org/Docs/wiki/html/EnsemblDocs/LogicNames.html Note
-		 * that this test case actually uses the names from the file logicnames.txt which
-		 * currently has to be manually created from the above URL.
-		 * 
-		 * @return
-		 */
-	private boolean checkLogicNames(Connection con) throws SQLException {
-		boolean result = true;
-		String message = CASE_SENSITIVE ? "Logic name comparison is case sensitive" : "Logic name comparison is NOT case sensitive";
-		logger.info(message);
+    } // run
 
-		// read the file containing the allowed logic names
-		String[] allowedLogicNames = Utils.readTextFile(LOGIC_NAMES_FILE);
-		logger.fine("Read " + allowedLogicNames.length + " logic names from " + LOGIC_NAMES_FILE);
+    /**
+     * Looks at analysis IDs in feature tables and checks the logic names they are associated with
+     * will be displayed by the web code. The list of valid logic names is currently at
+     * http://www.ensembl.org/Docs/wiki/html/EnsemblDocs/LogicNames.html Note that this test case
+     * actually uses the names from the file logicnames.txt which currently has to be manually
+     * created from the above URL.
+     * 
+     * @return
+     */
+    private boolean checkLogicNames(Connection con) throws SQLException {
 
-		// cache logic_names by analysis_id
-		Map logicNamesByAnalID = new HashMap();
-		Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT analysis_id, logic_name" + " FROM analysis");
-		while (rs.next()) {
-			logicNamesByAnalID.put(rs.getString("analysis_id"), rs.getString("logic_name"));
-		}
+        boolean result = true;
+        String message = CASE_SENSITIVE ? "Logic name comparison is case sensitive" : "Logic name comparison is NOT case sensitive";
+        logger.info(message);
 
-		for (int t = 0; t < featureTables.length; t++) {
-			String featureTableName = featureTables[t];
-			logger.finest("Analysing features in " + featureTableName);
+        // read the file containing the allowed logic names
+        String[] allowedLogicNames = Utils.readTextFile(LOGIC_NAMES_FILE);
+        logger.fine("Read " + allowedLogicNames.length + " logic names from " + LOGIC_NAMES_FILE);
 
-			// get analysis IDs
-			String[] analysisIDs = getColumnValues(con, "SELECT DISTINCT analysis_id FROM " + featureTableName);
+        // we don't want to complain about analyses that are defined in supporting features as
+        // these will be drawn automatically by the web code, so find these and add them to the
+        // list of allowed logic names
+        allowedLogicNames = addSupportingFeatureLogicNames(con, allowedLogicNames);
 
-			// check each analysis ID
-			for (int i = 0; i < analysisIDs.length; i++) {
-				/*
-						 * check that there is an entry in the analysis table with this ID (i.e. foreign
-						 * key integrity check)
-						 */
-				if (logicNamesByAnalID.get(analysisIDs[i]) == null) {
-					ReportManager.problem(this, con, "Feature table " + featureTableName + " refers to non-existant analysis with ID " + analysisIDs[i]);
-					result = false;
-				} else {
-					// check that logic name corresponding to this analysis id is valid
-					String logicName = (String)logicNamesByAnalID.get(analysisIDs[i]);
-					if (!Utils.stringInArray(logicName, allowedLogicNames, CASE_SENSITIVE)) {
-						ReportManager.problem(this, con, "Feature table " + featureTableName + " has features with logic name " + logicName + " which will not be drawn");
-						result = false;
-					}
-				}
-			}
-		}
-		rs.close();
-		stmt.close();
-		return result;
-	}
+        // cache logic_names by analysis_id
+        Map logicNamesByAnalID = new HashMap();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT analysis_id, logic_name" + " FROM analysis");
+        while (rs.next()) {
+            logicNamesByAnalID.put(rs.getString("analysis_id"), rs.getString("logic_name"));
+        }
 
-	/*
-		 * Does a set of analysis checks for the protein feature table This table has some
-		 * bizarre requirements for the associated analysis which will hopefully change at some
-		 * point. In the meantime this healthcheck will verify that those requirements are met.
-		 */
-	private boolean checkProteinFeatureAnalysis(Connection con) throws SQLException {
-		Statement stmt = con.createStatement();
-		ResultSet rs =
-			stmt.executeQuery(
-				"SELECT a.analysis_id, a.gff_feature, a.gff_source " + "FROM analysis a, protein_feature pf " + "WHERE a.analysis_id = pf.analysis_id " + "GROUP BY a.analysis_id");
+        for (int t = 0; t < featureTables.length; t++) {
+            String featureTableName = featureTables[t];
+            logger.finest("Analysing features in " + featureTableName);
 
-		boolean noProblems = true;
+            // get analysis IDs
+            String[] analysisIDs = getColumnValues(con, "SELECT DISTINCT analysis_id FROM " + featureTableName);
 
-		while (rs != null && rs.next()) {
-			int analysisId = rs.getInt(1);
-			String gffFeat = rs.getString(2).toUpperCase();
-			String gffSource = rs.getString(3).toUpperCase();
+            // check each analysis ID
+            for (int i = 0; i < analysisIDs.length; i++) {
+                /*
+                 * check that there is an entry in the analysis table with this ID (i.e. foreign
+                 * key integrity check)
+                 */
+                if (logicNamesByAnalID.get(analysisIDs[i]) == null) {
+                    ReportManager.problem(this, con, "Feature table " + featureTableName
+                            + " refers to non-existant analysis with ID " + analysisIDs[i]);
+                    result = false;
+                } else {
+                    // check that logic name corresponding to this analysis id
+                    // is valid
+                    String logicName = (String) logicNamesByAnalID.get(analysisIDs[i]);
+                    if (!Utils.stringInArray(logicName, allowedLogicNames, CASE_SENSITIVE)) {
+                        ReportManager.problem(this, con, "Feature table " + featureTableName + " has features with logic name "
+                                + logicName + " which will not be drawn");
+                        result = false;
+                    }
+                }
+            }
+        }
+        rs.close();
+        stmt.close();
+        return result;
+    }
 
-			if (gffSource.equals("PRINTS") || gffSource.equals("PFAM") || gffSource.equals("PROSITE") || gffSource.equals("PROFILE")) {
+    /*
+     * Does a set of analysis checks for the protein feature table This table has some bizarre
+     * requirements for the associated analysis which will hopefully change at some point. In the
+     * meantime this healthcheck will verify that those requirements are met.
+     */
+    private boolean checkProteinFeatureAnalysis(Connection con) throws SQLException {
 
-				/* gff_feature must be domain */
-				if (!gffFeat.equals("DOMAIN")) {
-					ReportManager.problem(
-						this,
-						con,
-						"protein_feature" + " analysis with analysis_id = " + analysisId + " and gffSource = " + gffSource + " must have gffSource eq 'DOMAIN'");
-					noProblems = false;
-				}
-			} else if (gffSource.equals("BLASTP") || gffSource.equals("SEG") || gffSource.equals("TMHMM") || gffSource.equals("NCOILS") || gffSource.equals("SIGNALP")) {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT a.analysis_id, a.gff_feature, a.gff_source "
+                + "FROM analysis a, protein_feature pf " + "WHERE a.analysis_id = pf.analysis_id " + "GROUP BY a.analysis_id");
 
-				/* gff_feature must not be domain */
-				if (gffFeat.equals("DOMAIN")) {
-					ReportManager.problem(
-						this,
-						con,
-						"protein_feature" + " analysis with analysis_id = " + analysisId + " and gffSource = " + gffSource + " must have gffSource ne 'DOMAIN'");
-					noProblems = false;
-				}
-			} else if (!gffSource.equals("SUPERFAMILY")) {
-				/* problem... not a known gffsource */
-				ReportManager.problem(this, con, "protein_feature" + " analysis with analysis_id = " + analysisId + " has unknown gff_source = '" + gffSource + "'");
-				noProblems = false;
-			}
-		} // while rs
+        boolean noProblems = true;
 
-		if (noProblems) {
-			ReportManager.correct(this, con, "protein_feature analysis table " + "entries look OK");
-		}
+        while (rs != null && rs.next()) {
+            int analysisId = rs.getInt(1);
+            String gffFeat = rs.getString(2).toUpperCase();
+            String gffSource = rs.getString(3).toUpperCase();
 
-		rs.close();
-		stmt.close();
+            if (gffSource.equals("PRINTS") || gffSource.equals("PFAM") || gffSource.equals("PROSITE")
+                    || gffSource.equals("PROFILE")) {
 
-		return noProblems;
-	}
+                /* gff_feature must be domain */
+                if (!gffFeat.equals("DOMAIN")) {
+                    ReportManager.problem(this, con, "protein_feature" + " analysis with analysis_id = " + analysisId
+                            + " and gffSource = " + gffSource + " must have gffSource eq 'DOMAIN'");
+                    noProblems = false;
+                }
+            } else if (gffSource.equals("BLASTP") || gffSource.equals("SEG") || gffSource.equals("TMHMM")
+                    || gffSource.equals("NCOILS") || gffSource.equals("SIGNALP")) {
+
+                /* gff_feature must not be domain */
+                if (gffFeat.equals("DOMAIN")) {
+                    ReportManager.problem(this, con, "protein_feature" + " analysis with analysis_id = " + analysisId
+                            + " and gffSource = " + gffSource + " must have gffSource ne 'DOMAIN'");
+                    noProblems = false;
+                }
+            } else if (!gffSource.equals("SUPERFAMILY")) {
+                /* problem... not a known gffsource */
+                ReportManager.problem(this, con, "protein_feature" + " analysis with analysis_id = " + analysisId
+                        + " has unknown gff_source = '" + gffSource + "'");
+                noProblems = false;
+            }
+        } // while rs
+
+        if (noProblems) {
+            ReportManager.correct(this, con, "protein_feature analysis table " + "entries look OK");
+        }
+
+        rs.close();
+        stmt.close();
+
+        return noProblems;
+    }
+
+    // -------------------------------------------------------------------------
+    /**
+     * Add any logic names defined in supporting features to the list of allowed logic names.
+     * 
+     * @param initialNames
+     *            The initial list of logic names.
+     * @return initialNames with any logic names from the supporting feature tables added to it.
+     */
+    private String[] addSupportingFeatureLogicNames(Connection con, String[] initialNames) {
+
+        List result = new ArrayList(Arrays.asList(initialNames));
+
+        String[] featureTypes = getColumnValues(con, "SELECT DISTINCT(feature_type) FROM supporting_feature");
+
+        for (int i = 0; i < featureTypes.length; i++) {
+            String featureType = featureTypes[i];
+            String sql = "SELECT DISTINCT a.logic_name FROM supporting_feature sf, " + featureType + " f, analysis a "
+                    + "WHERE sf.feature_type='" + featureType + "' AND sf.feature_id=f." + featureType + "_id "
+                    + "AND f.analysis_id=a.analysis_id";
+            String[] logicNames = getColumnValues(con, sql);
+            for (int j = 0; j < logicNames.length; j++) {
+                result.add(logicNames[j]);
+                logger.finest("Added logic name " + logicNames[j] + " from supporting features in " + featureType);
+            }
+        }
+
+        return (String[]) result.toArray(new String[result.size()]);
+
+    } // addSupportingFeatureLogicNames
+
+    // -------------------------------------------------------------------------
 
 } // LogicNamesDisplayable
