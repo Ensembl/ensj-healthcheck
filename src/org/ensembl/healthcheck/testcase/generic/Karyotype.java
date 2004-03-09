11a95d6aedd4cf44bb93fb356e6de469cd453199
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-09 13:08:02 +0000

    Moved out of old ChromosomeLengths test case.

---
+++ src/org/ensembl/healthcheck/testcase/generic/Karyotype.java
@@ -0,0 +1,66 @@
+/*
+ * Created on 09-Mar-2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.*;
+
+import org.ensembl.healthcheck.*;
+import org.ensembl.healthcheck.testcase.*;
+
+/**
+ * Check if any chromosomes that have different lengths in karyotype & seq_region tables.
+ */
+public class Karyotype extends SingleDatabaseTestCase {
+
+    public boolean run(DatabaseRegistryEntry dbre) {
+
+        boolean result = true;
+
+        Connection con = dbre.getConnection();
+
+        // The seq_region.length and karyotype.length should always be the same.
+        // The SQL returns failures
+
+        String karsql = "SELECT sr.name, max(kar.seq_region_end), sr.length " + "FROM seq_region sr, karyotype kar "
+                + "WHERE sr.seq_region_id=kar.seq_region_id " + "GROUP BY kar.seq_region_id "
+                + "HAVING sr.length <> MAX(kar.seq_region_end)";
+        int count = 0;
+        try {
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(karsql);
+            if (rs != null) {
+                while (rs.next() && count < 50) {
+                    count++;
+                    String chrName = rs.getString(1);
+                    int karLen = rs.getInt(2);
+                    int chrLen = rs.getInt(3);
+                    String prob = "";
+                    int bp = 0;
+                    if (karLen > chrLen) {
+                        bp = karLen - chrLen;
+                        prob = "longer";
+                    } else {
+                        bp = chrLen - karLen;
+                        prob = "shorter";
+                    }
+                    result = false;
+                    ReportManager.problem(this, con, "Chromosome " + chrName + " is " + bp + "bp " + prob
+                            + " in the karyotype table than " + "in the seq_region table");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (count == 0) {
+            ReportManager.correct(this, con, "Chromosome lengths are the same" + " in karyotype and seq_region tables");
+        }
+
+        return result;
+
+    } // run
+
+} // Karyotype
