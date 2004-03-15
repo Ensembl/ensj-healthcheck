09d39ea785a10644cac480ef8448dc7bcda14b82
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-15 13:27:40 +0000

    Tidied up formatting.

--- src/org/ensembl/healthcheck/testcase/generic/GeneTranscriptStartEnd.java
+++ src/org/ensembl/healthcheck/testcase/generic/GeneTranscriptStartEnd.java
@@ -1,18 +1,25 @@
 /*
- * Created on 15-Mar-2004
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Copyright (C) 2004 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
  */
+
 package org.ensembl.healthcheck.testcase.generic;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
-import org.ensembl.healthcheck.DatabaseRegistryEntry;
-import org.ensembl.healthcheck.ReportManager;
-import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+import org.ensembl.healthcheck.*;
+import org.ensembl.healthcheck.testcase.*;
 
 /**
  * Check that the start and end of genes and transcripts make sense.
