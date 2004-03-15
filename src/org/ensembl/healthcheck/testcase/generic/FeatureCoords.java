3de42427e1988ac0f2749edcde71ec32163ef5c5
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-15 16:10:36 +0000

    Added density_feature to list of feature tables.

--- src/org/ensembl/healthcheck/testcase/generic/FeatureCoords.java
+++ src/org/ensembl/healthcheck/testcase/generic/FeatureCoords.java
@@ -1,19 +1,19 @@
 /*
-  Copyright (C) 2004 EBI, GRL
+ Copyright (C) 2004 EBI, GRL
  
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
  
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
  
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 package org.ensembl.healthcheck.testcase.generic;
@@ -29,98 +29,83 @@ import org.ensembl.healthcheck.*;
  */
 public class FeatureCoords extends SingleDatabaseTestCase {
 
-	/**
-	 * Creates a new instance of CheckFeatureCoordsTestCase
-	 */
-	public FeatureCoords() {
-		addToGroup("post_genebuild");
-		addToGroup("release");
-		setDescription("Check that feature co-ords (DNA and protein) make sense.");
-		setHintLongRunning(true);
-	}
+    /**
+     * Creates a new instance of CheckFeatureCoordsTestCase
+     */
+    public FeatureCoords() {
 
-	/**
-	 * Iterate over each affected database and perform various checks.
-	 * @return Result.
-	 */
-	public boolean run(DatabaseRegistryEntry dbre) {
+        addToGroup("post_genebuild");
+        addToGroup("release");
+        setDescription("Check that feature co-ords (DNA and protein) make sense.");
+        setHintLongRunning(true);
+    }
 
-		boolean result = true;
+    /**
+     * Iterate over each affected database and perform various checks.
+     * 
+     * @return Result.
+     */
+    public boolean run(DatabaseRegistryEntry dbre) {
 
-		String[] featureTables =
-			{
-				"gene",
-				"dna_align_feature",
-				"protein_align_feature",
-				"exon",
-				"repeat_feature",
-				"prediction_transcript",
-				"prediction_exon",
-				"simple_feature",
-				"marker_feature",
-				"misc_feature",
-				"qtl_feature",
-				"karyotype",
-				"transcript" };
+        boolean result = true;
 
-		for (int tableIndex = 0; tableIndex < featureTables.length; tableIndex++) {
-			String tableName = featureTables[tableIndex];
+        String[] featureTables = { "gene", "dna_align_feature", "protein_align_feature", "exon", "repeat_feature",
+                "prediction_transcript", "prediction_exon", "simple_feature", "marker_feature", "misc_feature", "qtl_feature",
+                "karyotype", "transcript", "density_feature"};
 
-			Connection con = dbre.getConnection();
+        for (int tableIndex = 0; tableIndex < featureTables.length; tableIndex++) {
+            String tableName = featureTables[tableIndex];
 
-			logger.info("Checking " + tableName + " for " + DBUtils.getShortDatabaseName(con) + " ...");
+            Connection con = dbre.getConnection();
 
-			String sql =
-				"SELECT f.seq_region_id, f.seq_region_start, f.seq_region_end, s.length "
-					+ "FROM "
-					+ tableName
-					+ " f, seq_region s "
-					+ "WHERE s.seq_region_id = f.seq_region_id "
-					+ "AND ( f.seq_region_start > f.seq_region_end "
-					+ " OR f.seq_region_start < 1 "
-					+ " OR f.seq_region_end > s.length )";
-			try {
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(sql);
-				int smallStart = 0;
-				int swapCoords = 0;
-				int largeEnd = 0;
+            logger.info("Checking " + tableName + " for " + DBUtils.getShortDatabaseName(con) + " ...");
 
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					int start = rs.getInt(2);
-					int end = rs.getInt(3);
-					int len = rs.getInt(4);
+            String sql = "SELECT f.seq_region_id, f.seq_region_start, f.seq_region_end, s.length " + "FROM " + tableName
+                    + " f, seq_region s " + "WHERE s.seq_region_id = f.seq_region_id "
+                    + "AND ( f.seq_region_start > f.seq_region_end " + " OR f.seq_region_start < 1 "
+                    + " OR f.seq_region_end > s.length )";
+            try {
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                int smallStart = 0;
+                int swapCoords = 0;
+                int largeEnd = 0;
 
-					if (end < start && swapCoords < 10) {
-						ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start > seq_region_end");
-						swapCoords++;
-					}
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    int start = rs.getInt(2);
+                    int end = rs.getInt(3);
+                    int len = rs.getInt(4);
 
-					if (end > len && largeEnd < 10) {
-						ReportManager.warning(this, con, tableName + " " + id + " has seq_region_end > seq_region_length");
-						largeEnd++;
-					}
+                    if (end < start && swapCoords < 10) {
+                        ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start > seq_region_end");
+                        swapCoords++;
+                    }
 
-					if (start < 1 && smallStart < 10) {
-						ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start < 1");
-						smallStart++;
-					}
-				}
+                    if (end > len && largeEnd < 10) {
+                        ReportManager.warning(this, con, tableName + " " + id + " has seq_region_end > seq_region_length");
+                        largeEnd++;
+                    }
 
-				if (smallStart + largeEnd + swapCoords > 0) {
-					ReportManager.problem(this, con, (tableName + " table has coordinate mistakes"));
-					result = false;
-				} else {
-					ReportManager.correct(this, con, (tableName + " coordinates seem correct"));
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+                    if (start < 1 && smallStart < 10) {
+                        ReportManager.warning(this, con, tableName + " " + id + " has seq_region_start < 1");
+                        smallStart++;
+                    }
+                }
 
-		return result;
+                if (smallStart + largeEnd + swapCoords > 0) {
+                    ReportManager.problem(this, con, (tableName + " table has coordinate mistakes"));
+                    result = false;
+                } else {
+                    ReportManager.correct(this, con, (tableName + " coordinates seem correct"));
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
-	} // run
+        return result;
+
+    } // run
 
 } // FeatureCoords
