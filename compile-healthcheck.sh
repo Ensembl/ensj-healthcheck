553f52725fa0b02ed274ff015ff731ab66aa1bd5
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-18 15:09:12 +0000

    Added JUnit jar file to compilation classpath

--- compile-healthcheck.sh
+++ compile-healthcheck.sh
@@ -5,6 +5,6 @@ if [ ! -d build ]; then
   mkdir build
 fi
 
-find src -name '*.java' | xargs $JAVA_HOME/bin/javac -d build -sourcepath src
+find src -name '*.java' | xargs $JAVA_HOME/bin/javac -classpath "lib/junit.jar" -d build -sourcepath src
 
 $JAVA_HOME/bin/jar cf lib/ensj-healthcheck.jar -C build org images/*.gif
