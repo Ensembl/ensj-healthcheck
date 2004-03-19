ab03d0d434b3f55bdff15274a7cf5c3e8129bf2d
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-19 17:16:51 +0000

    Fixed bug noticed by Graham - now prints out more than one of the problems.

--- src/org/ensembl/healthcheck/testcase/generic/AssemblySeqregion.java
+++ src/org/ensembl/healthcheck/testcase/generic/AssemblySeqregion.java
@@ -39,12 +39,11 @@ public class AssemblySeqregion extends SingleDatabaseTestCase {
         addToGroup("post_genebuild");
         addToGroup("release");
         setDescription("Check that the chromosome lengths from the seq_region table agree with both the assembly table and the karyotype table.");
-    
+
     }
 
     /**
-     * @param dbre
-     *          The database to use.
+     * @param dbre The database to use.
      * @return The test case result.
      */
     public boolean run(DatabaseRegistryEntry dbre) {
@@ -59,22 +58,23 @@ public class AssemblySeqregion extends SingleDatabaseTestCase {
         // assembly length
         // The SQL returns failures
         // ----------------------------------------------------
-        String sql = "SELECT sr.name AS name, sr.length, cs.name AS coord_system " + "FROM seq_region sr, assembly ass, coord_system cs "
-                + "WHERE sr.coord_system_id=cs.coord_system_id " + "AND ass.asm_seq_region_id = sr.seq_region_id "
-                + "GROUP BY ass.asm_seq_region_id " + "HAVING sr.length < MAX(ass.asm_end)";
+        String sql = "SELECT sr.name AS name, sr.length, cs.name AS coord_system "
+                + "FROM seq_region sr, assembly ass, coord_system cs " + "WHERE sr.coord_system_id=cs.coord_system_id "
+                + "AND ass.asm_seq_region_id = sr.seq_region_id " + "GROUP BY ass.asm_seq_region_id "
+                + "HAVING sr.length < MAX(ass.asm_end)";
 
         try {
 
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery(sql);
             int i = 0;
-            if (rs.next() && i++ < 50) {
+            while (rs.next() && i++ < 50) {
                 result = false;
                 String cs = rs.getString("coord_system");
                 String sr = rs.getString("name");
                 ReportManager.problem(this, con, cs + " " + sr + " is shorter in seq_region than in assembly");
-
-            } else {
+            }
+            if (i == 0) {
                 ReportManager.correct(this, con,
                         "Sequence region lengths are equal or greater in the seq_region table compared to the assembly table");
             }
@@ -85,15 +85,16 @@ public class AssemblySeqregion extends SingleDatabaseTestCase {
 
         // -------------------------------------------------------
         // check various other things about the assembly table
-        // Check for mismatched lengths of assembled and component sides. 
-        // ie where (asm_end - asm_start + 1) != (cmp_end - cmp_start + 1) 
+        // Check for mismatched lengths of assembled and component sides.
+        // ie where (asm_end - asm_start + 1) != (cmp_end - cmp_start + 1)
         int rows = getRowCount(con, "SELECT COUNT(*) FROM assembly WHERE (asm_end - asm_start + 1) != (cmp_end - cmp_start + 1)");
         if (rows > 0) {
-            ReportManager.problem(this, con, rows + " rows in assembly table have mismatched lengths of assembled and component sides");
+            ReportManager.problem(this, con, rows
+                    + " rows in assembly table have mismatched lengths of assembled and component sides");
         } else {
             ReportManager.correct(this, con, "All rows in assembly table have matching lengths of assembled and component sides");
         }
-        
+
         // check for start/end < 1
         rows = getRowCount(con, "SELECT COUNT(*) FROM assembly WHERE asm_start < 1 OR asm_end < 1 OR cmp_start < 1 OR cmp_end < 1");
         if (rows > 0) {
@@ -101,7 +102,7 @@ public class AssemblySeqregion extends SingleDatabaseTestCase {
         } else {
             ReportManager.correct(this, con, "All rows in assembly table have start and end coords > 0");
         }
-        
+
         // check for end < start
         rows = getRowCount(con, "SELECT COUNT(*) FROM assembly WHERE asm_end < asm_start OR cmp_end < cmp_start");
         if (rows > 0) {
@@ -109,7 +110,7 @@ public class AssemblySeqregion extends SingleDatabaseTestCase {
         } else {
             ReportManager.correct(this, con, "All rows in assembly table have end coords > start coords");
         }
-        
+
         return result;
 
     } // run
