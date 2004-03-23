8df8c40fe171d5d395e247e9c23b307b660f2702
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-23 16:28:49 +0000

    Added JGoodies look and feel jar to classpath.

--- run-healthcheck-gui.sh
+++ run-healthcheck-gui.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-$JAVA_HOME/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.gui.GuiTestRunner $*
+$JAVA_HOME/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar:lib/looks-1.2.1.jar" org.ensembl.healthcheck.gui.GuiTestRunner $*
