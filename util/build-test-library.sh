4adf73df0a9938e13988379c65ddd8cb86ce86bf
Glenn Proctor <gp1@sanger.ac.uk>
2004-03-24 11:55:06 +0000

    Windows equvalents of .sh scripts in main directory. Put in util so as not to clutter everything else up.

--- util/build-test-library.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-$JAVA_HOME/bin/java -classpath "lib/ensj-healthcheck.jar:lib/ensj.jar:lib/junit.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.BuildTestLibrary doc/external_web_pages/testlist_template.html 
