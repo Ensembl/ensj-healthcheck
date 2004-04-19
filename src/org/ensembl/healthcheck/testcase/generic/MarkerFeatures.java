78ee62950adb66a50a203c36121bd8a3f3e5e011
Glenn Proctor <gp1@sanger.ac.uk>
2004-04-19 10:35:01 +0000

    map_weight check now verifies that the map_weights have been calculated correctly rather than just checking that they're non-zero.

--- src/org/ensembl/healthcheck/testcase/generic/MarkerFeatures.java
+++ src/org/ensembl/healthcheck/testcase/generic/MarkerFeatures.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2004 EBI, GRL
  * 
- * This library is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free Software
- * Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  * 
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License along with
- * this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
  */
 
 package org.ensembl.healthcheck.testcase.generic;
@@ -23,8 +23,8 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * Check that marker features exist if markers exist, and that map_wieghts are
- * set to non-zero values
+ * Check that marker features exist if markers exist, and that map_wieghts are set to non-zero
+ * values
  */
 public class MarkerFeatures extends SingleDatabaseTestCase {
 
@@ -32,16 +32,16 @@ public class MarkerFeatures extends SingleDatabaseTestCase {
      * Creates a new instance of CheckMarkerFeatures
      */
     public MarkerFeatures() {
+
         addToGroup("post_genebuild");
         addToGroup("release");
         setDescription("Checks that marker_features exist and that they have" + " non-zero map_weights");
     }
 
     /**
-     * Verify marker features exist if markers exist, and that map weights are
-     * non-zero.
-     * @param dbre
-     *          The database to use.
+     * Verify marker features exist if markers exist, and that map weights are non-zero.
+     * 
+     * @param dbre The database to use.
      * @return Result.
      */
     public boolean run(DatabaseRegistryEntry dbre) {
@@ -52,28 +52,30 @@ public class MarkerFeatures extends SingleDatabaseTestCase {
         boolean markersExist = getRowCount(con, "select count(*) from marker") > 0;
 
         /*
-         * assume this species has no markers, dangling refs test case will
-         * catch problem if marker_features exist without markers
+         * assume this species has no markers, dangling refs test case will catch problem if
+         * marker_features exist without markers
          */
         if (!markersExist) {
             return true;
         }
 
-        int count = getRowCount(con, "select count(*) from marker_feature");
+        int rowCount = getRowCount(con, "SELECT COUNT(*) FROM marker_feature");
 
-        if (count == 0) {
-            ReportManager.problem(this, con, "no marker features in database" + "even though markers are present");
+        if (rowCount == 0) {
+            ReportManager.problem(this, con, "No marker features in database even though markers are present");
             result = false;
         }
 
-        count = getRowCount(con, "select count(*) from marker_feature" + " where map_weight = 0");
+        int badWeightCount = getRowCount(con, "SELECT marker_id, COUNT(*) AS correct, map_weight FROM marker_feature GROUP BY marker_id HAVING map_weight != correct");
 
-        if (count > 0) {
-            ReportManager.problem(this, con, "marker features have not been" + "assigned correct map weights");
+        if (badWeightCount > 0) {
+            ReportManager.problem(this, con, badWeightCount + " marker features have not been assigned correct map weights");
             result = false;
         }
 
-        ReportManager.correct(this, con, "Marker features appear to be ok");
+        if (result) {
+            ReportManager.correct(this, con, "Marker features appear to be ok");
+        }
 
         return result;
 
