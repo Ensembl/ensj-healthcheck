c759b698b8de0db3a5bcd20f848d7dafe80ef7be
Stephen Searle <searle@sanger.ac.uk>
2004-07-23 10:11:40 +0000

    Test to check dna tables in non core, core schema dbs (eg. est and estgene) are empty (should be using core db as reference).

---
+++ src/org/ensembl/healthcheck/testcase/generic/DNAEmpty.java
@@ -0,0 +1,85 @@
+/*
+ Copyright (C) 2004 EBI, GRL
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.Connection;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.DatabaseType;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+
+/**
+ * Healthcheck for the dna table.
+ * Should be empty in an est database
+ */
+
+public class DNAEmpty extends SingleDatabaseTestCase {
+
+    /**
+     * Check the assembly_exception table.
+     */
+    public DNAEmpty() {
+        addToGroup("post_genebuild");
+        addToGroup("release");
+        setDescription("Check that dna table is empty");
+    }
+
+    /**
+     * Check the data in the assembly_exception table. Note referential
+     * integrity checks are done in CoreForeignKeys.
+     * 
+     * @param dbre
+     *          The database to use.
+     * @return Result.
+     */
+    public boolean run(DatabaseRegistryEntry dbre) {
+
+        boolean result = true;
+
+        Connection con = dbre.getConnection();
+
+        // check that seq_region_end > seq_region_start
+        int rows = getRowCount(con, "SELECT COUNT(*) FROM dna");
+        if (rows > 0) {
+            result = false;
+            ReportManager.problem(this, con, "dna table contains " + rows
+                    + " - it should be empty");
+        }
+
+
+        if (result) {
+            ReportManager.correct(this, con, "dna table is empty");
+        }
+
+        return result;
+
+    }
+
+    /**
+     * This applies to all core schema databases apart from 'core'
+     */
+    public void types() {
+
+        removeAppliesToType(DatabaseType.CORE);
+
+    }
+
+
+} // DNAEmpty
