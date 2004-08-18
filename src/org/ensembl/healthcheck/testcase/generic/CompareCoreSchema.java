652dbab736d5355035445b0e975acc5cdf5fe048
Glenn Proctor <gp1@sanger.ac.uk>
2004-08-18 07:27:58 +0000

    Organised imports

--- src/org/ensembl/healthcheck/testcase/generic/CompareCoreSchema.java
+++ src/org/ensembl/healthcheck/testcase/generic/CompareCoreSchema.java
@@ -15,16 +15,12 @@ package org.ensembl.healthcheck.testcase.generic;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
-
-import java.sql.ResultSetMetaData;
 
 import org.ensembl.healthcheck.DatabaseRegistry;
 import org.ensembl.healthcheck.DatabaseRegistryEntry;
