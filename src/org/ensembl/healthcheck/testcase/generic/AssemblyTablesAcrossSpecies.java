555cc623fdb65eef44464d800cf0d22191fd10ec
Glenn Proctor <gp1@sanger.ac.uk>
2004-11-11 14:14:59 +0000

    Added EST and ESTGENE to types of database to check since these seem to have assembly tables in the latest releases.

--- src/org/ensembl/healthcheck/testcase/generic/AssemblyTablesAcrossSpecies.java
+++ src/org/ensembl/healthcheck/testcase/generic/AssemblyTablesAcrossSpecies.java
@@ -25,7 +25,7 @@ import org.ensembl.healthcheck.testcase.MultiDatabaseTestCase;
  */
 public class AssemblyTablesAcrossSpecies extends MultiDatabaseTestCase {
 
-    private DatabaseType[] types = {DatabaseType.CORE, DatabaseType.VEGA};
+    private DatabaseType[] types = {DatabaseType.CORE, DatabaseType.EST, DatabaseType.ESTGENE, DatabaseType.VEGA};
 
     /**
      * Creates a new instance of AssemblyTablesAcrossSpecies
