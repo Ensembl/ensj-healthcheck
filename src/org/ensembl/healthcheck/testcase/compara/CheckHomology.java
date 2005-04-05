7b4fcbe2a6e27bd1fd172b554edb1234c5033522
Jessica Severin <jessica@sanger.ac.uk>
2005-04-05 10:08:49 +0000

    added checks for valid threshold_on_ds values.  Fails if user configured system to run dn/ds runs between species which are too distant for the data to be useful.

---
+++ src/org/ensembl/healthcheck/testcase/compara/CheckHomology.java
@@ -0,0 +1,69 @@
+/*
+ Copyright (C) 2004 EBI, GRL
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.ensembl.healthcheck.testcase.compara;
+
+import java.sql.Connection;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+
+/**
+ * An EnsEMBL Healthcheck test case that looks for broken foreign-key
+ * relationships.
+ */
+
+public class CheckHomology extends SingleDatabaseTestCase {
+
+    /**
+     * Create an CheckHomology that applies to a specific set of databases.
+     */
+    public CheckHomology() {
+
+        addToGroup("compara_db_constraints");
+        setDescription("Check for broken foreign-key relationships in ensembl_compara databases.");
+
+    }
+
+    /**
+     * Run the test.
+     * 
+     * @param dbre
+     *          The database to use.
+     * @return true if the test passed.
+     *  
+     */
+    public boolean run(DatabaseRegistryEntry dbre) {
+
+        boolean result = true;
+
+        Connection con = dbre.getConnection();
+
+        if (tableHasRows(con, "homology") && tableHasRows(con, "homology")) {
+            result &= checkCountIsZero(con,"homology","threshold_on_ds=0.0");
+            result &= checkCountIsZero(con,"homology","threshold_on_ds>2.0");
+        } else {
+            ReportManager.problem(this, con, "NO ENTRIES in homology or homology_member tables");
+        }
+
+        return result;
+
+    }
+
+} // CheckHomology
