6f2b662da8d7ded1d6a10d9cd74dded1b85e2ec1
Vivek Iyer <vvi@sanger.ac.uk>
2005-04-07 22:27:50 +0000

    Blind copy-paste-edit of DuplicateExons logic. This is quite useful for me (though completely redundant to the exon test) - it just saves a lot of time when working out what to fix at the last minute. Sharing the love...

---
+++ src/org/ensembl/healthcheck/testcase/generic/DuplicateGenes.java
@@ -0,0 +1,134 @@
+/*
+ Copyright (C) 2004 EBI, GRL
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.DatabaseType;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+
+/**
+ * An EnsEMBL Healthcheck test case which checks if any genes
+ * are obvious duplicates of each other (it might be all OK,
+ * but it's worth a look!)
+ */
+
+public class DuplicateGenes extends SingleDatabaseTestCase {
+
+    private static final int MAX_WARNINGS = 100000;
+
+    /**
+     * Create an OrphanTestCase that applies to a specific set of databases.
+     */
+    public DuplicateGenes() {
+
+        addToGroup("post_genebuild");
+        addToGroup("release");
+
+    }
+    
+    /**
+     * This test only applies to core and Vega databases.
+     */
+    public void types() {
+
+        removeAppliesToType(DatabaseType.EST);
+
+    }
+    
+    /**
+     * Check for (strongly likely to be) duplicate genes.
+     * 
+     * @param dbre
+     *          The database to check.
+     * @return True if the test passes.
+     */
+
+    public boolean run(DatabaseRegistryEntry dbre) {
+
+        boolean result = true;
+
+        String sql = "SELECT g.gene_id, g.seq_region_start AS start, g.seq_region_end AS end, g.seq_region_id AS chromosome_id, g.seq_region_strand AS strand "
+                + "             FROM gene g ORDER BY chromosome_id, strand, start, end";
+
+        Connection con = dbre.getConnection();
+        try {
+            
+            Statement stmt = con.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
+                    java.sql.ResultSet.CONCUR_READ_ONLY);
+            stmt.setFetchSize(1000);
+            ResultSet rs = stmt.executeQuery(sql);
+
+            int geneStart, geneEnd, geneChromosome, geneId, geneStrand;
+            int lastGeneId=0;
+            int lastGeneStart = -1;
+            int lastGeneEnd = -1;
+            int lastGeneChromosome = -1;
+            int lastGeneStrand = -1;
+            int duplicateGene = 0;
+
+            boolean first = true;
+
+            while (rs.next()) {
+
+                // load the vars
+                geneId = rs.getInt(1);
+                geneStart = rs.getInt(2);
+                geneEnd = rs.getInt(3);
+                geneChromosome = rs.getInt(4);
+                geneStrand = rs.getInt(5);
+
+                if (!first) {
+                    if (lastGeneChromosome == geneChromosome && lastGeneStart == geneStart && lastGeneEnd == geneEnd && lastGeneStrand == geneStrand) {
+                        duplicateGene++;
+                        ReportManager.warning(this, con, "Gene " + geneId + " is duplicated - see gene "+lastGeneId);
+                    }
+                } else {
+                    first = false;
+                }
+
+                lastGeneId = geneId;
+                lastGeneStart = geneStart;
+                lastGeneEnd = geneEnd;
+                lastGeneChromosome = geneChromosome;
+                lastGeneStrand = geneStrand;
+                
+            } // while rs
+
+            if (duplicateGene > 0) {
+                ReportManager.problem(this, con, "Has " + duplicateGene + " duplicated genes.");
+                result = false;
+            }
+            rs.close();
+            stmt.close();
+
+        } catch (Exception e) {
+            result = false;
+            e.printStackTrace();
+        }
+
+        return result;
+
+    }
+
+} // DuplicateGenes
