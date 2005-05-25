53968747f0a6dc7b0563050893f03386cdb7e8a3
Craig Melsopp <craig@sanger.ac.uk>
2005-05-25 14:07:22 +0000

    Fixed package statement.
    Organised imports.

--- src/org/ensembl/healthcheck/testcase/xref/ForeignKeys.java
+++ src/org/ensembl/healthcheck/testcase/xref/ForeignKeys.java
@@ -15,14 +15,13 @@
  License along with this library; if not, write to the Free Software
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package org.ensembl.healthcheck.testcase.generic;
+package org.ensembl.healthcheck.testcase.xref;
 
 import java.sql.Connection;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.ensembl.healthcheck.DatabaseRegistryEntry;
-import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.DatabaseType;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
