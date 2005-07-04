727272fc6f664b5fc5710d347bb15a81eff5ff6a
Jan-hinnerk Vogel <jhv@sanger.ac.uk>
2005-07-04 12:58:14 +0000

    checking for regulatory_motif removed since column does not exist anymore
    // checkForOrphans(con, "regulatory_feature", "regulatory_motif_id", "regulatory_motif", "regulatory_motif_id", true);

--- src/org/ensembl/healthcheck/testcase/generic/CoreForeignKeys.java
+++ src/org/ensembl/healthcheck/testcase/generic/CoreForeignKeys.java
@@ -98,8 +98,6 @@ public class CoreForeignKeys extends SingleDatabaseTestCase {
 	
 	result &= checkForOrphans(con, "transcript_attrib", "transcript_id", "transcript", "transcript_id", true);
 
-	result &= checkForOrphans(con, "regulatory_feature", "regulatory_motif_id", "regulatory_motif", "regulatory_motif_id", true);
-
 	result &= checkForOrphans(con, "regulatory_feature", "regulatory_feature_id", "regulatory_feature_object", "regulatory_feature_id", true);
 
 	// ----------------------------
