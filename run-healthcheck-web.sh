89193e19b028dfb0bd403214b913b98a8425f466
Glenn Proctor <gp1@sanger.ac.uk>
2005-08-03 07:09:18 +0000

    Explicitly set variables to make it easier to run under cron.

--- run-healthcheck-web.sh
+++ run-healthcheck-web.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-/usr/opt/java142/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.15-ga-bin.jar" org.ensembl.healthcheck.WebTestRunner $*
+home=/nfs/acari/gp1/work/ensj-healthcheck
+java=/usr/opt/java141/bin/java
+
+cp=$home
+cp=$cp:$home/lib/ensj-healthcheck.jar
+cp=$cp:$home/lib/mysql-connector-java-3.0.15-ga-bin.jar
+
+cd $home
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $*
