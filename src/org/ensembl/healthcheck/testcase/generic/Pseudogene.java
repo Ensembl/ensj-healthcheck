843ea531bc2d4765cbf8c9c8b0a4342389da1412
Glenn Proctor <gp1@sanger.ac.uk>
2005-08-19 12:46:26 +0000

    Corrected SQL

--- src/org/ensembl/healthcheck/testcase/generic/Pseudogene.java
+++ src/org/ensembl/healthcheck/testcase/generic/Pseudogene.java
@@ -1,19 +1,14 @@
 /*
- Copyright (C) 2004 EBI, GRL
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
- 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
- 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Copyright (C) 2004 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 package org.ensembl.healthcheck.testcase.generic;
@@ -43,8 +38,7 @@ public class Pseudogene extends SingleDatabaseTestCase {
     /**
      * Check the data in the assembly_exception table. Note referential integrity checks are done in CoreForeignKeys.
      * 
-     * @param dbre
-     *            The database to use.
+     * @param dbre The database to use.
      * @return Result.
      */
     public boolean run(DatabaseRegistryEntry dbre) {
@@ -53,14 +47,13 @@ public class Pseudogene extends SingleDatabaseTestCase {
 
         Connection con = dbre.getConnection();
 
-        String qry = "select translation.* from gene,transcript,translation " + "where gene.biotype like '%pseudogene%'"
+        String qry = "select count(*) from gene,transcript,translation " + "where gene.biotype like '%pseudogene%'"
                 + " and transcript.gene_id=gene.gene_id " + " and translation.transcript_id=transcript.transcript_id";
 
         int rows = getRowCount(con, qry);
         if (rows > 0) {
             result = false;
-            ReportManager.problem(this, con, "Translation table contains " + rows
-                    + " rows for pseudogene types - should contain none");
+            ReportManager.problem(this, con, "Translation table contains " + rows + " rows for pseudogene types - should contain none");
         }
 
         if (result) {
@@ -79,7 +72,7 @@ public class Pseudogene extends SingleDatabaseTestCase {
         removeAppliesToType(DatabaseType.EST);
         removeAppliesToType(DatabaseType.ESTGENE);
         removeAppliesToType(DatabaseType.VEGA);
-        
+
     }
 
 } // Pseudogene
