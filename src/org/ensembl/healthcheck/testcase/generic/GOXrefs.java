2a6ee71a4e4542075646fdbba6fbb2653bb2eeee
Glenn Proctor <gp1@sanger.ac.uk>
2005-10-27 11:05:43 +0000

    Added check for blank or null linkage_types

--- src/org/ensembl/healthcheck/testcase/generic/GOXrefs.java
+++ src/org/ensembl/healthcheck/testcase/generic/GOXrefs.java
@@ -20,91 +20,104 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * Check that GO xrefs exist for certain species.
+ * Check that GO xrefs exist for certain species, and that there are no blank or null linkage types.
  */
 
 public class GOXrefs extends SingleDatabaseTestCase {
 
-	/**
-   * Create a new GOXrefs testcase.
-   */
-	public GOXrefs() {
+    /**
+     * Create a new GOXrefs testcase.
+     */
+    public GOXrefs() {
 
-		addToGroup("post_genebuild");
-		addToGroup("release");
-		addToGroup("core_xrefs");
-		setDescription("Check that GO xrefs exist for certain species (human, mouse, rat, drosophila)");
+	addToGroup("post_genebuild");
+	addToGroup("release");
+	addToGroup("core_xrefs");
+	setDescription("Check that GO xrefs exist for certain species (human, mouse, rat, drosophila)");
 
-	}
+    }
 
-	/**
-   * This only really applies to core databases
-   */
-	public void types() {
+    /**
+     * This only really applies to core databases
+     */
+    public void types() {
 
-		removeAppliesToType(DatabaseType.EST);
-		removeAppliesToType(DatabaseType.ESTGENE);
-		removeAppliesToType(DatabaseType.VEGA);
-		removeAppliesToType(DatabaseType.CDNA);
+	removeAppliesToType(DatabaseType.EST);
+	removeAppliesToType(DatabaseType.ESTGENE);
+	removeAppliesToType(DatabaseType.VEGA);
+	removeAppliesToType(DatabaseType.CDNA);
 
-	}
+    }
 
-	/**
-   * Run the test.
-   * 
-   * @param dbre The database to use.
-   * @return true if the test pased.
-   * 
-   */
-	public boolean run(DatabaseRegistryEntry dbre) {
+    /**
+     * Run the test.
+     * 
+     * @param dbre The database to use.
+     * @return true if the test pased.
+     * 
+     */
+    public boolean run(DatabaseRegistryEntry dbre) {
 
-		boolean result = true;
+	boolean result = true;
 
-		// only check for GO xrefs for human, mouse, rat & drosophila
-		// if (dbre.getSpecies().equals(Species.HOMO_SAPIENS) || dbre.getSpecies().equals(Species.MUS_MUSCULUS) ||
-    // dbre.getSpecies().equals(Species.RATTUS_NORVEGICUS) || dbre.getSpecies().equals(Species.DROSOPHILA_MELANOGASTER)) {
+	// only check for GO xrefs for human, mouse, rat & drosophila
+	// if (dbre.getSpecies().equals(Species.HOMO_SAPIENS) || dbre.getSpecies().equals(Species.MUS_MUSCULUS) ||
+	// dbre.getSpecies().equals(Species.RATTUS_NORVEGICUS) || dbre.getSpecies().equals(Species.DROSOPHILA_MELANOGASTER)) {
 
-		if (true) {
-			Connection con = dbre.getConnection();
+	Connection con = dbre.getConnection();
 
-			// check that they exist in the xref table
-			String sql = "SELECT COUNT(*) FROM external_db edb, xref x WHERE edb.db_name= 'go' AND edb.external_db_id = x.external_db_id";
+	if (true) {
 
-			int xref_rows = getRowCount(con, sql);
-			if (xref_rows == 0) {
+	    // check that they exist in the xref table
+	    String sql = "SELECT COUNT(*) FROM external_db edb, xref x WHERE edb.db_name= 'go' AND edb.external_db_id = x.external_db_id";
 
-				ReportManager.problem(this, con, "No GO xrefs found.");
-				result = false;
+	    int xref_rows = getRowCount(con, sql);
+	    if (xref_rows == 0) {
 
-			} else {
+		ReportManager.problem(this, con, "No GO xrefs found.");
+		result = false;
 
-				ReportManager.correct(this, con, "Found " + xref_rows + " GO xrefs");
+	    } else {
 
-				// if GO xrefs exist, check that the go_xref table is populated
-				int go_xref_rows = getRowCount(con, "SELECT COUNT(*) FROM go_xref");
-				if (go_xref_rows == 0) {
+		ReportManager.correct(this, con, "Found " + xref_rows + " GO xrefs");
+
+		// if GO xrefs exist, check that the go_xref table is populated
+		int go_xref_rows = getRowCount(con, "SELECT COUNT(*) FROM go_xref");
+		if (go_xref_rows == 0) {
 					
-					ReportManager.problem(this, con, "Found " + xref_rows + " GO xrefs in xref table but go_xref table is empty");
-					result = false;
+		    ReportManager.problem(this, con, "Found " + xref_rows + " GO xrefs in xref table but go_xref table is empty");
+		    result = false;
 					
-				} else {
-					
-					ReportManager.correct(this, con, "go_xref table has " + go_xref_rows + " rows");
-					
-				}
-			}
-			
-			
-
 		} else {
-
-			logger.info("Not checking for GO xrefs in " + dbre.getSpecies());
-			return true;
-
+					
+		    ReportManager.correct(this, con, "go_xref table has " + go_xref_rows + " rows");
+					
 		}
+	    }
+			
+			
 
-		return result;
+	} else {
 
-	} // run
+	    logger.info("Not checking for GO xrefs in " + dbre.getSpecies());
+	    return true;
+
+	}
+
+	// check for blank or null linkage_type
+	int blank = getRowCount(con, "SELECT COUNT(*) FROM go_xref WHERE linkage_type IS NULL OR linkage_type=''");
+	if (blank > 0) {
+		    
+	    ReportManager.problem(this, con, blank + " rows in go_xref have null or blank ('') linkage_type");
+	    result = false;
+
+	} else {
+
+	    ReportManager.correct(this, con, "No blank or null linkage_types in go_xref");
+	}
+
+	return result;
+
+    } // run
 
 } // GOXrefs
