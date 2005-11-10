2decf166355376c155e3b98a1e77b2fb3eff3c74
Glenn Proctor <gp1@sanger.ac.uk>
2005-11-10 13:05:32 +0000

    Created; checks that analyses used in gene and prediction_transcript all have analysis descriptions.

---
+++ src/org/ensembl/healthcheck/testcase/generic/AnalysisDescription.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2003 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+
+/**
+ * Check that all of certain types of objects have analysis_descriptions.
+ */
+
+public class AnalysisDescription extends SingleDatabaseTestCase {
+
+	String[] types = { "gene", "prediction_transcript" }; // add more here - must have analysis_id
+
+	/**
+   * Create a new AnalysisDescription testcase.
+   */
+	public AnalysisDescription() {
+
+		addToGroup("post_genebuild");
+		addToGroup("release");
+		setDescription("Check that all of certain types of objects have analysis_descriptions.");
+
+	}
+
+	/**
+   * Run the test.
+   * 
+   * @param dbre The database to use.
+   * @return true if the test pased.
+   * 
+   */
+	public boolean run(DatabaseRegistryEntry dbre) {
+
+		boolean result = true;
+
+		Connection con = dbre.getConnection();
+
+		// cache logic_names by analysis_id
+		Map logicNamesByAnalID = new HashMap();
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT analysis_id, logic_name FROM analysis");
+			while (rs.next()) {
+				logicNamesByAnalID.put(rs.getString("analysis_id"), rs.getString("logic_name"));
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		for (int i = 0; i < types.length; i++) {
+
+			// get analyses that are used
+			String[] analyses = getColumnValues(con, "SELECT DISTINCT(analysis_id) FROM " + types[i]);
+
+			// check each one has an analysis_description
+			for (int j = 0; j < analyses.length; j++) {
+				int count = getRowCount(con, "SELECT COUNT(*) FROM analysis_description WHERE analysis_id=" + analyses[j]);
+				if (count == 0) {
+					ReportManager.problem(this, con, "Analysis " + logicNamesByAnalID.get(analyses[j]) + " is used in " + types[i]
+							+ " but has no entry in analysis_description");
+					result = false;
+				} else {
+					ReportManager.correct(this, con, "Analysis " + logicNamesByAnalID.get(analyses[j]) + " is used in " + types[i]
+							+ " and has an entry in analysis_description");
+				}
+
+			}
+		}
+
+		return result;
+
+	} // run
+
+} // AnalysisDescription
