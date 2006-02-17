b240f3e1cdad0ac61097af6d3b6ea662acdcacaa
Glenn Proctor <gp1@sanger.ac.uk>
2006-02-17 18:57:57 +0000

    Check that the number of KNOWN genes is within 20% in the new and previous databases.

--- src/org/ensembl/healthcheck/testcase/generic/GeneStatus.java
+++ src/org/ensembl/healthcheck/testcase/generic/GeneStatus.java
@@ -20,19 +20,21 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * Check that there are at least some KNOWN genes.
+ * Check that the number of KNOWN & NOVEL genes is within 20% in the new and previous databases.
  */
 
 public class GeneStatus extends SingleDatabaseTestCase {
+
+	// fraction of KNOWN & NOVEL genes that are allowed to be different
+	private static double THRESHOLD = 0.2;
 
 	/**
 	 * Create a new GeneStatus testcase.
 	 */
 	public GeneStatus() {
 
-		addToGroup("post_genebuild");
 		addToGroup("release");
-		setDescription("Check that at least some genes have KNOWN status.");
+		setDescription("Check that the number of KNOWN genes is within 20% in the new and previous databases");
 
 	}
 
@@ -59,19 +61,58 @@ public class GeneStatus extends SingleDatabaseTestCase {
 
 		Connection con = dbre.getConnection();
 
-		int rows = getRowCount(con, "SELECT COUNT(*) FROM gene WHERE status=\'KNOWN\'");
+		DatabaseRegistryEntry sec = getEquivalentFromSecondaryServer(dbre);
 
-		if (rows == 0) {
-
-			ReportManager.problem(this, con, "No genes have KNOWN status");
-			result = false;
-
-		} else {
-
-			ReportManager.correct(this, con, rows + " genes have KNOWN status");
-
+		if (sec == null) {
+			logger.warning("Can't get equivalent database for " + dbre.getName());
+			return true;
 		}
 
+		logger.finest("Equivalent database on secondary server is " + sec.getName());
+
+		//String[] stats = { "KNOWN", "NOVEL" };
+		String[] stats = { "KNOWN" };
+
+		for (int i = 0; i < stats.length; i++) {
+
+			String status = stats[i];
+
+			String sql = "SELECT COUNT(*) FROM gene WHERE status='" + status + "'";
+			int current = getRowCount(dbre.getConnection(), sql);
+			int previous = getRowCount(sec.getConnection(), sql);
+
+			// if there are no KNOWN genes at all, fail
+			if (status.equals("KNOWN") && current == 0) {
+
+				ReportManager.problem(this, con, "No genes have status " + status);
+				return false;
+
+			}
+
+			// otherwise check ratios
+			if (previous == 0) { // avoid division by zero
+			
+				ReportManager.warning(this, con, "Previous count of " + status + " genes is 0, skipping");
+				return false;
+				
+			}
+			
+			double difference = (double)(previous - current) / (double)previous;
+
+			//System.out.println(previous + " " + current + " " + difference);
+			
+			if (difference > THRESHOLD) {
+
+				ReportManager.problem(this, con, "Only " + current + " genes have " + status
+						+ " status in the current database, compared with " + previous + " in the previous database");
+				result = false;
+
+			}
+
+			ReportManager.correct(this, con, "Current database has " + current + " genes of status " + status + " compared to " + previous + " in the previous database, which is within the allowed tollerance.");
+			
+		}
+	
 		return result;
 
 	} // run
