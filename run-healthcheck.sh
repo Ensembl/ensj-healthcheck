a5cffa72159ab2280344e0a7cf137e440f6a9f41
Craig Melsopp <craig@sanger.ac.uk>
2006-03-09 13:15:33 +0000

    Now runs from any directory, no need to "cd ensj-healthcheck".
    Only minor inconvenience is that if you specify "-config FILE" the
    file path must be absolute e.g. "-config `pwd`/FILE".

--- run-healthcheck.sh
+++ run-healthcheck.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-/usr/opt/java142/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.15-ga-bin.jar" org.ensembl.healthcheck.TextTestRunner $*
+# Usage note: If you use -config FILE you must fully specify the path to FILE otherwise the program won't find the file.
+# e.g. ~/dev/ensj-healthcheck/run-healthcheck.sh  -config `pwd`/db.properties -d my_database SOME_TEST
+
+home=`dirname $0`
+java=/usr/opt/java/bin/java
+
+cp=$home
+cp=$cp:$home/build/classes
+cp=$cp:$home/lib/ensj-healthcheck.jar
+cp=$cp:$home/lib/mysql-connector-java-3.0.15-ga-bin.jar
+
+$java -Duser.dir=$home -cp $cp  org.ensembl.healthcheck.TextTestRunner $*
+
