7ad81f6f5f0ee4777c73ccf177f2f74497375231
Glenn Proctor <gp1@sanger.ac.uk>
2006-03-15 14:29:21 +0000

    Ignore regulatory_feature-regulatory_feature_object orphans for drosophila only to prevent lots of false positives - features in drosophila are predictions of sites only.

--- src/org/ensembl/healthcheck/testcase/generic/CoreForeignKeys.java
+++ src/org/ensembl/healthcheck/testcase/generic/CoreForeignKeys.java
@@ -17,166 +17,178 @@ import java.sql.Connection;
 
 import org.ensembl.healthcheck.DatabaseRegistryEntry;
 import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.Species;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * An EnsEMBL Healthcheck test case that looks for broken foreign-key realtionships.
+ * An EnsEMBL Healthcheck test case that looks for broken foreign-key
+ * realtionships.
  */
 
 public class CoreForeignKeys extends SingleDatabaseTestCase {
 
-    /**
-     * Create an OrphanTestCase that applies to a specific set of databases.
-     */
-    public CoreForeignKeys() {
+	/**
+	 * Create an OrphanTestCase that applies to a specific set of databases.
+	 */
+	public CoreForeignKeys() {
 
-        addToGroup("post_genebuild");
-        addToGroup("release");
-        setDescription("Check for broken foreign-key relationships.");
-    }
+		addToGroup("post_genebuild");
+		addToGroup("release");
+		setDescription("Check for broken foreign-key relationships.");
+	}
 
-    /**
-     * Look for broken foreign key realtionships.
-     * 
-     * @param dbre The database to use.
-     * @return true Ff all foreign key relationships are valid.
-     */
-    public boolean run(DatabaseRegistryEntry dbre) {
+	/**
+	 * Look for broken foreign key realtionships.
+	 * 
+	 * @param dbre
+	 *          The database to use.
+	 * @return true Ff all foreign key relationships are valid.
+	 */
+	public boolean run(DatabaseRegistryEntry dbre) {
 
-        boolean result = true;
+		boolean result = true;
 
-        Connection con = dbre.getConnection();
+		Connection con = dbre.getConnection();
 
-        // ----------------------------
+		// ----------------------------
 
-        result &= checkForOrphans(con, "exon", "exon_id", "exon_transcript", "exon_id", false);
+		result &= checkForOrphans(con, "exon", "exon_id", "exon_transcript", "exon_id", false);
 
-        result &= checkForOrphans(con, "transcript", "transcript_id", "exon_transcript", "transcript_id", false);
+		result &= checkForOrphans(con, "transcript", "transcript_id", "exon_transcript", "transcript_id", false);
 
-        result &= checkForOrphans(con, "gene", "gene_id", "transcript", "gene_id", false);
+		result &= checkForOrphans(con, "gene", "gene_id", "transcript", "gene_id", false);
 
-        result &= checkForOrphans(con, "object_xref", "xref_id", "xref", "xref_id", true);
+		result &= checkForOrphans(con, "object_xref", "xref_id", "xref", "xref_id", true);
 
-        result &= checkForOrphans(con, "xref", "external_db_id", "external_db", "external_db_id", true);
+		result &= checkForOrphans(con, "xref", "external_db_id", "external_db", "external_db_id", true);
 
-        result &= checkForOrphans(con, "dna", "seq_region_id", "seq_region", "seq_region_id", true);
+		result &= checkForOrphans(con, "dna", "seq_region_id", "seq_region", "seq_region_id", true);
 
-        result &= checkForOrphans(con, "seq_region", "coord_system_id", "coord_system", "coord_system_id", true);
+		result &= checkForOrphans(con, "seq_region", "coord_system_id", "coord_system", "coord_system_id", true);
 
-        result &= checkForOrphans(con, "assembly", "cmp_seq_region_id", "seq_region", "seq_region_id", true);
+		result &= checkForOrphans(con, "assembly", "cmp_seq_region_id", "seq_region", "seq_region_id", true);
 
-        result &= checkForOrphans(con, "marker_feature", "marker_id", "marker", "marker_id", true);
+		result &= checkForOrphans(con, "marker_feature", "marker_id", "marker", "marker_id", true);
 
-        result &= checkForOrphans(con, "seq_region_attrib", "seq_region_id", "seq_region", "seq_region_id", true);
+		result &= checkForOrphans(con, "seq_region_attrib", "seq_region_id", "seq_region", "seq_region_id", true);
 
-        result &= checkForOrphans(con, "seq_region_attrib", "attrib_type_id", "attrib_type", "attrib_type_id", true);
+		result &= checkForOrphans(con, "seq_region_attrib", "attrib_type_id", "attrib_type", "attrib_type_id", true);
 
-        result &= checkForOrphans(con, "misc_feature_misc_set", "misc_feature_id", "misc_feature", "misc_feature_id", true);
+		result &= checkForOrphans(con, "misc_feature_misc_set", "misc_feature_id", "misc_feature", "misc_feature_id", true);
 
-        result &= checkForOrphans(con, "misc_feature_misc_set", "misc_set_id", "misc_set", "misc_set_id", true);
+		result &= checkForOrphans(con, "misc_feature_misc_set", "misc_set_id", "misc_set", "misc_set_id", true);
 
-        result &= checkForOrphans(con, "misc_feature", "misc_feature_id", "misc_attrib", "misc_feature_id", true);
+		result &= checkForOrphans(con, "misc_feature", "misc_feature_id", "misc_attrib", "misc_feature_id", true);
 
-        result &= checkForOrphans(con, "misc_attrib", "attrib_type_id", "attrib_type", "attrib_type_id", true);
+		result &= checkForOrphans(con, "misc_attrib", "attrib_type_id", "attrib_type", "attrib_type_id", true);
 
-        result &= checkForOrphans(con, "assembly_exception", "seq_region_id", "seq_region", "seq_region_id", true);
+		result &= checkForOrphans(con, "assembly_exception", "seq_region_id", "seq_region", "seq_region_id", true);
 
-        result &= checkForOrphans(con, "assembly_exception", "exc_seq_region_id", "seq_region", "seq_region_id", true);
+		result &= checkForOrphans(con, "assembly_exception", "exc_seq_region_id", "seq_region", "seq_region_id", true);
 
-        result &= checkForOrphans(con, "protein_feature", "translation_id", "translation", "translation_id", true);
+		result &= checkForOrphans(con, "protein_feature", "translation_id", "translation", "translation_id", true);
 
-        result &= checkForOrphans(con, "marker_synonym", "marker_id", "marker", "marker_id", true);
+		result &= checkForOrphans(con, "marker_synonym", "marker_id", "marker", "marker_id", true);
 
-        result &= checkForOrphans(con, "translation_attrib", "translation_id", "translation", "translation_id", true);
+		result &= checkForOrphans(con, "translation_attrib", "translation_id", "translation", "translation_id", true);
 
-        result &= checkForOrphans(con, "transcript_attrib", "transcript_id", "transcript", "transcript_id", true);
+		result &= checkForOrphans(con, "transcript_attrib", "transcript_id", "transcript", "transcript_id", true);
 
-        result &= checkForOrphans(con, "regulatory_feature", "regulatory_feature_id", "regulatory_feature_object", "regulatory_feature_id", true);
+		// currently ignore this for drosophila as features there are only predicted
+		// sites
+		if (!dbre.getSpecies().equals(Species.DROSOPHILA_MELANOGASTER)) {
+			result &= checkForOrphans(con, "regulatory_feature", "regulatory_feature_id", "regulatory_feature_object",
+					"regulatory_feature_id", true);
+		}
 
-        //result &= checkForOrphans(con, "regulatory_factor_coding", "transcript_id", "transcript", "transcript_id", true);
+		// result &= checkForOrphans(con, "regulatory_factor_coding",
+		// "transcript_id", "transcript", "transcript_id", true);
 
-        //result &= checkForOrphans(con, "regulatory_factor_coding", "gene_id", "gene", "gene_id", true);
+		// result &= checkForOrphans(con, "regulatory_factor_coding", "gene_id",
+		// "gene", "gene_id", true);
 
-        result &= checkForOrphans(con, "regulatory_factor_coding", "regulatory_factor_id", "regulatory_factor", "regulatory_factor_id", true);
+		result &= checkForOrphans(con, "regulatory_factor_coding", "regulatory_factor_id", "regulatory_factor", "regulatory_factor_id",
+				true);
 
-        result &= checkForOrphans(con, "regulatory_feature", "regulatory_factor_id", "regulatory_factor", "regulatory_factor_id", true);
+		result &= checkForOrphans(con, "regulatory_feature", "regulatory_factor_id", "regulatory_factor", "regulatory_factor_id", true);
 
-        result &= checkForOrphans(con, "regulatory_feature", "analysis_id", "analysis", "analysis_id", true);
+		result &= checkForOrphans(con, "regulatory_feature", "analysis_id", "analysis", "analysis_id", true);
 
-        result &= checkForOrphans(con, "external_synonym", "xref_id", "xref", "xref_id", true);
-        
-        result &= checkForOrphans(con, "identity_xref", "object_xref_id", "object_xref", "object_xref_id", true);
-        
-        result &= checkForOrphans(con, "supporting_feature", "exon_id", "exon", "exon_id", true);
-        
-        result &= checkForOrphans(con, "translation", "transcript_id", "transcript", "transcript_id", true);
-        
-        // ----------------------------
-        // Check regulatory features point to existing objects
-        String[] rfTypes = { "Gene", "Transcript", "Translation" };
-        for (int i = 0; i < rfTypes.length; i++) {
-            result &= checkRegulatoryFeatureKeys(con, rfTypes[i]);
-        }
+		result &= checkForOrphans(con, "external_synonym", "xref_id", "xref", "xref_id", true);
 
-        // ----------------------------
-        // Check stable IDs all correspond to an existing object
-        String[] stableIDtypes = { "gene", "transcript", "translation", "exon" };
-        for (int i = 0; i < stableIDtypes.length; i++) {
-            result &= checkStableIDKeys(con, stableIDtypes[i]);
-        }
+		result &= checkForOrphans(con, "identity_xref", "object_xref_id", "object_xref", "object_xref_id", true);
 
-        // ----------------------------
-        // Ensure that feature tables reference existing seq_regions
+		result &= checkForOrphans(con, "supporting_feature", "exon_id", "exon", "exon_id", true);
 
-        String[] featTabs = getCoreFeatureTables();
+		result &= checkForOrphans(con, "translation", "transcript_id", "transcript", "transcript_id", true);
 
-        for (int i = 0; i < featTabs.length; i++) {
-            String featTab = featTabs[i];
-            // skip large tables as this test takes an inordinately long time
-            if (featTab.equals("protein_align_feature") || featTab.equals("dna_align_feature") || featTab.equals("repeat_feature")) {
-                continue;
-            }
-            result &= checkForOrphans(con, featTab, "seq_region_id", "seq_region", "seq_region_id", true);
-        }
+		// ----------------------------
+		// Check regulatory features point to existing objects
+		String[] rfTypes = { "Gene", "Transcript", "Translation" };
+		for (int i = 0; i < rfTypes.length; i++) {
+			result &= checkRegulatoryFeatureKeys(con, rfTypes[i]);
+		}
 
-        return result;
+		// ----------------------------
+		// Check stable IDs all correspond to an existing object
+		String[] stableIDtypes = { "gene", "transcript", "translation", "exon" };
+		for (int i = 0; i < stableIDtypes.length; i++) {
+			result &= checkStableIDKeys(con, stableIDtypes[i]);
+		}
 
-    }
+		// ----------------------------
+		// Ensure that feature tables reference existing seq_regions
 
-    // -------------------------------------------------------------------------
-    private boolean checkStableIDKeys(Connection con, String type) {
+		String[] featTabs = getCoreFeatureTables();
 
-        if (tableHasRows(con, type + "_stable_id")) {
+		for (int i = 0; i < featTabs.length; i++) {
+			String featTab = featTabs[i];
+			// skip large tables as this test takes an inordinately long time
+			if (featTab.equals("protein_align_feature") || featTab.equals("dna_align_feature") || featTab.equals("repeat_feature")) {
+				continue;
+			}
+			result &= checkForOrphans(con, featTab, "seq_region_id", "seq_region", "seq_region_id", true);
+		}
 
-            return checkForOrphans(con, type, type + "_id", type + "_stable_id", type + "_id", false);
+		return result;
 
-        }
+	}
 
-        return true;
+	// -------------------------------------------------------------------------
+	private boolean checkStableIDKeys(Connection con, String type) {
 
-    } // checkStableIDKeys
+		if (tableHasRows(con, type + "_stable_id")) {
 
-    // -------------------------------------------------------------------------
-    private boolean checkRegulatoryFeatureKeys(Connection con, String type) {
+			return checkForOrphans(con, type, type + "_id", type + "_stable_id", type + "_id", false);
 
-        String table = type.toLowerCase();
+		}
 
-        int rows = getRowCount(con, "SELECT COUNT(*) FROM regulatory_feature_object rfo LEFT JOIN " + table + " ON rfo.ensembl_object_id=" + table
-                + "." + table + "_id WHERE rfo.ensembl_object_type=\'" + type + "\' AND rfo.ensembl_object_id IS NULL");
+		return true;
 
-        if (rows > 0) {
+	} // checkStableIDKeys
 
-            ReportManager.problem(this, con, rows + " in regulatory_feature refer to non-existent " + table + "s");
-            return false;
+	// -------------------------------------------------------------------------
+	private boolean checkRegulatoryFeatureKeys(Connection con, String type) {
 
-        } else {
+		String table = type.toLowerCase();
 
-            ReportManager.correct(this, con, "All rows in regulatory_feature refer to valid " + table + "s");
-            return true;
-        }
+		int rows = getRowCount(con, "SELECT COUNT(*) FROM regulatory_feature_object rfo LEFT JOIN " + table
+				+ " ON rfo.ensembl_object_id=" + table + "." + table + "_id WHERE rfo.ensembl_object_type=\'" + type
+				+ "\' AND rfo.ensembl_object_id IS NULL");
 
-    } // checkRegulatoryFeatureKeys
+		if (rows > 0) {
 
-    // -------------------------------------------------------------------------
+			ReportManager.problem(this, con, rows + " in regulatory_feature refer to non-existent " + table + "s");
+			return false;
+
+		} else {
+
+			ReportManager.correct(this, con, "All rows in regulatory_feature refer to valid " + table + "s");
+			return true;
+		}
+
+	} // checkRegulatoryFeatureKeys
+
+	// -------------------------------------------------------------------------
 
 } // CoreForeignKeys
