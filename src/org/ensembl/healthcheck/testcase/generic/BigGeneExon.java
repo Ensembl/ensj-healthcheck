f3e604e68bee689fe848d5e8a1f54e99160ac25b
Glenn Proctor <gp1@sanger.ac.uk>
2006-04-07 17:04:05 +0000

    Add status to report.

--- src/org/ensembl/healthcheck/testcase/generic/BigGeneExon.java
+++ src/org/ensembl/healthcheck/testcase/generic/BigGeneExon.java
@@ -142,11 +142,13 @@ public class BigGeneExon extends SingleDatabaseTestCase {
 			String stableID = getRowColumnValue(con, "SELECT stable_id FROM gene_stable_id WHERE gene_id=" + id);
 			String name = getRowColumnValue(con, "SELECT x.display_label FROM gene g, xref x WHERE x.xref_id=g.display_xref_id AND g.gene_id=" + id);
 			String description = getRowColumnValue(con, "SELECT description FROM gene WHERE gene_id=" + id);
+			String status = getRowColumnValue(con, "SELECT status FROM gene WHERE gene_id=" + id);
 			
 			String str = "Gene " + stableID;
 			if (name != null && name.length() > 0) {
 				str += " (" + name + ")";
 			}
+			str += " " + status;
 			str += " has length " + length;
 			if (description != null && description.length() > 0) {
 				str += " (" + description + ")";
