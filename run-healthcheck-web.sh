35300fa10ffc22a61246d704ab04cd1c5fce3d70
Glenn Proctor <gp1@sanger.ac.uk>
2006-06-26 11:47:07 +0000

    Add new species.

--- run-healthcheck-web.sh
+++ run-healthcheck-web.sh
@@ -14,6 +14,7 @@ rm -f timings.txt
 
 # comment/uncomment the ones to run
 
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_aedes_aegypti.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_anopheles_gambiae.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_bos_taurus.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_caenorhabditis_elegans.properties
@@ -21,16 +22,23 @@ $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config we
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_ciona_intestinalis.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_ciona_savignyi.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_danio_rerio.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_dasypus_novemcinctus.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_drosophila_melanogaster.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_echinops_telfairi.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_fugu_rubripes.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_gallus_gallus.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_gasterosteus_aculeatus.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_homo_sapiens.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_loxodonta_africana.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_monodelphis_domestica.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_macaca_mulatta.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_mus_musculus.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_oryctolagus_cuniculus.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_oryzias_latipes.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_pan_troglodytes.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_rattus_norvegicus.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_saccharomyces_cerevisiae.properties
+$java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_sus_scrofa.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_tetraodon_nigroviridis.properties
 $java -server -classpath $cp org.ensembl.healthcheck.WebTestRunner $* -config web_xenopus_tropicalis.properties
 
