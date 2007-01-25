11388e02b6675d5a305359c73c5b528269e46fad
Patrick Meidl <pm2@sanger.ac.uk>
2007-01-25 11:03:49 +0000

    adapted path to java binary for farm

--- show-groups.sh
+++ show-groups.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/opt/java142/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.ListAllTests -groups
+/usr/opt/java/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.ListAllTests -groups
