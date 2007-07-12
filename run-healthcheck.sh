1b92bad8c9c52c77a706e1c23bb08cf6cbe6b44c
Patrick Meidl <pm2@sanger.ac.uk>
2007-07-12 08:13:20 +0000

    changed java binary path

--- run-healthcheck.sh
+++ run-healthcheck.sh
@@ -4,7 +4,7 @@
 # e.g. ~/dev/ensj-healthcheck/run-healthcheck.sh  -config `pwd`/db.properties -d my_database SOME_TEST
 
 home=`dirname $0`
-java=/usr/opt/java/bin/java
+java=/software/bin/java
 
 cp=$home
 cp=$cp:$home/build/classes
