157a66dd2c7f49d765d3cc3aef98231180057e6b
Ensembl Account <ensembl@sanger.ac.uk>
2007-07-17 16:49:21 +0000

    Use $JAVA_HOME instead of hardcoding path to JVM

--- run-healthcheck-web.sh
+++ run-healthcheck-web.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-dir=$HOME/ensj-healthcheck
-java=/usr/bin/java
+dir=/nfs/acari/ensembl/ensj-healthcheck
 
 cp=$dir
 cp=$cp:$dir/lib/ensj-healthcheck.jar
@@ -9,6 +8,6 @@ cp=$cp:$dir/lib/mysql-connector-java-3.0.15-ga-bin.jar
 
 cd $dir
 
-$java -server -classpath $cp -Xmx512m org.ensembl.healthcheck.DatabaseTestRunner
+$JAVA_HOME/bin/java -server -classpath $cp -Xmx512m org.ensembl.healthcheck.DatabaseTestRunner
 
 
--- run-healthcheck.sh
+++ run-healthcheck.sh
@@ -4,12 +4,11 @@
 # e.g. ~/dev/ensj-healthcheck/run-healthcheck.sh  -config `pwd`/db.properties -d my_database SOME_TEST
 
 home=`dirname $0`
-java=/software/bin/java
 
 cp=$home
 cp=$cp:$home/build/classes
 cp=$cp:$home/lib/ensj-healthcheck.jar
 cp=$cp:$home/lib/mysql-connector-java-3.0.15-ga-bin.jar
 
-$java -Duser.dir=$home -cp $cp -Xmx1500m org.ensembl.healthcheck.TextTestRunner $*
+$JAVA_HOME/bin/java -Duser.dir=$home -cp $cp -Xmx1500m org.ensembl.healthcheck.TextTestRunner $*
 
