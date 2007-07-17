2713f81d21a35bb2334236b506689d78006e4fa1
Ensembl Account <ensembl@sanger.ac.uk>
2007-07-17 17:03:32 +0000

    More JAVA_HOME fun

--- list-tests-in-group.sh
+++ list-tests-in-group.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/opt/java/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.ListAllTests $*
+$JAvA_HOME/bin/java -classpath "lib/ensj-healthcheck.jar:lib/mysql-connector-java-3.0.8-stable-bin.jar" org.ensembl.healthcheck.ListAllTests $*
