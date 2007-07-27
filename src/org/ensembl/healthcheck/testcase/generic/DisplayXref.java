474ccbba2f0b2035342a9e3d7a6bd9227df1b6c7
Glenn Proctor <gp1@sanger.ac.uk>
2007-07-27 12:07:16 +0000

    Add check for numeric EntrezGenes being used as display_xrefs.

--- src/org/ensembl/healthcheck/testcase/generic/DisplayXref.java
+++ src/org/ensembl/healthcheck/testcase/generic/DisplayXref.java
@@ -20,14 +20,15 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * Check that some display_xrefs are set.
+ * Check that some display_xrefs are set. Also check for numeric EntrezGenes
+ * being used as display xrefs.
  */
 
 public class DisplayXref extends SingleDatabaseTestCase {
 
 	/**
-   * Create a new DisplayXref testcase.
-   */
+	 * Create a new DisplayXref testcase.
+	 */
 	public DisplayXref() {
 
 		addToGroup("post_genebuild");
@@ -38,8 +39,8 @@ public class DisplayXref extends SingleDatabaseTestCase {
 	}
 
 	/**
-   * This only applies to core and Vega databases.
-   */
+	 * This only applies to core and Vega databases.
+	 */
 	public void types() {
 
 		removeAppliesToType(DatabaseType.OTHERFEATURES);
@@ -49,25 +50,28 @@ public class DisplayXref extends SingleDatabaseTestCase {
 	}
 
 	/**
-   * Run the test.
-   * 
-   * @param dbre The database to use.
-   * @return true if the test pased.
-   * 
-   */
+	 * Run the test.
+	 * 
+	 * @param dbre
+	 *          The database to use.
+	 * @return true if the test passed.
+	 * 
+	 */
 	public boolean run(DatabaseRegistryEntry dbre) {
 
 		boolean result = true;
 
 		Connection con = dbre.getConnection();
 
-		// check that some (not necessarily all) genes and transcripts have valid display_xref_ids
-		// TODO - is checking that there is at least 1 OK or should there be a minimum of say 100?
+		// check that some (not necessarily all) genes and transcripts have valid
+		// display_xref_ids
+		// TODO - is checking that there is at least 1 OK or should there be a
+		// minimum of say 100?
 		String[] types = { "gene", "transcript" };
 		for (int i = 0; i < types.length; i++) {
 
 			int total = getRowCount(con, "SELECT COUNT(*) FROM " + types[i]);
-			
+
 			if (getRowCount(con, "SELECT COUNT(*) FROM " + types[i] + " WHERE display_xref_id IS NOT NULL AND display_xref_id > 0") == 0) {
 
 				ReportManager.problem(this, con, "No " + types[i] + "s with valid display_xref_id");
@@ -83,20 +87,34 @@ public class DisplayXref extends SingleDatabaseTestCase {
 				result = false;
 
 			}
-			
+
 			// check for display_xref_ids that don't point to xrefs
-			// can't use countOrphans() here as we need to rule out cases where both are null
-			
-				int orphans = getRowCount(con,
-																	"SELECT COUNT(*) FROM " + types[i] + " LEFT JOIN xref ON " + types[i] + ".display_xref_id=xref.xref_id WHERE " + types[i] + ".display_xref_id IS NOT NULL AND xref.xref_id IS NULL");
+			// can't use countOrphans() here as we need to rule out cases where both
+			// are null
 
-				if (orphans > 0) {
+			int orphans = getRowCount(con, "SELECT COUNT(*) FROM " + types[i] + " LEFT JOIN xref ON " + types[i]
+					+ ".display_xref_id=xref.xref_id WHERE " + types[i] + ".display_xref_id IS NOT NULL AND xref.xref_id IS NULL");
 
-					ReportManager.problem(this, con, orphans + " " + types[i] + "s (out of a total of " + total + ") have display_xref_ids pointing to non-existent xrefs.");
-					result = false;
+			if (orphans > 0) {
 
-				}
-		
+				ReportManager.problem(this, con, orphans + " " + types[i] + "s (out of a total of " + total
+						+ ") have display_xref_ids pointing to non-existent xrefs.");
+				result = false;
+
+			}
+
+		}
+
+		// check for numeric EntrezGenes being used as display_xrefs
+		int numeric = getRowCount(
+				con,
+				"SELECT COUNT(*) FROM gene g, xref x, external_db e WHERE e.external_db_id=x.external_db_id AND g.display_xref_id=x.xref_id AND e.db_name='EntrezGene' AND x.display_label REGEXP '^[0-9]+$'");
+
+		if (numeric > 0) {
+
+			ReportManager.problem(this, con, numeric + " genes have display_xrefs that are from EntrezGene and are numeric");
+			result = false;
+
 		}
 
 		if (result) {
