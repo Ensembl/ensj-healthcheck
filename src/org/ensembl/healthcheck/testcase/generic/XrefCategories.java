0cdc133b406388d59ba5491e8fcd146c22b12ce4
Glenn Proctor <gp1@sanger.ac.uk>
2007-10-26 14:01:36 +0000

    Typo

--- src/org/ensembl/healthcheck/testcase/generic/XrefCategories.java
+++ src/org/ensembl/healthcheck/testcase/generic/XrefCategories.java
@@ -49,7 +49,7 @@ public class XrefCategories extends SingleDatabaseTestCase {
 	 * 
 	 * @param dbre
 	 *          The database to use.
-	 * @return true if the test pased.
+	 * @return true if the test passed.
 	 * 
 	 */
 	public boolean run(DatabaseRegistryEntry dbre) {
