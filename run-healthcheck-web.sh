e0f77e6f8b5a841f5d13cc1fd1b25fbcce936503
Ensembl Account <ensembl@sanger.ac.uk>
2007-10-30 09:02:31 +0000

    *** empty log message ***

--- run-healthcheck-web.sh
+++ run-healthcheck-web.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JAVA_HOME=/usr/bin/java
+JAVA_HOME=/usr/java/j2sdk1.4.2_05
 dir=/nfs/acari/ensembl/ensj-healthcheck
 
 cp=$dir
@@ -9,6 +9,6 @@ cp=$cp:$dir/lib/mysql-connector-java-3.0.15-ga-bin.jar
 
 cd $dir
 
-$JAVA_HOME/bin/java -server -classpath $cp -Xmx512m org.ensembl.healthcheck.DatabaseTestRunner
+$JAVA_HOME/bin/java -server -classpath $cp -Xmx2048m org.ensembl.healthcheck.DatabaseTestRunner
 
 
