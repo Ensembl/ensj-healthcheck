49d866a050c8e4aa2f1a25f3ccbf4bedb20df892
Glenn Proctor <gp1@sanger.ac.uk>
2007-11-20 09:38:15 +0000

    Typo.

--- src/org/ensembl/healthcheck/testcase/generic/DensityFeatures.java
+++ src/org/ensembl/healthcheck/testcase/generic/DensityFeatures.java
@@ -81,7 +81,7 @@ public class DensityFeatures extends SingleDatabaseTestCase {
 	 * 
 	 * @param dbre
 	 *          The database to use.
-	 * @return true if the test pased.
+	 * @return true if the test passed.
 	 * 
 	 */
 	public boolean run(DatabaseRegistryEntry dbre) {
