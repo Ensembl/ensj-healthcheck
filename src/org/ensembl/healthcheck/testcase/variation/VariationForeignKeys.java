07bcc7728cb1da1b324237ed9ca031be264fba84
Yuan Chen <yuan@sanger.ac.uk>
2007-11-23 11:32:09 +0000

    add check for variation_id,seq_region_id between variation_feature and flanking_sequence tables

--- src/org/ensembl/healthcheck/testcase/variation/VariationForeignKeys.java
+++ src/org/ensembl/healthcheck/testcase/variation/VariationForeignKeys.java
@@ -61,14 +61,16 @@ public class VariationForeignKeys extends SingleDatabaseTestCase {
  	      	
 	result &= checkForOrphans(con, "allele", "sample_id", "sample", "sample_id", true);
 
-	/*
+	/*This is a sql to the above command
 	SELECT COUNT(*) FROM allele LEFT JOIN sample ON allele.sample_id = sample.sample_id WHERE sample.sample_id is NULL;
 	*/
 	result &= checkForOrphans(con, "variation", "source_id", "source", "source_id", true);
 
 	result &= checkForOrphans(con, "variation_synonym", "source_id", "source", "source_id", true);
 
-	result &= checkForOrphans(con, "variation_feature", "source_id", "source", "source_id", true);
+        result &= checkForOrphans(con, "variation_synonym", "variation_id", "variation", "variation_id", true);
+        
+        result &= checkForOrphans(con, "variation_feature", "source_id", "source", "source_id", true);
 
 	result &= checkForOrphans(con, "allele_group", "sample_id", "sample", "sample_id", true);
 	
@@ -84,8 +86,10 @@ public class VariationForeignKeys extends SingleDatabaseTestCase {
 		
 	result &= checkForOrphans(con, "individual_genotype_multiple_bp", "sample_id", "sample", "sample_id", true);
 	
-	/*result &= checkForOrphans(con, "tmp_individual_genotype_single_bp", "sample_id", "individual_population", "individual_sample_id", true); instead check compressed_genotype_single_bp with individual table*/
-	
+	result &= checkForOrphans(con, "tmp_individual_genotype_single_bp", "variation_id", "variation", "variation_id", true); /*instead check compressed_genotype_single_bp with individual table*/
+
+        /*result &= checkForOrphans(con, "tmp_individual_genotype_single_bp", "variation_id", "variation_feature", "variation_id", true); this is true only for ensembl snps*/
+        
 	result &= checkForOrphans(con, "compressed_genotype_single_bp", "sample_id", "individual", "sample_id", true);
 
 	result &= checkForOrphans(con, "individual_population", "individual_sample_id", "sample", "sample_id", true);
@@ -103,7 +107,7 @@ public class VariationForeignKeys extends SingleDatabaseTestCase {
 	result &= checkForOrphans(con, "variation_synonym", "variation_id", "variation", "variation_id", true);
 	
 	result &= checkForOrphans(con, "flanking_sequence", "variation_id", "variation", "variation_id", true);
-        
+
 	result &= checkForOrphans(con, "variation_feature", "variation_id", "flanking_sequence", "variation_id",true);
 	
 	result &= checkForOrphans(con, "variation_feature", "variation_id", "allele", "variation_id",true);
@@ -112,7 +116,11 @@ public class VariationForeignKeys extends SingleDatabaseTestCase {
 
 	result &= checkForOrphans(con, "transcript_variation", "variation_feature_id", "variation_feature", "variation_feature_id", true);
 	
-	
+	rows = getRowCount(con,"SELECT COUNT(*) FROM variation_feature vf, flanking_sequence f where vf.variation_id=f.variation_id and vf.seq_region_id != f.seq_region_id");
+	if (rows > 0){
+	    ReportManager.problem(this, con, "flanking_sequence contains entries have same variation_id, but different seq_region_id compare with table variation features");
+	    result =  false;
+	}
 	rows = getRowCount(con,"SELECT COUNT(*) FROM compressed_genotype_single_bp c where c.seq_region_start not in (select vf.seq_region_start from variation_feature vf where c.seq_region_id = vf.seq_region_id)");
 	if (rows > 0){
 	    ReportManager.problem(this, con, "Compressed genotype table corrupted: contains entries without variation features");
