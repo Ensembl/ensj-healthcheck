90832d505196d4cd9195c60bfd552314f3fef8d1
Glenn Proctor <gp1@sanger.ac.uk>
2008-03-27 08:18:48 +0000

    Check that certain regions have a specific gene count, e.g. human MT chr should have 13.

---
+++ src/org/ensembl/healthcheck/testcase/generic/GeneCount.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2004 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ */
+
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.Connection;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.Species;
+import org.ensembl.healthcheck.testcase.Priority;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+
+/**
+ * Check that certain regions have a specific gene count.
+ */
+public class GeneCount extends SingleDatabaseTestCase {
+
+	/**
+	 * Creates a new instance of GeneCount
+	 */
+	public GeneCount() {
+
+		addToGroup("release");
+		setDescription("Check that certain regions have a specific gene count.");
+		setPriority(Priority.AMBER);
+		setEffect("Causes incorrect display of gene counts and confusing contigview displays.");
+		setFix("Add/remove genes.");
+
+	}
+
+	/**
+	 * Run the test.
+	 * 
+	 * @param dbr
+	 *          The database registry containing all the specified databases.
+	 */
+	public boolean run(DatabaseRegistryEntry dbre) {
+
+		boolean result = true;
+
+		// MT chromosome should have 13 protein coding genes
+		if (dbre.getSpecies() == Species.HOMO_SAPIENS) {
+			result &= countMTGenes(dbre.getConnection());
+		}
+
+		return result;
+
+	} // run
+
+	// -----------------------------------------------------------------------
+
+	private boolean countMTGenes(Connection con) {
+
+		boolean result = true;
+		
+		int genes = getRowCount(con, "SELECT COUNT(*) FROM coord_system cs, seq_region sr, gene g WHERE cs.coord_system_id=sr.coord_system_id AND cs.name='chromosome' AND cs.version='NCBI36' AND sr.name='MT' and g.seq_region_id=sr.seq_region_id AND g.biotype='protein_coding'");
+
+		if (genes != 13) {
+			
+			ReportManager.problem(this, con, "Human MT chromosome should have 13 protein coding genes, actually has " + genes);
+			result = false;
+			
+		} else {
+			
+			ReportManager.correct(this, con, "Human MT chromosome has 13 protein coding genes.");
+			
+		}
+		
+		return result;
+		
+	}
+
+	// -----------------------------------------------------------------------
+
+} // GeneCount
+
