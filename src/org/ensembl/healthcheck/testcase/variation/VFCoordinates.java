7664b54804f7e44d5b3c229dd832c17a410eaf16
Glenn Proctor <gp1@sanger.ac.uk>
2008-08-13 08:50:11 +0000

    Removed extraneous log message

--- src/org/ensembl/healthcheck/testcase/variation/VFCoordinates.java
+++ src/org/ensembl/healthcheck/testcase/variation/VFCoordinates.java
@@ -34,84 +34,89 @@ import org.ensembl.healthcheck.testcase.MultiDatabaseTestCase;
 
 public class VFCoordinates extends MultiDatabaseTestCase {
 
-     /**
-     * Create an ForeignKeyCoreId that applies to a specific set of databases.
-     */
-    public VFCoordinates() {
+	/**
+	 * Create an ForeignKeyCoreId that applies to a specific set of databases.
+	 */
+	public VFCoordinates() {
 
-        addToGroup("variation");
-	addToGroup("variation-release");
-	setDescription("Check for possible wrong coordinates in Vf table, due to wrong length or outside range seq_region.");
-	setHintLongRunning(true);
+		addToGroup("variation");
+		addToGroup("variation-release");
+		setDescription("Check for possible wrong coordinates in Vf table, due to wrong length or outside range seq_region.");
+		setHintLongRunning(true);
 
-    }
+	}
 
-    /**
-     * Run the test.
-     * 
-     * @param databases
-     *          The databases to check, in order core->variation
-     * @return true if same transcripts and seq_regions in core and variation are the same.
-     *  
-     */
-    public boolean run(DatabaseRegistry dbr) {
+	/**
+	 * Run the test.
+	 * 
+	 * @param databases
+	 *          The databases to check, in order core->variation
+	 * @return true if same transcripts and seq_regions in core and variation are
+	 *         the same.
+	 * 
+	 */
+	public boolean run(DatabaseRegistry dbr) {
 
+		boolean result = true;
 
-        boolean result = true;
+		DatabaseRegistryEntry[] variationDBs = dbr.getAll(DatabaseType.VARIATION);
 
-	DatabaseRegistryEntry[] variationDBs = dbr.getAll(DatabaseType.VARIATION);
-	
-	for (int i = 0; i < variationDBs.length; i++){
-	    
-	    DatabaseRegistryEntry dbrvar = variationDBs[i];
-	    Species species = dbrvar.getSpecies();
-	    String variationName = dbrvar.getName();
-	    String coreName = variationName.replaceAll("variation","core");
-	    DatabaseRegistryEntry dbrcore = new DatabaseRegistryEntry(coreName,species,DatabaseType.CORE,true);
-	    if (dbrcore == null){
-		result = false;
-		logger.severe("Incorrect core database " + coreName + " for " + variationName);
+		for (int i = 0; i < variationDBs.length; i++) {
+
+			DatabaseRegistryEntry dbrvar = variationDBs[i];
+			Species species = dbrvar.getSpecies();
+			String variationName = dbrvar.getName();
+			String coreName = variationName.replaceAll("variation", "core");
+			DatabaseRegistryEntry dbrcore = new DatabaseRegistryEntry(coreName, species, DatabaseType.CORE, true);
+			if (dbrcore == null) {
+				result = false;
+				logger.severe("Incorrect core database " + coreName + " for " + variationName);
+				return result;
+			}
+
+			Connection con = dbrvar.getConnection();
+
+			System.out.println("Using " + coreName + " as core database and " + variationName + " as variation database");
+
+			int mc = getRowCount(
+					con,
+					"SELECT COUNT(*) FROM "
+							+ variationName
+							+ ".variation_feature WHERE length(allele_string) = 3 and seq_region_start<> seq_region_end and allele_string NOT LIKE '%-%'");
+
+			if (mc > 0) {
+				ReportManager.problem(this, con, "Wrong allele length !! (allele_string <> coordinates length) for " + variationName);
+				result = false;
+			}
+
+			mc = getRowCount(con, "SELECT COUNT(*) FROM " + coreName + ".seq_region s, " + variationName
+					+ ".variation_feature vf WHERE vf.seq_region_id = s.seq_region_id AND vf.seq_region_end > s.length");
+			if (mc > 0) {
+				ReportManager.problem(this, con, "Variation Features outside range in " + variationName);
+				result = false;
+			}
+			mc = getRowCount(con, "SELECT COUNT(*) FROM " + variationName
+					+ ".variation_feature vf WHERE vf.seq_region_start = 1 AND vf.seq_region_end > 1");
+			if (mc > 0) {
+				ReportManager.problem(this, con, "Variation Features with coordinates = 1 " + variationName);
+				result = false;
+			}
+			
+		}
 		return result;
-	    }
 
-	    Connection con = dbrvar.getConnection();
-	    
-	    System.out.println("Using " + coreName + " as core database and " + variationName + " as variation database");
+	}
 
-	    int mc = getRowCount(con,"SELECT COUNT(*) FROM " + variationName + ".variation_feature WHERE length(allele_string) = 3 and seq_region_start<> seq_region_end and allele_string NOT LIKE '%-%'");
-	    
-	    if (mc > 0){
-		ReportManager.problem(this, con, "Wrong allele length !! (allele_string <> coordinates length) for " + variationName);
-		result = false;
-	    }
-	   	    
-	    mc = getRowCount(con,"SELECT COUNT(*) FROM " + coreName + ".seq_region s, " + variationName + ".variation_feature vf WHERE vf.seq_region_id = s.seq_region_id AND vf.seq_region_end > s.length");
-	     if (mc > 0){
-		ReportManager.problem(this, con, "Variation Features outside range in " + variationName);
-		result = false;
-	    }	     
-	     mc = getRowCount(con,"SELECT COUNT(*) FROM " + variationName + ".variation_feature vf WHERE vf.seq_region_start = 1 AND vf.seq_region_end > 1");
-	     if (mc > 0){
-		 ReportManager.problem(this, con, "Variation Features with coordinates = 1 " + variationName);
-		 result = false;
-	     }
-	 	if (result){
-	    	 ReportManager.info(this,con,"VFCoordinates run successfully");
-	     } 
-		}	  
-        return result;
+	/**
+	 * This only applies to variation databases.
+	 */
+	public void types() {
 
-    }
-    /**
-     * This only applies to variation databases.
-     */
-     public void types() {
+		removeAppliesToType(DatabaseType.OTHERFEATURES);
+		removeAppliesToType(DatabaseType.CDNA);
+		removeAppliesToType(DatabaseType.CORE);
+		removeAppliesToType(DatabaseType.VEGA);
 
-	 removeAppliesToType(DatabaseType.OTHERFEATURES);
-	 removeAppliesToType(DatabaseType.CDNA);
-	 removeAppliesToType(DatabaseType.CORE);
-	 removeAppliesToType(DatabaseType.VEGA);
-
-     }
+	}
 
 } // VFCoordinates
