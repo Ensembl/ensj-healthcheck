d5b47ac80201ebf185534c338fca31093cae53b0
Glenn Proctor <gp1@sanger.ac.uk>
2008-11-14 21:51:06 +0000

    Tweaked match pattern to avoid false positives in mouse

--- src/org/ensembl/healthcheck/testcase/generic/XrefHTML.java
+++ src/org/ensembl/healthcheck/testcase/generic/XrefHTML.java
@@ -55,10 +55,10 @@ public class XrefHTML extends SingleDatabaseTestCase {
 
 		Connection con = dbre.getConnection();
 		
-		int rows = getRowCount(con, "SELECT COUNT(*) FROM xref WHERE display_label LIKE '%<%'");
+		int rows = getRowCount(con, "SELECT COUNT(*) FROM xref WHERE display_label LIKE '%<%>%<%>%'");
 		
 		if (rows > 0) {
-			ReportManager.problem(this, con, rows + " xrefs appear to have HTML markup in the display_label");
+			ReportManager.problem(this, con, rows + " xrefs appear to have HTML markup (<...>) in the display_label");
 			result = false;
 		} else {
 			ReportManager.correct(this, con, "No xrefs appear to have HTML markup in the display_label");
