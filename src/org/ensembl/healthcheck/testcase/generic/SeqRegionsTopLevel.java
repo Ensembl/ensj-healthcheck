d8f95ad85fa8fb814e2613d1a0f53617339cfb2c
Glenn Proctor <gp1@sanger.ac.uk>
2009-01-09 08:44:38 +0000

    Refactored.

--- src/org/ensembl/healthcheck/testcase/generic/SeqRegionsTopLevel.java
+++ src/org/ensembl/healthcheck/testcase/generic/SeqRegionsTopLevel.java
@@ -20,9 +20,9 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * Check that all seq_regions comprising genes are marked as toplevel in
- * seq_region_attrib. Also checks that there is at least one seq_region marked
- * as toplevel (needed by compara).
+ * Check that all seq_regions comprising genes are marked as toplevel in seq_region_attrib. Also checks that there is at least one
+ * seq_region marked as toplevel (needed by compara). Also check that all toplevel seq regions are marked as such, and no seq
+ * regions that are marked as toplevel are not toplevel.
  */
 
 public class SeqRegionsTopLevel extends SingleDatabaseTestCase {
@@ -34,7 +34,7 @@ public class SeqRegionsTopLevel extends SingleDatabaseTestCase {
 
 		addToGroup("post_genebuild");
 		addToGroup("release");
-		setDescription("Check that all seq_regions comprising genes are marked as toplevel in seq_region_attrib, and that there is at least one toplevel seq_region.");
+		setDescription("Check that all seq_regions comprising genes are marked as toplevel in seq_region_attrib, and that there is at least one toplevel seq_region. Also check that all toplevel seq regions are marked as such, and no seq regions that are marked as toplevel are not toplevel");
 
 	}
 
@@ -59,27 +59,53 @@ public class SeqRegionsTopLevel extends SingleDatabaseTestCase {
 
 		Connection con = dbre.getConnection();
 
+		int topLevelAttribTypeID = getAttribTypeID(con);
+		if (topLevelAttribTypeID == -1) {
+			return false;
+		}
+
+		result &= check_genes(con, topLevelAttribTypeID);
+
+		result &= check_one_seq_region(con, topLevelAttribTypeID);
+
+		result &= checkRankOne(dbre);
+
+		return result;
+
+	} // run
+
+	// --------------------------------------------------------------------------
+
+	private int getAttribTypeID(Connection con) {
+
 		// check that all gene seq_regions have toplevel attributes
-		// pre-fetch correct attrib_type_id to save a join
 		String val = getRowColumnValue(con, "SELECT attrib_type_id FROM attrib_type WHERE code=\'toplevel\'");
 		if (val == null || val.equals("")) {
 			ReportManager.problem(this, con, "Can't find a seq_region attrib_type with code 'toplevel', exiting");
-			return false;
+			return -1;
 		}
-		int topLevelAttribType = Integer.parseInt(val);
+		int topLevelAttribTypeID = Integer.parseInt(val);
 
-		logger.info("attrib_type_id for toplevel: " + topLevelAttribType);
+		logger.info("attrib_type_id for toplevel: " + topLevelAttribTypeID);
 
-		int numTopLevelGenes = getRowCount(con, "SELECT COUNT(*) FROM seq_region_attrib sra, gene g WHERE sra.attrib_type_id = "
-				+ topLevelAttribType + " AND sra.seq_region_id=g.seq_region_id");
+		return topLevelAttribTypeID;
+
+	}
+
+	// --------------------------------------------------------------------------
+
+	private boolean check_genes(Connection con, int topLevelAttribTypeID) {
+
+		boolean result = true;
+
+		int numTopLevelGenes = getRowCount(con, "SELECT COUNT(*) FROM seq_region_attrib sra, gene g WHERE sra.attrib_type_id = " + topLevelAttribTypeID + " AND sra.seq_region_id=g.seq_region_id");
 		int numGenes = getRowCount(con, "SELECT COUNT(*) FROM gene");
 
 		int nonTopLevelGenes = numGenes - numTopLevelGenes;
 
 		if (nonTopLevelGenes > 0) {
 
-			ReportManager.problem(this, con, nonTopLevelGenes
-					+ " genes are on seq_regions which are not toplevel; this may cause problems for Compara and slow down the mapper.");
+			ReportManager.problem(this, con, nonTopLevelGenes + " genes are on seq_regions which are not toplevel; this may cause problems for Compara and slow down the mapper.");
 			result = false;
 
 		} else {
@@ -88,8 +114,17 @@ public class SeqRegionsTopLevel extends SingleDatabaseTestCase {
 
 		}
 
+		return result;
+	}
+
+	// --------------------------------------------------------------------------
+
+	private boolean check_one_seq_region(Connection con, int topLevelAttribTypeID) {
+
+		boolean result = true;
+
 		// check for at least one toplevel seq_region
-		int rows = getRowCount(con, "SELECT COUNT(*) FROM seq_region_attrib WHERE attrib_type_id=" + val);
+		int rows = getRowCount(con, "SELECT COUNT(*) FROM seq_region_attrib WHERE attrib_type_id=" + topLevelAttribTypeID);
 		if (rows == 0) {
 
 			ReportManager.problem(this, con, "No seq_regions are marked as toplevel. This may cause problems for Compara");
@@ -101,10 +136,22 @@ public class SeqRegionsTopLevel extends SingleDatabaseTestCase {
 
 		}
 
+		return result;
+
+	}
+
+	// --------------------------------------------------------------------------
+
+	private boolean checkRankOne(DatabaseRegistryEntry dbre) {
+
+		boolean result = true;
+
+		Connection con = dbre.getConnection();
+
 		// check that there is one co-ordinate system with rank = 1
 		if (!dbre.isMultiSpecies()) {
 
-			rows = getRowCount(con, "SELECT COUNT(*) FROM coord_system WHERE rank=1");
+			int rows = getRowCount(con, "SELECT COUNT(*) FROM coord_system WHERE rank=1");
 			if (rows == 0) {
 
 				ReportManager.problem(this, con, "No co-ordinate systems have rank = 1");
@@ -121,9 +168,10 @@ public class SeqRegionsTopLevel extends SingleDatabaseTestCase {
 
 			}
 		}
-
 		return result;
 
-	} // run
+	}
+
+	// --------------------------------------------------------------------------
 
 } // SeqRegionsTopLevel
