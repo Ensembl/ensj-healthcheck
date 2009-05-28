43a4e16120d42b52db5ba8083e90d29d855f3102
Glenn Proctor <gp1@sanger.ac.uk>
2009-05-28 12:53:59 +0000

    Fix useful SQL to always have correct table name.

--- src/org/ensembl/healthcheck/testcase/generic/HitNameFormat.java
+++ src/org/ensembl/healthcheck/testcase/generic/HitNameFormat.java
@@ -62,7 +62,7 @@ public class HitNameFormat extends SingleDatabaseTestCase {
 
 			if (rows > 0) {
 				ReportManager.problem(this, con, rows + " " + table + "s appear to have incorrectly formatted hit_names (containing a '|' symbol)");
-				ReportManager.problem(this, con, "USEFUL SQL: SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(hit_name,'|',-2),'|',1) FROM dna_align_feature WHERE hit_name LIKE 'gi%|%'");
+				ReportManager.problem(this, con, "USEFUL SQL: SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(hit_name,'|',-2),'|',1) FROM " + table + " WHERE hit_name LIKE 'gi%|%'");
 				ReportManager.problem(this, con, "UPDATE protein_align_feature SET hit_name = SUBSTRING_INDEX(SUBSTRING_INDEX(hit_name,'|',-2),'|',1) WHERE hit_name LIKE 'gi|%'");
 				result = false;
 			} else {
