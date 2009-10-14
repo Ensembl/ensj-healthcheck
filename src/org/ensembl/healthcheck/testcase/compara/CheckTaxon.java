21305ef11aa29924f52c42cbd2417b2359696b86
Leo Gordon <lg4@ebi.ac.uk>
2009-10-14 14:21:27 +0000

    *** empty log message ***

--- src/org/ensembl/healthcheck/testcase/compara/CheckTaxon.java
+++ src/org/ensembl/healthcheck/testcase/compara/CheckTaxon.java
@@ -175,10 +175,14 @@ public class CheckTaxon extends MultiDatabaseTestCase {
               while (!this_taxon_id.equals("0")) {
                 values1 = getRowValues(comparaCon,
                     "SELECT rank, parent_id, genbank_hidden_flag FROM ncbi_taxa_node WHERE taxon_id = " + this_taxon_id);
-                if (values1[2].equals("0") && !values1[1].equals("0") && !values1[0].equals("subgenus") && !values1[0].equals("subspecies")) {
-                  comparaClassification += " " + getRowColumnValue(comparaCon,
+                if ( //values1[2].equals("0") && 
+                !values1[1].equals("1") && !values1[1].equals("0") && !values1[0].equals("subgenus") && !values1[0].equals("subspecies")) {
+                  String taxonName = getRowColumnValue(comparaCon,
                       "SELECT name FROM ncbi_taxa_name " +
                       "WHERE name_class = \"scientific name\" AND taxon_id = " + this_taxon_id);
+                  if (!taxonName.equals("Fungi/Metazoa group")) {
+                    comparaClassification += " " + taxonName;
+                  }
                 }
                 this_taxon_id = values1[1];
               }
