8c5e174daccf06d53b101952b6f0c2f8efde8da1
Glenn Proctor <gp1@sanger.ac.uk>
2009-10-24 10:44:20 +0000

    Added generics as part of a general review of this healthcheck - seems to be taking too long.

--- src/org/ensembl/healthcheck/testcase/generic/MetaCoord.java
+++ src/org/ensembl/healthcheck/testcase/generic/MetaCoord.java
@@ -32,8 +32,7 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * Check that meta_coord table contains entries for all the coordinate systems
- * that all the features are stored in.
+ * Check that meta_coord table contains entries for all the coordinate systems that all the features are stored in.
  */
 public class MetaCoord extends SingleDatabaseTestCase {
 
@@ -64,70 +63,77 @@ public class MetaCoord extends SingleDatabaseTestCase {
 
 		Connection con = dbre.getConnection();
 
-		// coordSystems is a hash of lists of coordinate systems that each feature
-		// table contains
-		Map coordSystems = new HashMap();
+		// coordSystems is a hash of lists of coordinate systems that each feature table contains
+		Map<String, List<String>> coordSystems = new HashMap<String, List<String>>();
 
 		try {
 
 			Statement stmt = con.createStatement();
 
-			// build up a list of all the coordinate systems that are in the various
-			// feature tables
-			for (int tableIndex = 0; tableIndex < featureTables.length; tableIndex++) {
+			// build up a list of all the coordinate systems that are in the various feature tables
+			for (String tableName : featureTables) {
 
-				String tableName = featureTables[tableIndex];
-				String sql = "SELECT DISTINCT(sr.coord_system_id) FROM seq_region sr, " + tableName
-						+ " f WHERE sr.seq_region_id = f.seq_region_id";
+				String sql = "SELECT DISTINCT(sr.coord_system_id) FROM seq_region sr, " + tableName + " f WHERE sr.seq_region_id = f.seq_region_id";
 
 				logger.finest("Getting feature coordinate systems for " + tableName);
 				ResultSet rs = stmt.executeQuery(sql);
 
 				while (rs.next()) {
+					
 					String coordSystemID = rs.getString(1);
 					logger.finest("Added feature coordinate system for " + tableName + ": " + coordSystemID);
+					
 					// check that the meta_coord table has an entry corresponding to this
-					int mc = getRowCount(con, "SELECT COUNT(*) FROM meta_coord WHERE coord_system_id=" + coordSystemID + " AND table_name='"
-							+ tableName + "'");
+					int mc = getRowCount(con, "SELECT COUNT(*) FROM meta_coord WHERE coord_system_id=" + coordSystemID + " AND table_name='" + tableName + "'");
+				
 					if (mc == 0) {
-						ReportManager.problem(this, con, "No entry for coordinate system with ID " + coordSystemID + " for " + tableName
-								+ " in meta_coord");
+						
+						ReportManager.problem(this, con, "No entry for coordinate system with ID " + coordSystemID + " for " + tableName + " in meta_coord");
 						result = false;
+			
 					} else if (mc > 1) {
-						ReportManager.problem(this, con, "Coordinate system with ID " + coordSystemID + " duplicated for " + tableName
-								+ " in meta_coord");
+			
+						ReportManager.problem(this, con, "Coordinate system with ID " + coordSystemID + " duplicated for " + tableName + " in meta_coord");
 						result = false;
+			
 					} else {
-						ReportManager.correct(this, con, "Coordinate system with ID " + coordSystemID + " for table " + tableName
-								+ " has an entry in meta_coord");
+						
+						ReportManager.correct(this, con, "Coordinate system with ID " + coordSystemID + " for table " + tableName + " has an entry in meta_coord");
+			
 					}
 
 					// store in coordSystems map - create List if necessary
-					List csList = (ArrayList) coordSystems.get(tableName);
+					List<String> csList = coordSystems.get(tableName);
+					
 					if (csList == null) {
-						csList = new ArrayList();
+						
+						csList = new ArrayList<String>();
+						
 					}
+					
 					csList.add(coordSystemID);
 					coordSystems.put(tableName, csList);
+					
 				}
 
 				rs.close();
 
 			}
 
-			// check that every meta_coord table entry refers to a coordinate system
-			// that is used in a feature
+			// check that every meta_coord table entry refers to a coordinate system that is used in a feature
 			// if this isn't true it's not fatal but should be flagged
 			String sql = "SELECT * FROM meta_coord";
 			ResultSet rs = stmt.executeQuery(sql);
+			
 			while (rs.next()) {
+			
 				String tableName = rs.getString("table_name");
 				String csID = rs.getString("coord_system_id");
 				logger.finest("Checking for coord_system_id " + csID + " in " + tableName);
-				List featureCSs = (ArrayList) coordSystems.get(tableName);
+				
+				List<String> featureCSs = coordSystems.get(tableName);
 				if (featureCSs != null && !featureCSs.contains(csID)) {
-					ReportManager.problem(this, con, "meta_coord has entry for coord_system ID " + csID + " in " + tableName
-							+ " but this coordinate system is not actually used in " + tableName);
+					ReportManager.problem(this, con, "meta_coord has entry for coord_system ID " + csID + " in " + tableName + " but this coordinate system is not actually used in " + tableName);
 					result = false;
 				}
 
