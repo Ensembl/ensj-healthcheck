a0ecd77533e5c1420058620d21c98a6c047bd68d
Glenn Proctor <gp1@sanger.ac.uk>
2009-10-27 14:12:46 +0000

    Now print IDs of both exons.

--- src/org/ensembl/healthcheck/testcase/generic/DuplicateExons.java
+++ src/org/ensembl/healthcheck/testcase/generic/DuplicateExons.java
@@ -28,115 +28,115 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 /**
- * An EnsEMBL Healthcheck test case which checks all exon of a gene are on the
- * same strand and in the correct order in their transcript..
+ * An EnsEMBL Healthcheck test case which checks all exon of a gene are on the same strand and in the correct order in their
+ * transcript..
  */
 
 public class DuplicateExons extends SingleDatabaseTestCase {
 
-    private static final int MAX_WARNINGS = 10;
+	private static final int MAX_WARNINGS = 10;
 
-    /**
-     * Create an OrphanTestCase that applies to a specific set of databases.
-     */
-    public DuplicateExons() {
+	/**
+	 * Create an OrphanTestCase that applies to a specific set of databases.
+	 */
+	public DuplicateExons() {
 
-        addToGroup("post_genebuild");
-        addToGroup("release");
+		addToGroup("post_genebuild");
+		addToGroup("release");
 
-    }
-    
-    /**
-     * This test only applies to core and Vega databases.
-     */
-    public void types() {
+	}
 
-        removeAppliesToType(DatabaseType.OTHERFEATURES);
-        removeAppliesToType(DatabaseType.CDNA);
-        
-    }
-    
-    /**
-     * Check for duplicate exons.
-     * 
-     * @param dbre
-     *          The database to check.
-     * @return True if the test passes.
-     */
+	/**
+	 * This test only applies to core and Vega databases.
+	 */
+	public void types() {
 
-    public boolean run(DatabaseRegistryEntry dbre) {
+		removeAppliesToType(DatabaseType.OTHERFEATURES);
+		removeAppliesToType(DatabaseType.CDNA);
 
-        boolean result = true;
+	}
 
-        String sql = "SELECT e.exon_id, e.phase, e.seq_region_start AS start, e.seq_region_end AS end, e.seq_region_id AS chromosome_id, e.end_phase, e.seq_region_strand AS strand "
-                + "             FROM exon e ORDER BY chromosome_id, strand, start, end, phase, end_phase";
+	/**
+	 * Check for duplicate exons.
+	 * 
+	 * @param dbre
+	 *          The database to check.
+	 * @return True if the test passes.
+	 */
 
-        Connection con = dbre.getConnection();
-        try {
-            
-            Statement stmt = con.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY,
-                    java.sql.ResultSet.CONCUR_READ_ONLY);
-            stmt.setFetchSize(1000);
-            ResultSet rs = stmt.executeQuery(sql);
+	public boolean run(DatabaseRegistryEntry dbre) {
 
-            int exonStart, exonEnd, exonPhase, exonChromosome, exonId, exonEndPhase, exonStrand;
-            int lastExonStart = -1;
-            int lastExonEnd = -1;
-            int lastExonPhase = -1;
-            int lastExonChromosome = -1;
-            int lastExonEndPhase = -1;
-            int lastExonStrand = -1;
-            int duplicateExon = 0;
+		boolean result = true;
 
-            boolean first = true;
+		String sql = "SELECT e.exon_id, e.phase, e.seq_region_start AS start, e.seq_region_end AS end, e.seq_region_id AS chromosome_id, e.end_phase, e.seq_region_strand AS strand "
+				+ "             FROM exon e ORDER BY chromosome_id, strand, start, end, phase, end_phase";
 
-            while (rs.next()) {
+		Connection con = dbre.getConnection();
+		try {
 
-                // load the vars
-                exonId = rs.getInt(1);
-                exonPhase = rs.getInt(2);
-                exonStart = rs.getInt(3);
-                exonEnd = rs.getInt(4);
-                exonChromosome = rs.getInt(5);
-                exonEndPhase = rs.getInt(6);
-                exonStrand = rs.getInt(7);
-                	
-                if (!first) {
-                    if (lastExonChromosome == exonChromosome && lastExonStart == exonStart && lastExonEnd == exonEnd
-                            && lastExonPhase == exonPhase && lastExonStrand == exonStrand
-                            && lastExonEndPhase == exonEndPhase) {
-                        duplicateExon++;
-                        if (duplicateExon <= MAX_WARNINGS) {
-                            ReportManager.warning(this, con, "Exon " + exonId + " is duplicated.");
-                        } 
-                    }
-                } else {
-                    first = false;
-                }
+			Statement stmt = con.createStatement(java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY);
+			stmt.setFetchSize(1000);
+			ResultSet rs = stmt.executeQuery(sql);
 
-                lastExonStart = exonStart;
-                lastExonEnd = exonEnd;
-                lastExonChromosome = exonChromosome;
-                lastExonPhase = exonPhase;
-                lastExonEndPhase = exonEndPhase;
-                lastExonStrand = exonStrand;
-                
-            } // while rs
+			int exonStart, exonEnd, exonPhase, exonChromosome, exonID, exonEndPhase, exonStrand;
+			int lastExonStart = -1;
+			int lastExonEnd = -1;
+			int lastExonPhase = -1;
+			int lastExonChromosome = -1;
+			int lastExonEndPhase = -1;
+			int lastExonStrand = -1;
+			int lastExonID = -1;
+			int duplicateExon = 0;
 
-            if (duplicateExon > 0) {
-                ReportManager.problem(this, con, "Has at least " + duplicateExon + " duplicated exons.");
-                result = false;
-            }
-            rs.close();
-            stmt.close();
+			boolean first = true;
 
-        } catch (Exception e) {
-            result = false;
-            e.printStackTrace();
-        }
+			while (rs.next()) {
 
-        return result;
+				// load the vars
+				exonID = rs.getInt(1);
+				exonPhase = rs.getInt(2);
+				exonStart = rs.getInt(3);
+				exonEnd = rs.getInt(4);
+				exonChromosome = rs.getInt(5);
+				exonEndPhase = rs.getInt(6);
+				exonStrand = rs.getInt(7);
 
-    }
+				if (!first) {
+					if (lastExonChromosome == exonChromosome && lastExonStart == exonStart && lastExonEnd == exonEnd && lastExonPhase == exonPhase && lastExonStrand == exonStrand
+							&& lastExonEndPhase == exonEndPhase) {
+						duplicateExon++;
+						if (duplicateExon <= MAX_WARNINGS) {
+							ReportManager.warning(this, con, "Exon " + exonID + " is a duplicate of exon " + lastExonID);
+						}
+					}
+				} else {
+					first = false;
+				}
+
+				lastExonStart = exonStart;
+				lastExonEnd = exonEnd;
+				lastExonChromosome = exonChromosome;
+				lastExonPhase = exonPhase;
+				lastExonEndPhase = exonEndPhase;
+				lastExonStrand = exonStrand;
+				lastExonID = exonID;
+				
+			} // while rs
+
+			if (duplicateExon > 0) {
+				ReportManager.problem(this, con, "Has at least " + duplicateExon + " duplicated exons.");
+				result = false;
+			}
+			rs.close();
+			stmt.close();
+
+		} catch (Exception e) {
+			result = false;
+			e.printStackTrace();
+		}
+
+		return result;
+
+	}
 
 } // DuplicateExons
