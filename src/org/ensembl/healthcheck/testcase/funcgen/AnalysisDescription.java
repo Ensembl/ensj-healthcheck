d1eb7d2151cb5a86bf47732cfe5f92cd66607a73
Glenn Proctor <gp1@sanger.ac.uk>
2010-01-07 11:16:18 +0000

    Incorrect package name caused complation failure.

--- src/org/ensembl/healthcheck/testcase/funcgen/AnalysisDescription.java
+++ src/org/ensembl/healthcheck/testcase/funcgen/AnalysisDescription.java
@@ -10,7 +10,7 @@
  * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package org.ensembl.healthcheck.testcase.generic;
+package org.ensembl.healthcheck.testcase.funcgen;
 
 import java.sql.Connection;
 import java.util.Map;
