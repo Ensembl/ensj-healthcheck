8af23d168ac07fe18f2d4fd9960a9be31590b4d5
Javier Herrero <jh7@sanger.ac.uk>
2010-02-03 22:30:35 +0000

    Extra HC for ancestral sequences DB only

---
+++ src/org/ensembl/healthcheck/testcase/generic/AncestralSequencesExtraChecks.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2004 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.Connection;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.DatabaseType;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+import org.ensembl.healthcheck.util.Utils;
+
+/**
+ * An EnsEMBL Healthcheck test case that looks for broken foreign-key relationships.
+ */
+
+public class AncestralSequencesExtraChecks extends SingleDatabaseTestCase {
+
+	/**
+	 * Create an OrphanTestCase that applies to a specific set of databases.
+	 */
+	public AncestralSequencesExtraChecks() {
+
+		addToGroup("compara-ancestral");
+		setTeamResponsible("compara");
+		setDescription("Additional checks for the ancestral sequences database (from compara).");
+
+	}
+
+	/**
+	 * Look for broken foreign key relationships.
+	 * 
+	 * @param dbre
+	 *          The database to use.
+	 * @return true if all foreign key relationships are valid.
+	 */
+	public boolean run(DatabaseRegistryEntry dbre) {
+
+		boolean result = true;
+
+		Connection con = dbre.getConnection();
+
+		// ----------------------------
+		// Adapted from CoreForeignKeys.java
+		// ----------------------------
+
+		result &= checkForOrphansWithConstraint(con, "seq_region", "seq_region_id", "dna", "seq_region_id",
+				"coord_system_id = (SELECT coord_system_id FROM coord_system WHERE attrib LIKE '%sequence_level%')");
+
+
+		return result;
+	}
+
+
+} // AncestralSequencesExtraChecks
