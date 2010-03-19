2578c2e8171a42a2f62ba54cc8dc623b2bd1a999
Glenn Proctor <gp1@sanger.ac.uk>
2010-03-19 14:39:25 +0000

    Add lots of extra checks as suggested by Albert.

--- src/org/ensembl/healthcheck/testcase/generic/CanonicalTranscriptCoding.java
+++ src/org/ensembl/healthcheck/testcase/generic/CanonicalTranscriptCoding.java
@@ -56,8 +56,120 @@ public class CanonicalTranscriptCoding extends SingleDatabaseTestCase {
 
 		Connection con = dbre.getConnection();
 
+		// --------------------------------
+		// A gene that has at least one transcript.biotype='protein_coding' should have gene.biotype='protein_coding'
+		int rows = getRowCount(con, "SELECT COUNT(*) FROM gene g WHERE g.gene_id IN (SELECT tr.gene_id FROM transcript tr WHERE tr.biotype='protein_coding') AND g.biotype!='protein_coding'");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " genes with at least one protein_coding transcript do not have biotype protein_coding");
+
+		} else {
+
+			ReportManager.correct(this, con, "All genes with protein_coding transcripts have protein_coding biotype");
+
+		}
+
+		// --------------------------------
+		// Protein_coding transcripts should all have translations
+		rows = getRowCount(con, "SELECT COUNT(*) FROM transcript tr LEFT JOIN translation tl ON tl.transcript_id=tr.transcript_id WHERE tr.biotype='protein_coding' AND tl.transcript_id is NULL");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " protein_coding transcripts do not have translations");
+
+		} else {
+
+			ReportManager.correct(this, con, "All protein_coding transcripts have translations");
+		}
+
+		// --------------------------------
+		// All genes should have a canonical transcript
+		rows = getRowCount(con, "SELECT COUNT(*) FROM gene g WHERE g.canonical_transcript_id is NULL");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " genes do not have a canonical transcript");
+
+		} else {
+
+			ReportManager.correct(this, con, "All genes have a canonical transcript");
+
+		}
+
+		// --------------------------------
+		// All canonical transcripts with a translation should belong to a gene with a biotype of 'protein_coding',
+		// 'IG_C_gene','IG_D_gene','IG_J_gene' or'IG_V_gene'
+		rows = getRowCount(
+				con,
+				"SELECT COUNT(*) FROM gene g WHERE g.canonical_transcript_id IN (SELECT tr.transcript_id FROM transcript tr, translation tl WHERE tr.transcript_id=tl.transcript_id) AND g.biotype NOT IN ('rRNA','retrotransposed','protein_coding','IG_C_gene','IG_D_gene','IG_J_gene','IG_V_gene')");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " genes with canonical transcripts have the wrong biotype");
+
+		} else {
+
+			ReportManager.correct(this, con, "All genes with canonical transcripts have the correct biotype");
+
+		}
+
+		// --------------------------------
+		// None of the transcripts that have a translation and have a biotype different to
+		// ('protein_coding','IG_C_gene','IG_D_gene','IG_J_gene','IG_V_gene')) should be canonical transcripts to any gene.
+		rows = getRowCount(
+				con,
+				"SELECT COUNT(*) FROM gene g WHERE g.canonical_transcript_id IN (select tr.transcript_id FROM transcript tr, translation tl WHERE tr.transcript_id=tl.transcript_id AND tr.biotype NOT IN ('rRNA','retrotransposed','protein_coding','IG_C_gene','IG_D_gene','IG_J_gene','IG_V_gene'))");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " genes have canonical transripts with mismatched biotypes");
+
+		} else {
+
+			ReportManager.correct(this, con, "All genes have canonical transcripts with matching biotypes");
+
+		}
+
+		// --------------------------------
+		// A gene that has gene.biotype='protein_coding' and has at least one transcript.biotype='protein_coding' should have a
+		// canonical transcript.biotype='protein_coding'.
+		rows = getRowCount(con, "SELECT count(*) FROM gene g JOIN transcript t USING (gene_id) WHERE g.gene_id IN (SELECT g.gene_id FROM gene g JOIN transcript t ON (g.canonical_transcript_id = t.transcript_id) WHERE g.biotype = 'protein_coding' AND t.biotype != 'protein_coding') AND t.biotype = 'protein_coding'");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " genes with at least one protein_coding transcript do not have a protein_coding canonical transcript");
+
+		} else {
+
+			ReportManager.correct(this, con, "All genes with at least one protein_coding transcript have a protein_coding canonical transcript");
+
+		}
+		
+	// --------------------------------
+		// If a gene is gene.biotype='protein_coding' but has no transcripts that are transcript.biotype='protein_coding', at least one of the transcripts has to have a translation.
+		rows = getRowCount(con, "SELECT count(*) FROM gene g JOIN transcript t USING (gene_id) JOIN translation p ON (t.canonical_translation_id = p.translation_id) WHERE g.biotype = 'protein_coding' AND g.gene_id NOT IN (SELECT gene_id FROM transcript WHERE biotype = 'protein_coding')");
+
+		if (rows > 0) {
+
+			result = false;
+			ReportManager.problem(this, con, rows + " protein_coding genes with no protein_coding transcripts have no transcripts with translations");
+
+		} else {
+
+			ReportManager.correct(this, con, "All protein_coding genes with no protein_coding transcripts have at least one transcripts which translates");
+
+		}
+
+		// --------------------------------
 		// check if protein_coding genes have a canonical transcript that has a valid translation
-		int rows = getRowCount(con, "SELECT COUNT(*) FROM gene g LEFT JOIN translation tr ON g.canonical_transcript_id=tr.transcript_id WHERE g.biotype='protein_coding' AND tr.transcript_id IS NULL");
+		rows = getRowCount(con, "SELECT COUNT(*) FROM gene g LEFT JOIN translation tr ON g.canonical_transcript_id=tr.transcript_id WHERE g.biotype='protein_coding' AND tr.transcript_id IS NULL");
 
 		if (rows > 0) {
 
@@ -69,11 +181,12 @@ public class CanonicalTranscriptCoding extends SingleDatabaseTestCase {
 			ReportManager.correct(this, con, "All protein_coding genes have canonical_transcripts that translate");
 		}
 
+		// --------------------------------
 		// check that the number of canonical translations is correct
 		int numCanonical = getRowCount(con, "SELECT COUNT(*) FROM transcript t1, translation p, transcript t2 WHERE t1.canonical_translation_id = p.translation_id AND p.transcript_id = t2.transcript_id");
-		
+
 		int numTotal = getRowCount(con, "SELECT COUNT(*) FROM translation p, transcript t WHERE t.transcript_id = p.transcript_id");
-	
+
 		if (numCanonical != numTotal) {
 
 			result = false;
@@ -84,7 +197,6 @@ public class CanonicalTranscriptCoding extends SingleDatabaseTestCase {
 			ReportManager.correct(this, con, "Number of canonical translations is correct.");
 		}
 
-		
 		return result;
 
 	} // run
