63d05e01f82941b244a096384aabca82f491c053
Glenn Proctor <gp1@sanger.ac.uk>
2010-06-28 12:29:32 +0000

    Check that the schema_type meta key is present and correct.

---
+++ src/org/ensembl/healthcheck/testcase/generic/SchemaType.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2004 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307 USA
+ */
+
+package org.ensembl.healthcheck.testcase.generic;
+
+import java.sql.Connection;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.DatabaseType;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.testcase.Priority;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+import org.ensembl.healthcheck.util.Utils;
+
+/**
+ * Check that the schema_type meta key is present and correct.
+ */
+public class SchemaType extends SingleDatabaseTestCase {
+
+	String[] allowedKeys = { "core", "compara", "funcgen", "variation" };
+	
+	/**
+	 * Creates a new instance of SchemaType
+	 */
+	public SchemaType() {
+
+		addToGroup("release");
+		setDescription("Check that the schema_type meta key is present and correct.");
+		setPriority(Priority.AMBER);
+		setFix("Set schema_type meta key.");
+		setTeamResponsible("genebuilders");
+
+	}
+
+	public void types() {
+
+		addAppliesToType(DatabaseType.VARIATION);
+		addAppliesToType(DatabaseType.FUNCGEN);
+
+	}
+	
+	/**
+	 * Run the test.
+	 * 
+	 * @param dbr
+	 *          The database registry containing all the specified databases.
+	 */
+	public boolean run(DatabaseRegistryEntry dbre) {
+
+		boolean result = true;
+
+		Connection con = dbre.getConnection();
+
+		// check that the key is present
+		String key = getRowColumnValue(con, "SELECT meta_value FROM meta WHERE meta_key='schema_type'");
+		if (key == null || key.length() == 0) {
+			ReportManager.problem(this, con, "No entry in meta table for schema_type");
+			return false;
+		}
+
+		// check that its value is one of the allowed ones
+		if (!Utils.stringInArray(key, allowedKeys, false)) {
+			ReportManager.problem(this, con, "schema_type value " + key + " in meta table is not one of the allowed values (" + Utils.arrayToString(allowedKeys, ",") + ")");
+			return false;
+		}
+		
+		
+		// check that the value matches the type of the database
+		String type = dbre.getType().getName();
+		if (!type.equals(key)) {
+			ReportManager.problem(this, con, "schema_type value " + key + " in meta table does not match database type (" + type + ")");
+			result = false;
+		}
+		
+		return result;
+
+	} // run
+
+} // SchemaType
+
