00b5788ce0091a890742e8d50674f62eadf734b2
Michael Nuhn <mnuhn@ebi.ac.uk>
2010-09-03 15:49:43 +0000

    *** empty log message ***

--- src/testgroup/DemoGroup.java
+++ src/testgroup/DemoGroup.java
@@ -1,5 +1,19 @@
 package testgroup;
 
-public class DemoGroup {
+import org.ensembl.healthcheck.GroupOfTests;
+import org.ensembl.healthcheck.testcase.compara.*;
 
+public class DemoGroup extends GroupOfTests {
+
+	public DemoGroup() {
+
+		addTest(
+			CheckConservationScore.class,
+			ForeignKeyGenomicAlignId.class,
+			CheckFlatProteinTrees.class,
+			ForeignKeyHomologyId.class
+		);
+		
+		this.addTest(new AllComparaTests());
+	}
 }
