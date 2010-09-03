61255018c6468447d4a03ec9a77a002548b1bd3b
Kathryn Beal <kbeal@ebi.ac.uk>
2010-09-03 09:28:26 +0000

    Check for orphaned constrained_elements

--- src/org/ensembl/healthcheck/testcase/compara/ForeignKeyDnafragId.java
+++ src/org/ensembl/healthcheck/testcase/compara/ForeignKeyDnafragId.java
@@ -59,6 +59,7 @@ public class ForeignKeyDnafragId extends SingleDatabaseTestCase {
 
             result &= checkForOrphans(con, "dnafrag_region", "dnafrag_id", "dnafrag", "dnafrag_id");
             result &= checkForOrphans(con, "genomic_align", "dnafrag_id", "dnafrag", "dnafrag_id");
+            result &= checkForOrphans(con, "constrained_element", "dnafrag_id", "dnafrag", "dnafrag_id");
 
         } else {
             ReportManager.correct(this, con, "NO ENTRIES in dnafrag table, so nothing to test IGNORED");
