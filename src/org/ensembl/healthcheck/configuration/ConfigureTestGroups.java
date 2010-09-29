f48db9cefe29efee15179b5d0321d094f9c10f4f
Michael Nuhn <mnuhn@ebi.ac.uk>
2010-09-29 10:53:00 +0000

    Added annotations that describe the options for the automatically generated help message

--- src/org/ensembl/healthcheck/configuration/ConfigureTestGroups.java
+++ src/org/ensembl/healthcheck/configuration/ConfigureTestGroups.java
@@ -14,23 +14,43 @@ import uk.co.flamingpenguin.jewel.cli.Option;
 public interface ConfigureTestGroups {
 
 	// Specify one or many groups of tests that will be run.
-	@Option(shortName="g") 
+	@Option(
+		shortName   = "g",
+		description = "Specify which groups of tests should be run. Fully "
+			+ "qualified class names can be used as well as their short "
+			+ "names."
+	) 
 	List<String> getGroup();
 	boolean isGroup();
 
 	// A list of names of groups of tests that should not be run
 	// although they may be referenced in a test group
-	@Option(shortName="l") 
+	@Option(
+		shortName   = "l",
+		description = "Specify which groups of tests should not be run. Fully "
+			+ "qualified class names can be used as well as their short "
+			+ "names."
+	) 
 	List<String> getLess();
 	boolean isLess();
 
 	// A list of individual tests that should be run
-	@Option(shortName="t") 
+	@Option(
+		shortName="t",
+		description = "Specify which tests should be run. Fully "
+			+ "qualified class names can be used as well as their short "
+			+ "names."
+	) 
 	List<String> getTest();
 	boolean isTest();
 
 	// A list of individual tests that should not be run
-	@Option(shortName="n") 
+	@Option(
+		shortName="n",
+		description = "Specify which tests should not be run. Fully "
+			+ "qualified class names can be used as well as their short "
+			+ "names."
+	) 
 	List<String> getNotest();
 	boolean isNotest();
 }
