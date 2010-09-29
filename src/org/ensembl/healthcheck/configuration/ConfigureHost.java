b6b8d1b9c8ea45db2b84ce1a214a4c54130a6a27
Michael Nuhn <mnuhn@ebi.ac.uk>
2010-09-29 08:56:21 +0000

    Added annotations that describe the options for the automatically generated help message

--- src/org/ensembl/healthcheck/configuration/ConfigureHost.java
+++ src/org/ensembl/healthcheck/configuration/ConfigureHost.java
@@ -12,38 +12,63 @@ import uk.co.flamingpenguin.jewel.cli.Option;
  */
 public interface ConfigureHost {
 
-	@Option	String getHost();
+	@Option	(description = "The host for the database server you wish to connect to.")
+	String getHost();
 	boolean isHost();
 
-	@Option	String getPort();
+	@Option	(description = "The port for the database server you wish to connect to.")	
+	String getPort();
 	boolean isPort();
 	
-	@Option	String getUser();
+	@Option	(description = "The user for the database server you wish to connect to.")	
+	String getUser();
 	boolean isUser();
 	
-	@Option	String getPassword();
+	@Option	(description = "The password (if required) for the database server you wish to connect to.")	
+	String getPassword();
 	boolean isPassword();
 
-	@Option	String getDriver();
+	@Option	(description = "The driver to be used for connecting to the database server")
+	String getDriver();
 	boolean isDriver();
 
-	@Option(longName="secondary.host")
+	@Option(
+			longName    = "secondary.host",
+			description = "Some tests require a second database. This "
+				+ "configures the hostname of the second database server."
+	)
 	String getSecondaryHost();
 	boolean isSecondaryHost();
 
-	@Option(longName="secondary.port")
+	@Option(
+			longName    = "secondary.port",
+			description = "Some tests require a second database. This "
+				+ "configures the port of the second database server."
+	)
 	String getSecondaryPort();
 	boolean isSecondaryPort();
 	
-	@Option(longName="secondary.user")
+	@Option(
+			longName    = "secondary.user",
+			description = "Some tests require a second database. This "
+				+ "configures the user name for the second database server."
+	)
 	String getSecondaryUser();
 	boolean isSecondaryUser();
 	
-	@Option(longName="secondary.password")
+	@Option(
+			longName    = "secondary.password",
+			description = "Some tests require a second database. This "
+				+ "configures the password for the second database server."
+	)
 	String getSecondaryPassword();
 	boolean isSecondaryPassword();
 
-	@Option(longName="secondary.driver")
+	@Option(
+			longName="secondary.driver",
+			description = "Some tests require a second database. This "
+				+ "configures the driver for the second database server."
+	)
 	String getSecondaryDriver();
 	boolean isSecondaryDriver();
 }
