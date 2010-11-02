7188c84e7be26c9329536acc79930a9d0379b91d
Pontus Larsson <pl4@sanger.ac.uk>
2010-11-02 16:07:20 +0000

    Healthcheck for comparing to database on secondary server

---
+++ src/org/ensembl/healthcheck/testcase/variation/ComparePreviousVersionAlleles.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2003 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.ensembl.healthcheck.testcase.variation;
+
+import java.util.Map;
+import org.ensembl.healthcheck.testcase.generic.ComparePreviousVersionBase;
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+
+/**
+ * Compare the number of alleles between the current database and the
+ * database on the secondary server.
+ */
+
+public class ComparePreviousVersionAlleles extends ComparePreviousVersionBase {
+
+	/**
+	 * Create a new testcase.
+	 */
+	public ComparePreviousVersionAlleles() {
+
+		addToGroup("variation");
+		addToGroup("variation-release");
+		setDescription("Compare the number of alleles in the current database with those from the equivalent database on the secondary server");
+	}
+
+	protected Map getCounts(DatabaseRegistryEntry dbre) {
+
+		return getCountsBySQL(dbre, "SELECT 'alleles', COUNT(*) FROM allele");
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected String entityDescription() {
+
+		return "number of";
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected double threshold() {
+
+		return 1;
+
+	}
+
+	// ------------------------------------------------------------------------
+
+} // ComparePreviousVersionAlleles
---
+++ src/org/ensembl/healthcheck/testcase/variation/ComparePreviousVersionGenotypes.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2003 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.ensembl.healthcheck.testcase.variation;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.ensembl.healthcheck.testcase.generic.ComparePreviousVersionBase;
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+
+/**
+ * Compare the number of genotypes between the current database and the
+ * database on the secondary server.
+ */
+
+public class ComparePreviousVersionGenotypes extends ComparePreviousVersionBase {
+
+	/**
+	 * Create a new testcase.
+	 */
+	public ComparePreviousVersionGenotypes() {
+
+		addToGroup("variation");
+		addToGroup("variation-release");
+		setDescription("Compare the number of genotypes in the current database with those from the equivalent database on the secondary server");
+	}
+
+	protected Map getCounts(DatabaseRegistryEntry dbre) {
+            
+            Map<String, Integer> counts = new HashMap<String, Integer>();
+            
+            // Count genotypes in population_genotype
+            counts.putAll(getCountsBySQL(dbre, "SELECT 'population_genotype', COUNT(*) FROM population_genotype"));
+            // Count genotypes in compressed_genotype_single_bp
+            counts.putAll(getCountsBySQL(dbre, "SELECT 'compressed_genotype_single_bp', COUNT(*) FROM compressed_genotype_single_bp"));
+            // Count genotypes in individual_genotype_multiple_bp
+            counts.putAll(getCountsBySQL(dbre, "SELECT 'individual_genotype_multiple_bp', COUNT(*) FROM individual_genotype_multiple_bp"));
+            
+            return counts;
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected String entityDescription() {
+
+		return "number of entries in";
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected double threshold() {
+
+		return 1;
+
+	}
+
+	// ------------------------------------------------------------------------
+
+} // ComparePreviousVersionGenotypes
