cf7708092cfedcd79b8dd06d34f11b17d952d049
Glenn Proctor <gp1@sanger.ac.uk>
2010-12-01 10:11:24 +0000

    Added havana_ig_gene to list of transcripts that don't need supporting features.
    Removed unused method.

--- src/org/ensembl/healthcheck/testcase/generic/CoreForeignKeys.java
+++ src/org/ensembl/healthcheck/testcase/generic/CoreForeignKeys.java
@@ -260,7 +260,7 @@ public class CoreForeignKeys extends SingleDatabaseTestCase {
 
 		// list of transcript analysis logic_names which are allowed to not have supporting features
 		String allowedNoSupporting = "('BGI_Augustus_geneset', 'BGI_Genewise_geneset', 'BGI_Genscan_geneset', 'zfish_RNASeq', 'gorilla_RNASeq', 'ccds_import', 'refseq_human_import',"
-				+ " 'Medaka_Genome_Project', 'oxford_FGU', 'MT_genbank_import', 'LRG_import', 'ncRNA', 'havana')";
+				+ " 'Medaka_Genome_Project', 'oxford_FGU', 'MT_genbank_import', 'LRG_import', 'ncRNA', 'havana', 'havana_ig_gene')";
 
 		String sql = "SELECT COUNT(*) FROM transcript t LEFT JOIN transcript_supporting_feature tsf ON t.transcript_id = tsf.transcript_id JOIN analysis a ON a.analysis_id=t.analysis_id WHERE a.analysis_id=t.analysis_id and tsf.transcript_id IS NULL AND a.logic_name NOT IN "
 				+ allowedNoSupporting;
@@ -293,21 +293,6 @@ public class CoreForeignKeys extends SingleDatabaseTestCase {
 		return true;
 
 	} // checkStableIDKeys
-
-	// -------------------------------------------------------------------------
-	/**
-	 * Gene table in otherfeatures databases can contain genes and ... other things, so need to do it differently depending on
-	 * analysis
-	 */
-	private boolean checkOtherfeaturesGeneStableIDs(DatabaseRegistryEntry dbre) {
-
-		boolean result = true;
-
-		String sql = "SELECT COUNT(*) FROM gene g LEFT JOIN gene_sta"; // XXX
-
-		return result;
-
-	} // checkOtherfeaturesGeneStableIDs
 
 	// -------------------------------------------------------------------------
 	public boolean checkKeysByEnsemblObjectType(Connection con, String baseTable, String type) {
