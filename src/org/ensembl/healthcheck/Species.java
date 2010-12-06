65de4442f767222240b31d6f8bcdb6e15975dbd4
Bronwen Aken <ba1@sanger.ac.uk>
2010-12-06 22:22:46 +0000

    Updated pig and marmoset assembly prefixes to what is actually used in the assembly.default table

--- src/org/ensembl/healthcheck/Species.java
+++ src/org/ensembl/healthcheck/Species.java
@@ -11,7 +11,7 @@ public enum Species {
 			"panda,giant panda,ailuropoda melanoleuca,ailuropoda_melanoleuca"), PAPIO_HAMADRYAS(9557, "Pham", "ENSPHA",
 			"baboon,Papio_hamadryas,papio_hamadryas,papio_hamadryas,sacred_baboon,western_baboon,red_baboon"), MELEAGRIS_GALLOPAVO(9103, "UMD2", "ENSMGA",
 			"turkey,common turkey,wild turkey,meleagris_gallopavo, meleagris_gallopavo"), MACROPUS_EUGENII(9315, "Meug", "ENSMEU",
-			"wallaby,tammar_wallaby,macropuseugenii,m_eugenii,tammarwallaby,Macropus eugenii,macropus_eugenii"), CALLITHRIX_JACCHUS(9483, "calJac", "ENSCJA",
+			"wallaby,tammar_wallaby,macropuseugenii,m_eugenii,tammarwallaby,Macropus eugenii,macropus_eugenii"), CALLITHRIX_JACCHUS(9483, "C_jacchus", "ENSCJA",
 			"marmoset,white-tufted-ear marmoset,callithrix_jacchus,callithrix jacchus,Callithrix_jacchus,Callithrix jacchus,callithrix"), CHOLOEPUS_HOFFMANNI(9358, "choHof", "ENSCHO",
 			"Sloth,Two-toed_sloth,Hoffmans_two-fingered_sloth,choloepus_hoffmanni"), GORILLA_GORILLA(9593, "gorGor", "ENSGGO", "gorilla,gorilla_gorilla,ggor"), TAENIOPYGIA_GUTTATA(59729, "taeGut",
 			"ENSTGU", "zebrafinch,zebra_finch,taeniopygia_guttata,taeniopygiaguttata,tguttata,poephila_guttata,taenopygia_guttata"), ORYCTOLAGUS_CUNICULUS(9986, "oryCun2", "ENSOCU",
@@ -20,7 +20,7 @@ public enum Species {
 			"culex,culexpipiens,culex_pipiens"), TAKIFUGU_RUBRIPES(31033, "FUGU", "ENSTRU", "pufferfish,fugu,frubripes,fugurubripes,fugu_rubripes,takifugu,trubripes,takifugurubripes,takifugu_rubripes"), CAENORHABDITIS_BRIGGSAE(
 			6238, "CBR", "", "briggsae,cbriggsae,caenorhabditisbriggsae,caenorhabditis_briggsae"), FELIS_CATUS(9685, "CAT", "ENSFCA", "cat,fcatus,felis,domestic_cat,felis_catus"), MUS_MUSCULUS(10090,
 			"NCBIM", "ENSMUS", "mouse,mmusculus,musmusculus,mus_musculus"), SPERMOPHILUS_TRIDECEMLINEATUS(43179, "SQUIRREL", "ENSSTO",
-			"squirrel,stridecemlineatus,thirteen-lined_ground_squirrel,spermophilus_tridecemlineatus_arenicola,spermophilus_tridecemlineatus"), SUS_SCROFA(9823, "PIG", "ENSSSC",
+			"squirrel,stridecemlineatus,thirteen-lined_ground_squirrel,spermophilus_tridecemlineatus_arenicola,spermophilus_tridecemlineatus"), SUS_SCROFA(9823, "Sscrofa", "ENSSSC",
 			"pig,boar,wildboar,wild_boar,susscrofa,sus_scrofa"), ERINACEUS_EUROPAEUS(9365, "HEDGEHOG", "ENSEEU", "hedgehog,european_hedgehog,eeuropaeus,erinaceus_europaeus"), MONODELPHIS_DOMESTICA(13616,
 			"BROADO", "ENSMOD", "opossum,monodelphis,mdomestica,mdomesticus,monodelphisdomestica,monodelphisdomesticus,monodelphis_domesticus,monodelphis_domestica"), RATTUS_NORVEGICUS(10116, "RGSC",
 			"ENSRNO", "rat,rnovegicus,rattusnorvegicus,rattus_norvegicus"), TETRAODON_NIGROVIRIDIS(99883, "TETRAODON", "IGNORE", "tetraodon,tnigroviridis,tetraodonnigroviridis,tetraodon_nigroviridis"), PONGO_PYGMAEUS(
