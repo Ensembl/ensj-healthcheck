1897b3f50735d468d369711236ffc3feb252f86e
Michael Nuhn <mnuhn@ebi.ac.uk>
2011-03-29 10:54:28 +0000

    The addTest method can now be used not only to add classes of testcases, but also classes of GroupOfTests. GroupOfTests don't have to be instantiated for that anymore. Several classes of GroupOfTests can be added by passing them to the addTest method and they can be mixed with classes of testcases. Added functionality so that this class now remembers which groups were added to it.

--- src/org/ensembl/healthcheck/GroupOfTests.java
+++ src/org/ensembl/healthcheck/GroupOfTests.java
@@ -1,68 +1,79 @@
 package org.ensembl.healthcheck;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 import java.util.Iterator;
 import org.ensembl.healthcheck.testcase.EnsTestCase;
 
 /**
- * 
- * This is the base class for groups of tests, it provides functionality
+ * <p>
+ * 	This is the base class for groups of tests, it provides functionality
  * for adding and removing tests and groups of tests using the addTest
  * and removeTest methods.
+ * </p>
  * 
  */
 public class GroupOfTests {
 	
-	public String getShortName() {
-		
-		return this.getClass().getSimpleName();		
-	}
+	final protected List<GroupOfTests> sourceGroups;
 	
+	public List<GroupOfTests> getSourceGroups() {
+		return sourceGroups;
+	}
+
+	final protected Set<Class<? extends EnsTestCase>> setOfTests;
+
 	public Set<Class<? extends EnsTestCase>> getSetOfTests() {
 		return setOfTests;
 	}
 
-	public void setSetOfTests(Set<Class<? extends EnsTestCase>> setOfTests) {
-		this.setOfTests = setOfTests;
+	public String getShortName() {		
+		return this.getClass().getSimpleName();		
+	}
+	
+	public GroupOfTests() {
+		this.setOfTests   = new HashSet<Class<? extends EnsTestCase>>();
+		this.sourceGroups = new ArrayList<GroupOfTests>();
 	}
 
-	protected Set<Class<? extends EnsTestCase>> setOfTests;
-	
-	protected Set<Class<? extends EnsTestCase>> getListOfTests() {
+	public Set<Class<? extends EnsTestCase>> getTestClasses() {
 		return setOfTests;
 	}
 
-	protected void setListOfTests(Set<Class<? extends EnsTestCase>> listOfTests) {
-		this.setOfTests = listOfTests;
-	}
-
-	public GroupOfTests() {
-		this.setOfTests = new HashSet<Class<? extends EnsTestCase>>();
-	}
-	
 	/**
-	 * @param nameOfTestClass
-	 * @throws ClassNotFoundException
+	 * <p>
+	 * 	Creates and returns a list of testclasses that are in this testgroup.
+	 * </p>
 	 * 
-	 * Uses the classloader to fetch the class specifies in nameOfTestClass
-	 * and add it to the set of tests for this group.
+	 * @return List<Class<? extends EnsTestCase>>
 	 * 
 	 */
-	public void addTest(String... nameOfTestClass) throws ClassNotFoundException {
+	public List<Class<? extends EnsTestCase>> getListOfTests() {
 		
-		for (String currentNameOfTestClass : nameOfTestClass) {
-				Class<? extends EnsTestCase> testClass = (Class<? extends EnsTestCase>) Class.forName(currentNameOfTestClass);		
-				this.setOfTests.add(testClass);
+		Iterator<Class<? extends EnsTestCase>> i = this.getSetOfTests().iterator();
+		
+		List<Class<? extends EnsTestCase>> list
+			= new LinkedList<Class<? extends EnsTestCase>>();
+		
+		while(i.hasNext()) {
+			
+			Class<? extends EnsTestCase> etc = i.next();
+			list.add(etc);
 		}
+		return list;
 	}
 
 	/**
 	 * @param nameOfTestClass
 	 * @throws ClassNotFoundException
 	 * 
-	 * Uses the classloader to fetch the class specifies in nameOfTestClass
+	 * <p>
+	 * 	Uses the classloader to fetch the class specifies in nameOfTestClass
 	 * and remove it from the set of tests for this group.
+	 * </p>
 	 * 
 	 */
 	public void removeTest(String... nameOfTestClass) throws ClassNotFoundException {
@@ -80,7 +91,81 @@ public class GroupOfTests {
 	 * 
 	 */
 	public void addTest(GroupOfTests groupOfTests) {
-		this.setOfTests.addAll(groupOfTests.getListOfTests());
+		this.setOfTests.addAll(groupOfTests.getTestClasses());
+	}
+	
+	/**
+	 * <p>
+	 * Adds the tests from a single test class to the set of tests to be
+	 * run. Additionally it keeps track of which groups were added. This
+	 * may be used by the GUI in the future to show the user of which
+	 * subgroups a group comprises.
+	 * </p>
+	 * 
+	 * @param groupOfTestsClass
+	 * 
+	 */
+	public void addTest(Class<? extends GroupOfTests> groupOfTestsClass) {
+		
+		GroupOfTests g;
+		try {
+			
+			g = groupOfTestsClass.newInstance();
+			
+		}
+		// Instantiation should always work. If not, we want to deal with
+		// failures here rather than forcing every group definition file to 
+		// deal with these problems.
+		//
+		catch (InstantiationException e) { throw new RuntimeException(e); }
+		//
+		// This would happen, if the constructor of the healthcheck was 
+		// private or protected.
+		//
+		catch (IllegalAccessException e) { throw new RuntimeException(e); }
+		
+		sourceGroups.add(g);		
+		addTest(g);
+	}
+
+	/**
+	 * <p>
+	 * 	Use this to add tests and groups of tests to the set of tests 
+	 * that you want to run.
+	 * </p>
+	 * 
+	 * @param testCasesOrGroupsOfTests
+	 * @throws ClassNotFoundException
+	 * 
+	 */
+	public void addTest(Class... testCasesOrGroupsOfTests) {
+		
+		for (Class testCaseOrGroupOfTests : testCasesOrGroupsOfTests) {
+	
+				/** 
+				 * The user could have passed a class that is neither a 
+				 * GroupOfTests nor an EnsTestCase. In that case the class
+				 * can't be handled this variable will indicate that.
+				 */
+				boolean canHandleThisClass = false;
+			
+				if (GroupOfTests.class.isAssignableFrom(testCaseOrGroupOfTests)) {
+					
+					addTest(testCaseOrGroupOfTests);
+					canHandleThisClass = true;
+				}
+				if (EnsTestCase.class.isAssignableFrom(testCaseOrGroupOfTests)) {
+					
+					setOfTests.add(testCaseOrGroupOfTests);
+					canHandleThisClass = true;
+				}
+				if (!canHandleThisClass) {
+					throw new RuntimeException(
+						"Don't know what to do with class "
+						+ testCaseOrGroupOfTests.getCanonicalName()
+					);
+				}
+		}
 	}
 	
 	/**
@@ -90,22 +175,9 @@ public class GroupOfTests {
 	 * 
 	 */
 	public void removeTest(GroupOfTests groupOfTests) {
-		this.setOfTests.removeAll(groupOfTests.getListOfTests());
+		this.setOfTests.removeAll(groupOfTests.getTestClasses());
 	}
 
-	/**
-	 * @param testClass
-	 * 
-	 * Adds a test class to the set of tests in this group.
-	 * 
-	 */
-	public void addTest(Class<? extends EnsTestCase>... testClass) {
-		for(Class<? extends EnsTestCase> test: testClass) {
-			setOfTests.add(test);
-		}
-	}
-	
-	
 	/**
 	 * @param tests
 	 * 
@@ -119,8 +191,10 @@ public class GroupOfTests {
 	}
 	
 	/**
-	 * Returns instance of all the tests in this group. Tests are instantiated
-	 * on the fly. 
+	 * <p>
+	 * 	Returns instance of all the tests in this group. Tests are instantiated
+	 * on the fly.
+	 * </p> 
 	 * 
 	 * @return
 	 */
@@ -142,15 +216,26 @@ public class GroupOfTests {
 	}
 	
 	/**
-	 * Useful for debugging. Prints the name of the class and which test 
+	 * <p>
+	 * 	Useful for debugging. Prints the name of the class and which test 
 	 * classes were defined to run.
+	 * </p>
+	 * 
 	 */
 	public String toString() {
 		
-		Iterator<Class<? extends EnsTestCase>> testIterator = this.getListOfTests().iterator();
+		Iterator<Class<? extends EnsTestCase>> testIterator = this.getTestClasses().iterator();
 		StringBuffer asString = new StringBuffer(); 
 		
 		asString.append("Class: " + this.getClass().getName() + "\n");
+		
+		asString.append("Has all tests from the following groups:\n");
+		
+		for (GroupOfTests sourceGroups : getSourceGroups()) {
+			
+			asString.append(" - " + sourceGroups.getClass().getCanonicalName() + "\n");
+		}
+		
 		asString.append("Tests defined:\n");
 		
 		while (testIterator.hasNext()) {
