aaf75ef530bd80e40351e2ce2a00a9fe269e3dba
Michael Nuhn <mnuhn@ebi.ac.uk>
2011-05-12 11:41:12 +0000

    Finally found out how to make the popup menu open at the first right mouse click and not with the second as it did before.

--- src/org/ensembl/healthcheck/eg_gui/JPopupTextArea.java
+++ src/org/ensembl/healthcheck/eg_gui/JPopupTextArea.java
@@ -103,10 +103,17 @@ class JPopupTextArea extends JTextArea
             this.component = component;
         }
 
-        //some systems trigger popup on mouse press, others on mouse release, we want to cater for both
-        private void showMenuIfPopupTrigger(MouseEvent e)
-        {
-            if (e.isPopupTrigger())
+        private void showMenuIfPopupTrigger(MouseEvent e) {
+        	
+        	// Using e.isPopupTrigger will cause the popup menu to open only
+        	// after the second right click. This makes the popup menu appear
+        	// lazy. The reason is that after the first right click only a
+        	// mouseReleased event is fired. The second right click fires all
+        	// three mousePressed, mouseClicked and mouseReleased events as 
+        	// expected.
+        	//
+        	if (e.getButton()==MouseEvent.BUTTON3)
+            //if (e.isPopupTrigger())
             {
                popup.show(component, e.getX() + 3, e.getY() + 3);
             }
@@ -114,21 +121,11 @@ class JPopupTextArea extends JTextArea
 
         //according to the javadocs on isPopupTrigger, checking for popup trigger on mousePressed and mouseReleased
         //should be all  that is required
-        //public void mouseClicked(MouseEvent e)  
-        //{
-        //    showMenuIfPopupTrigger(e);
-        //}
+        public void mouseClicked(MouseEvent e) {}
+        public void mousePressed(MouseEvent e) {}
 
-        public void mousePressed(MouseEvent e)
-        {
+        public void mouseReleased(MouseEvent e) {
             showMenuIfPopupTrigger(e);
         }
-
-        public void mouseReleased(MouseEvent e)
-        {
-            showMenuIfPopupTrigger(e);
-        }
-
     }
-
 }
