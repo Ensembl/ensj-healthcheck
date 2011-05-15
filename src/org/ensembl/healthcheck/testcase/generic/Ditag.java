1c3e07f63fdf9ea23d845d5a0e959248ca8e0921
Glenn Proctor <gp1@sanger.ac.uk>
2011-05-15 17:42:17 +0000

    Remove debug.

--- src/org/ensembl/healthcheck/testcase/generic/Ditag.java
+++ src/org/ensembl/healthcheck/testcase/generic/Ditag.java
@@ -261,7 +261,7 @@ public class Ditag extends SingleDatabaseTestCase {
 			// Check for ditag_ids that occur only once, ignore CAGE tags ("F")
 			String sql = "SELECT COUNT(*) AS singles FROM (select count(*) as count from ditag_feature df where analysis_id IN(" + analysis_ids
 					+ ") and df.ditag_side!='F' group by ditag_id, ditag_pair_id having count=1) as counter LIMIT 5;";
-			System.out.println(sql);
+
 			int count = 0;
 			rs = stmt.executeQuery(sql);
 			rs.next();
