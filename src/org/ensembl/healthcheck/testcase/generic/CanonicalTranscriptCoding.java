c4481c71f4ed0405d979f97258deb2ad1bea506d
Amonida Zadissa <amonida@ebi.ac.uk>
2011-05-17 09:28:42 +0000

    Added TR_C_gene, TR_J_gene, TR_V_gene and LRG_gene to the list of allowed gene biotypes where the type of the canonical transcript does not match the gene biotype.

--- src/org/ensembl/healthcheck/testcase/generic/CanonicalTranscriptCoding.java
+++ src/org/ensembl/healthcheck/testcase/generic/CanonicalTranscriptCoding.java
@@ -143,7 +143,7 @@ public class CanonicalTranscriptCoding extends SingleDatabaseTestCase {
 		// --------------------------------
 		// None of the transcripts that have a translation and have a biotype different to
 		// ('protein_coding','IG_C_gene','IG_D_gene','IG_J_gene','IG_V_gene')) should be canonical transcripts to any gene.
-		sql = "SELECT COUNT(*) FROM gene g WHERE g.canonical_transcript_id IN (select tr.transcript_id FROM transcript tr, translation tl WHERE tr.transcript_id=tl.transcript_id AND tr.biotype NOT IN ('rRNA','retrotransposed','protein_coding','IG_C_gene','IG_D_gene','IG_J_gene','IG_V_gene'";
+		sql = "SELECT COUNT(*) FROM gene g WHERE g.canonical_transcript_id IN (select tr.transcript_id FROM transcript tr, translation tl WHERE tr.transcript_id=tl.transcript_id AND tr.biotype NOT IN ('rRNA','retrotransposed','protein_coding','IG_C_gene','IG_D_gene','IG_J_gene','IG_V_gene', 'TR_C_gene','TR_J_gene','TR_V_gene', 'LRG_gene'";
 		if (dbre.getType() == DatabaseType.SANGER_VEGA) {// for sanger_vega ignore genes that do not have source havana or WU
 			sql += ", 'polymorphic_pseudogene','polymorphic','IG_gene','TR_gene')) and (g.source='havana' or g.source='WU')";
 		} else {
