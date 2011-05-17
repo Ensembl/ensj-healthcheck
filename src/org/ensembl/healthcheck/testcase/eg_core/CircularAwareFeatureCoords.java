440c0d72ca6d9f0bf5bd74c382cc0e197af9a6d4
Glenn Proctor <gp1@sanger.ac.uk>
2011-05-17 07:23:32 +0000

    Added constructor with removeFromAllGroups() to prevent this test being run as part of the Ensembl healthchecks.

--- src/org/ensembl/healthcheck/testcase/eg_core/CircularAwareFeatureCoords.java
+++ src/org/ensembl/healthcheck/testcase/eg_core/CircularAwareFeatureCoords.java
@@ -8,28 +8,28 @@ import org.ensembl.healthcheck.util.TemplateBuilder;
 
 public class CircularAwareFeatureCoords extends FeatureCoords {
 
-	public final static String START_END_SQL = "SELECT COUNT(*) FROM $tableName$ "
-			+ "WHERE seq_region_start > seq_region_end and "
-			+ "seq_region_id not in "
-			+ "(select seq_region_id from seq_region_attrib "
-			+ "join attrib_type using (attrib_type_id) "
-			+ "where code = 'circular_seq' and value=1)";
+	public final static String START_END_SQL = "SELECT COUNT(*) FROM $tableName$ " + "WHERE seq_region_start > seq_region_end and " + "seq_region_id not in "
+			+ "(select seq_region_id from seq_region_attrib " + "join attrib_type using (attrib_type_id) " + "where code = 'circular_seq' and value=1)";
 
+	public CircularAwareFeatureCoords() {
+	
+		super();
+		removeFromAllGroups();
+		
+	}
+	
 	@Override
 	protected boolean checkStartEnd(String tableName, Connection con) {
-		String sql = TemplateBuilder.template(START_END_SQL, "tableName",
-				tableName);
+		String sql = TemplateBuilder.template(START_END_SQL, "tableName", tableName);
 		boolean result = true;
 		// ------------------------
 		logger.info("Checking " + tableName + " for start > end");
 		int rows = getRowCount(con, sql);
 		if (rows > 0) {
-			ReportManager.problem(this, con, rows + " rows in " + tableName
-					+ " have seq_region_start > seq_region_end");
+			ReportManager.problem(this, con, rows + " rows in " + tableName + " have seq_region_start > seq_region_end");
 			result = false;
 		} else {
-			ReportManager.correct(this, con, "All rows in " + tableName
-					+ " have seq_region_start < seq_region_end");
+			ReportManager.correct(this, con, "All rows in " + tableName + " have seq_region_start < seq_region_end");
 		}
 		return result;
 	}
