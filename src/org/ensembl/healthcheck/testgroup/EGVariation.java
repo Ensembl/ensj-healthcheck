68b644fcabe85198f3cd6e6ede3f375d678a0134
Dan Staines <dstaines@ebi.ac.uk>
2011-06-24 15:33:50 +0000

    back from the dead

---
+++ src/org/ensembl/healthcheck/testgroup/EGVariation.java
@@ -0,0 +1,45 @@
+package org.ensembl.healthcheck.testgroup;
+
+import org.ensembl.healthcheck.GroupOfTests;
+import org.ensembl.healthcheck.testcase.variation.AlleleFrequencies;
+import org.ensembl.healthcheck.testcase.variation.CompareVariationSchema;
+import org.ensembl.healthcheck.testcase.variation.EmptyVariationTables;
+import org.ensembl.healthcheck.testcase.variation.FlankingUpDownSeq;
+import org.ensembl.healthcheck.testcase.variation.ForeignKeyCoreId;
+import org.ensembl.healthcheck.testcase.variation.IndividualType;
+import org.ensembl.healthcheck.testcase.variation.Meta;
+import org.ensembl.healthcheck.testcase.variation.Meta_coord;
+import org.ensembl.healthcheck.testcase.variation.StructuralVariation;
+import org.ensembl.healthcheck.testcase.variation.TranscriptVariation;
+import org.ensembl.healthcheck.testcase.variation.VFCoordinates;
+import org.ensembl.healthcheck.testcase.variation.VariationForeignKeys;
+import org.ensembl.healthcheck.testcase.variation.VariationSet;
+
+/**
+ * Group of tests for variation databases
+ * 
+ * @author dstaines
+ * 
+ */
+public class EGVariation extends GroupOfTests {
+
+	public EGVariation() {
+		addTest(
+			EGCommon.class,
+
+			AlleleFrequencies.class, 
+			CompareVariationSchema.class,
+			EmptyVariationTables.class, 
+			FlankingUpDownSeq.class,
+			ForeignKeyCoreId.class, 
+			IndividualType.class, 
+			Meta_coord.class,
+			Meta.class, 
+			StructuralVariation.class,
+			TranscriptVariation.class, 
+			VariationForeignKeys.class,
+			VariationSet.class, 
+			VFCoordinates.class
+		);
+	}
+}
