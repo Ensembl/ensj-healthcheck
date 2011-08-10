483859dee4fc16e72e9ca277630c44b5cdc12a9a
Michael Nuhn <mnuhn@ebi.ac.uk>
2011-08-10 15:24:26 +0000

    GroupsOfTests can have a description now.

--- src/org/ensembl/healthcheck/GroupOfTests.java
+++ src/org/ensembl/healthcheck/GroupOfTests.java
@@ -43,6 +43,16 @@ public class GroupOfTests {
 	 */
 	protected String name = "";
 	
+	protected String description = "";
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
