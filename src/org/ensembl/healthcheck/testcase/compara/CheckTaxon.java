a46c52a44f4f2e380a4bafed24986bd476f48d21
Leo Gordon <lg4@ebi.ac.uk>
2011-08-15 18:54:12 +0000

    another ad-hoc patch to bring the long classifications together

--- src/org/ensembl/healthcheck/testcase/compara/CheckTaxon.java
+++ src/org/ensembl/healthcheck/testcase/compara/CheckTaxon.java
@@ -190,8 +190,11 @@ public class CheckTaxon extends MultiDatabaseTestCase {
               while (!this_taxon_id.equals("0")) {
                 values1 = getRowValues(comparaCon,
                     "SELECT rank, parent_id, genbank_hidden_flag FROM ncbi_taxa_node WHERE taxon_id = " + this_taxon_id);
-                if ( //values1[2].equals("0") && 
-                !values1[1].equals("1") && !values1[1].equals("0") && !values1[0].equals("subgenus") && !values1[0].equals("subspecies")) {
+                if ( // values1[2].equals("0") &&       // we used to filter out entries with genbank_hidden_flag, we don't anymore
+                    !this_taxon_id.equals("33154") &&   // "Fungi/Metazoa" node (under various names) has to be excluded
+                    !values1[1].equals("1") && !values1[1].equals("0") &&
+                    !values1[0].equals("subgenus") && !values1[0].equals("subspecies")
+                ) {
                   String taxonName = getRowColumnValue(comparaCon,
                       "SELECT name FROM ncbi_taxa_name " +
                       "WHERE name_class = \"scientific name\" AND taxon_id = " + this_taxon_id);
