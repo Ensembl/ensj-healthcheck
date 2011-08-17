181e597010685466b18fb0a992d6ccc6b4780f5b
Michael Nuhn <mnuhn@ebi.ac.uk>
2011-08-17 13:49:58 +0000

    An abstract class that can be subclassed for tests that only want to send out a query and display the results.
    Theses tests can be used to assess what the data in the database looks like.
    
    The sql commands are executed by starting the mysql binary instead of jdbc, because the mysql binary renders the result as an ascii table.

---
+++ src/org/ensembl/healthcheck/testcase/MysqlCmd.java
@@ -0,0 +1,71 @@
+package org.ensembl.healthcheck.testcase;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+
+/**
+ * <p>
+ * 	Abstract class providing methods for subclasses used to assess what the 
+ * data looks like.
+ * </p>
+ * 
+ * <p>
+ * 	A subclass only has to set the sql and the description attribute.
+ * </p>
+ * 
+ * <p>
+ * 	The mysql client is the run to execute the query and the table returned
+ * will appear in the report.
+ * </p>
+ * 
+ * @author mnuhn
+ *
+ */
+public abstract class MysqlCmd extends AbstractShellBasedTestCase {
+
+	/**
+	 * This must be set by the subclass to the sql that will be executed.
+	 */
+	protected String sql;
+
+	/**
+	 * The command to call the mysql binary
+	 */
+	protected String mysqlCmd = "mysql";
+	
+	@Override
+	protected String[] createCommandLineArray(DatabaseRegistryEntry dbre, int speciesId) {
+		
+		String[] dbServerDetails = new String[] {
+			mysqlCmd,
+			"--host="     + dbre.getDatabaseServer().getHost().trim(),
+			"--port="     + dbre.getDatabaseServer().getPort().trim(),
+			"--user="     + dbre.getDatabaseServer().getUser().trim(),
+			"--password=" + dbre.getDatabaseServer().getPass().trim(),
+			dbre.getName(),
+			"-t",
+			"-e",
+			sql
+		};
+
+		return dbServerDetails;
+	}
+
+	public String getSql() {
+		return sql;
+	}
+
+	public void setSql(String sql) {
+		this.sql = sql;
+	}
+
+	public String getMysqlCmd() {
+		return mysqlCmd;
+	}
+
+	public void setMysqlCmd(String mysqlCmd) {
+		this.mysqlCmd = mysqlCmd;
+	}
+}
