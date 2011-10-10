eaf5dcab8139b8f94c8cdd2423feca7162dfd96c
Ensembl Account <ensembl@sanger.ac.uk>
2011-10-10 16:01:25 +0000

    mr6 : updated bushbaby assembly prefix

--- src/org/ensembl/healthcheck/Species.java
+++ src/org/ensembl/healthcheck/Species.java
@@ -55,7 +55,7 @@ public enum Species {
 	ORNITHORHYNCHUS_ANATINUS(9258, "OANA", "ENSOAN", "platypus,oanatius,ornithorhynchus_anatinus"),
 	ORYCTOLAGUS_CUNICULUS(9986, "oryCun", "ENSOCU", "rabbit,oryctolagus,domestic_rabbit,bunny,japanese_white_rabbit,european_rabbit,oryctolagus_cuniculus"),
 	ORYZIAS_LATIPES(8090, "MEDAKA", "ENSORL", "medaka,oryzias,japanese_medaka,japanese_rice_fish,japanese_ricefish,japanese_killifish,oryzias_latipes"),
-	OTOLEMUR_GARNETTII(30611, "BUSHBABY", "ENSOGA", "bushbaby,bush_baby,galago,small_eared_galago,ogarnettii,otolemur,otolemur_garnettii"),
+	OTOLEMUR_GARNETTII(30611, "OtoGar", "ENSOGA", "bushbaby,bush_baby,galago,small_eared_galago,ogarnettii,otolemur,otolemur_garnettii"),
 	PAN_TROGLODYTES(9598, "CHIMP", "ENSPTR", "chimp,chimpanzee,ptroglodytes,pantroglodytes,pan_troglodytes"),
 	PAPIO_HAMADRYAS(9557, "Pham", "ENSPHA", "baboon,Papio_hamadryas,papio_hamadryas,papio_hamadryas,sacred_baboon,western_baboon,red_baboon"),
 	PETROMYZON_MARINUS(7757, "petromyzon_marinus", "ENSPMA", "lamprey,sea_lamprey,pmarinus,petromyzon,petromyzon_marinus"),
