398b644377fca5f69c777076537b427124e486c0
Kieron Taylor <ktaylor@ebi.ac.uk>
2012-01-23 18:32:20 +0000

    Removed production DB temporary tables created by this healthcheck. Substituted with an in-memory DB, h2, that has a corresponding jar file.

--- src/org/ensembl/healthcheck/testcase/generic/XrefLevels.java
+++ src/org/ensembl/healthcheck/testcase/generic/XrefLevels.java
@@ -16,6 +16,8 @@
 
 package org.ensembl.healthcheck.testcase.generic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -73,86 +75,75 @@ public class XrefLevels extends MultiDatabaseTestCase {
 		boolean result = true;
 
 		// easier to do this in SQL than Java
+		// Create an in-memory SQL database via h2 driver
+		
+		try {
+			Class.forName("org.h2.Driver");
+			Connection tempDB = DriverManager.getConnection("jdbc:h2:~healthcheck"); // no login credential.
+			
+			createTempTable(tempDB);
 
-		// create a temporary table in the first database we come to to hold the
-		// master list of species, sources and objects
-		DatabaseRegistryEntry masterDBRE = null;
-		PreparedStatement masterPrep = null;
-
-		String masterTable = "";
-
-		DatabaseRegistryEntry[] dbres = dbr.getAll();
-
-		if (dbres.length == 0) {
-			return true;
-		}
-
-		for (DatabaseRegistryEntry dbre : dbres) {
-
-			if (masterDBRE == null) {
-
-				createTempTable(dbre);
-				masterDBRE = dbre;
-				masterTable = "healthcheck_xref";
-
-				try {
-
-					masterPrep = masterDBRE.getConnection().prepareStatement("INSERT INTO " + masterTable + " (species, source, object) VALUES (?,?,?)");
-
-				} catch (SQLException se) {
-					se.printStackTrace();
-				}
-
+			// master list of species, sources and objects
+			DatabaseRegistryEntry masterDBRE = null;
+			PreparedStatement masterPrep = null;
+			
+			String masterTable = "healthcheck_xref";
+			
+			DatabaseRegistryEntry[] dbres = dbr.getAll();
+			
+			if (dbres.length == 0) {
+				return true;
 			}
-
-			// fill with the list of sources and object types from each species
-			logger.fine("Adding sources and objects for " + dbre.getName());
-
-			try {
-
+			
+			for (DatabaseRegistryEntry dbre : dbres) {
+				
+				if (masterDBRE == null) {
+					masterDBRE = dbre;
+					masterPrep = tempDB.prepareStatement("INSERT INTO " + masterTable + " (species, source, object) VALUES (?,?,?)");
+				}
+				
+				// fill with the list of sources and object types from each species
+				logger.fine("Adding sources and objects for " + dbre.getName());
+				
 				Statement stmt = dbre.getConnection().createStatement();
-
+				
 				// can't do this in one query as the databases being written to and read from might be on separate servers
 				ResultSet rs = stmt
 						.executeQuery("SELECT e.db_name, ox.ensembl_object_type FROM external_db e, xref x, object_xref ox WHERE e.external_db_id=x.external_db_id AND x.xref_id=ox.xref_id GROUP BY e.db_name, ox.ensembl_object_type");
-
+				
 				while (rs.next()) {
 					masterPrep.setString(1, dbre.getSpecies().toString());
 					masterPrep.setString(2, rs.getString("db_name"));
 					masterPrep.setString(3, rs.getString("ensembl_object_type"));
 					masterPrep.execute();
 				}
-
+				
 				stmt.close();
-			} catch (SQLException se) {
-				se.printStackTrace();
+								
 			}
-
-		}
-
-		// find problems - loop over results grouped by source and object
-		// find situations where the source is the same but the species and object
-		// are different
-		try {
-
-			ResultSet rs = masterPrep.executeQuery("SELECT * FROM " + masterTable + " GROUP BY source, object");
-
+			masterPrep = tempDB.prepareStatement("SELECT * FROM " + masterTable + " GROUP BY source, object");
+			// find problems - loop over results grouped by source and object
+			// find situations where the source is the same but the species and object
+			// are different
+			
+			ResultSet rs = masterPrep.executeQuery();
+			
 			String source = "";
 			String object = "";
 			String species = "";
-
+			
 			String lastSource = "";
 			String lastObject = "";
 			String lastSpecies = "";
-
+			
 			while (rs.next()) {
-
+				
 				source = rs.getString("source");
 				object = rs.getString("object");
 				species = rs.getString("species");
-
+				
 				if (!source.equals("")) { // first time around is a special case
-
+					
 					if (source.equals(lastSource) && !species.equals(lastSpecies) && !object.equals(lastObject)) {
 						result = false;
 						ReportManager.problem(this, "", source + " is on " + object + " in some species (e.g. " + species + ") and " + lastObject + " in others (e.g." + lastSpecies + ")");
@@ -163,13 +154,21 @@ public class XrefLevels extends MultiDatabaseTestCase {
 				lastSource = source;
 				lastObject = object;
 				lastSpecies = species;
-
+				
 			}
-		} catch (SQLException se) {
-			se.printStackTrace();
+			
+			
+			dropTempTable(tempDB);
+			
+			tempDB.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block for making h2 database connection.
+			// Somebody do this properly!
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
-		dropTempTable(masterDBRE);
 
 		return result;
 
@@ -177,14 +176,13 @@ public class XrefLevels extends MultiDatabaseTestCase {
 
 	// -----------------------------------------------------------------------
 
-	private void createTempTable(DatabaseRegistryEntry dbre) {
+	private void createTempTable(Connection conn) {
 
-		// can't use a "real" temporary table here since connection pooling can lead to connections being closed
+		// making a temporary table in memory rather than affecting production DB
 		try {
-			Statement stmt = dbre.getConnection().createStatement();
-			stmt.execute("DROP TABLE IF EXISTS healthcheck_xref");
+			Statement stmt = conn.createStatement();
 			stmt.execute("CREATE TABLE healthcheck_xref (species VARCHAR(255), source VARCHAR(255), object VARCHAR(255))");
-			logger.fine("Created table healthcheck_xref in " + dbre.getName());
+			logger.fine("Created table healthcheck_xref in temporary DB");
 		} catch (SQLException se) {
 			se.printStackTrace();
 		}
@@ -193,12 +191,12 @@ public class XrefLevels extends MultiDatabaseTestCase {
 
 	// -----------------------------------------------------------------------
 
-	private void dropTempTable(DatabaseRegistryEntry dbre) {
+	private void dropTempTable(Connection conn) {
 
 		try {
-			Statement stmt = dbre.getConnection().createStatement();
+			Statement stmt = conn.createStatement();
 			stmt.execute("DROP TABLE IF EXISTS healthcheck_xref");
-			logger.fine("Dropped table healthcheck_xref in " + dbre.getName());
+			logger.fine("Dropped table healthcheck_xref in temporary DB");
 		} catch (SQLException se) {
 			se.printStackTrace();
 		}
