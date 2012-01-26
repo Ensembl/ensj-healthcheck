bfae88be474373662d47408d8caf2d78c9bfa7c5
Miguel Pignatelli <mp@ebi.ac.uk>
2012-01-26 14:57:41 +0000

    gene_trees instead of protein|nc trees

--- src/org/ensembl/healthcheck/testcase/compara/CheckFlatProteinTrees.java
+++ src/org/ensembl/healthcheck/testcase/compara/CheckFlatProteinTrees.java
@@ -54,15 +54,16 @@ public class CheckFlatProteinTrees extends AbstractTemplatedTestCase {
 		// Counts all nodes which do not represent a member, do not share the same
 		// id
 		// as their root and whose root id is not 0. Groups this by the root_id
-		String internalNodeCountSql = "SELECT ptn.root_id, count(*) AS internal_nodes FROM protein_tree_node ptn LEFT JOIN protein_tree_member ptm USING (node_id) WHERE ptm.node_id IS NULL AND ptn.node_id <> ptn.root_id AND ptn.root_id <> 0 GROUP BY ptn.root_id";
+        String internalNodeCountSql = "SELECT gtn.root_id, count(*) AS internal_nodes FROM gene_tree_node gtn LEFT JOIN gene_tree_member gtm USING (node_id) WHERE gtm.node_id IS NULL AND gtn.node_id <> gtn.root_id AND gtn.root_id <> 0 GROUP BY gtn.root_id";
 		// Counts all members per tree where root id is not 0, whose parent id is
 		// the same as the root id but have more than one of these per tree
-		String flatMemberCountSql = "SELECT ptn.root_id, count(*) AS root_members FROM protein_tree_node ptn JOIN protein_tree_member ptm USING (node_id) WHERE ptn.parent_id = ptn.root_id AND ptn.root_id <> 0 GROUP BY ptn.root_id having root_members > 1";
+        String flatMemberCountSql = "SELECT gtn.root_id, count(*) AS root_members FROM gene_tree_node gtn JOIN gene_tree_member gtm USING (node_id) WHERE gtn.parent_id = gtn.root_id AND gtn.root_id <> 0 GROUP BY gtn.root_id having root_members > 1";
 		// Count all members where root is not 0
-		String memberCountSql = "SELECT root_id, count(distinct root_id) from protein_tree_node where root_id <> 0 group by root_id";
+		String memberCountSql = "SELECT root_id, count(distinct root_id) from gene_tree_node where root_id <> 0 group by root_id";
 
 		Long totalTreesCount = template.queryForDefaultObject(
-				"select count(distinct root_id) from protein_tree_node", Long.class);
+				"select count(*) from gene_tree_root", Long.class);
+
 
 		Map<Long, Long> internalNodeCounts = template.queryForMap(
 				internalNodeCountSql, mapRowMapper);
