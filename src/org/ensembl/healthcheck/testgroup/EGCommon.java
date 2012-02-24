601f11faf310d2542323009882bf855e00d27863
Michael Nuhn <mnuhn@ebi.ac.uk>
2012-02-24 16:08:33 +0000

    Added SeqRegionLength

--- src/org/ensembl/healthcheck/testgroup/EGCommon.java
+++ src/org/ensembl/healthcheck/testgroup/EGCommon.java
@@ -1,12 +1,16 @@
 package org.ensembl.healthcheck.testgroup;
 
 import org.ensembl.healthcheck.GroupOfTests;
+import org.ensembl.healthcheck.testcase.eg_core.SeqRegionLength;
 import org.ensembl.healthcheck.testcase.generic.MySQLStorageEngine;
 
 public class EGCommon extends GroupOfTests {
 
 	public EGCommon() {
-		addTest(MySQLStorageEngine.class);
+		addTest(
+			MySQLStorageEngine.class,
+			SeqRegionLength.class
+		);
 	}
 
 }
