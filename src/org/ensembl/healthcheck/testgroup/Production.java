44416d1700ea77c673fa2fdbf62023905ec51b4f
Magali Ruffier <mr6@ebi.ac.uk>
2012-06-22 12:56:23 +0000

    added ProductionAnalysisLogicName test

--- src/org/ensembl/healthcheck/testgroup/Production.java
+++ src/org/ensembl/healthcheck/testgroup/Production.java
@@ -6,6 +6,7 @@ import org.ensembl.healthcheck.GroupOfTests;
  * These are the tests that register themselves as production. The tests are:
  * 
  * <ul>
+ *   <li> org.ensembl.healthcheck.testcase.generic.ProductionAnalysisLogicName </li> 
  *   <li> org.ensembl.healthcheck.testcase.generic.ProductionBiotypes </li> 
  *   <li> org.ensembl.healthcheck.testcase.generic.ProductionMasterTables </li> 
  *   <li> org.ensembl.healthcheck.testcase.generic.ProductionMeta </li> 
@@ -19,6 +20,7 @@ public class Production extends GroupOfTests {
 	public Production() {
 
 		addTest(
+                        org.ensembl.healthcheck.testcase.generic.ProductionAnalysisLogicName.class,
 			org.ensembl.healthcheck.testcase.generic.ProductionBiotypes.class,
 			org.ensembl.healthcheck.testcase.generic.ProductionMasterTables.class,
 			org.ensembl.healthcheck.testcase.generic.ProductionMeta.class
