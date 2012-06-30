968adfa175e7bec1368107db83d76e30ba257347
Bronwen Aken <ba1@sanger.ac.uk>
2012-06-30 06:45:40 +0000

    Added collared flycatcher

--- src/org/ensembl/healthcheck/Species.java
+++ src/org/ensembl/healthcheck/Species.java
@@ -37,6 +37,7 @@ public enum Species {
 	ECHINOPS_TELFAIRI(9371, "TENREC", "ENSETE", "tenrec,echinops,small_madagascar_hedgehog,lesser_hedgehog_tenrec,echinops_telfairi"),
 	EQUUS_CABALLUS(9796, "EquCab", "ENSECA", "horse,equus,mr_ed,ecaballus,equus_caballus"),
 	ERINACEUS_EUROPAEUS(9365, "HEDGEHOG", "ENSEEU", "hedgehog,european_hedgehog,eeuropaeus,erinaceus_europaeus"),
+	FICEDULA_ALBICOLLIS(59894, "FLYCATCHER", "ENSFAL", "flycatcher,falbicollis,collared_flycatcher,f_albicollis,ficalb"),
 	FELIS_CATUS(9685, "CAT", "ENSFCA", "cat,fcatus,felis,domestic_cat,felis_catus"),
         GADUS_MORHUA(8049, "gadMor", "ENSGMO", "cod,gadus_morhua,gmorhua,atlantic_cod"),
 	GALLUS_GALLUS(9031, "WASHUC", "ENSGAL", "chicken,chick,ggallus,gallusgallus,gallus_gallus"),
