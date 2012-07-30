f03fa8c7e4144082f82017a6a2cc9d2eac3242cc
Magali Ruffier <mr6@ebi.ac.uk>
2012-07-30 10:56:10 +0000

    reinstated the actual test to see if a feature's coordinate make sense with the slice it is on a bit of re-formatting of the other tests as well

--- src/org/ensembl/healthcheck/testcase/generic/FeatureCoords.java
+++ src/org/ensembl/healthcheck/testcase/generic/FeatureCoords.java
@@ -19,12 +19,25 @@
 package org.ensembl.healthcheck.testcase.generic;
 
 import java.sql.Connection;
+import java.util.Set;
+import java.util.Map;
+import java.util.logging.Logger;
+
+
 
 import org.ensembl.healthcheck.DatabaseRegistryEntry;
 import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.Team;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 import org.ensembl.healthcheck.util.DBUtils;
+import org.ensembl.healthcheck.util.SqlTemplate;
+import org.ensembl.healthcheck.util.Utils;
+import org.ensembl.healthcheck.util.RowMapper;
+import org.ensembl.healthcheck.util.DefaultMapRowMapper;
+import org.ensembl.healthcheck.util.DefaultObjectRowMapper;
+
+
+
 
 /**
  * Check that feature co-ords make sense.
@@ -58,53 +71,14 @@ public class FeatureCoords extends SingleDatabaseTestCase {
 		boolean result = true;
 
 		String[] featureTables = getCoreFeatureTables();
+                Connection con = dbre.getConnection();
 
 		for (int tableIndex = 0; tableIndex < featureTables.length; tableIndex++) {
 
 			String tableName = featureTables[tableIndex];
-
-			Connection con = dbre.getConnection();
-
-			// three separate queries to avoid using an OR
-
-			// ------------------------
-
-			logger.fine("Checking " + tableName + " for start < 1");
-			String sql = "SELECT COUNT(*) FROM " + tableName
-					+ " WHERE seq_region_start < 1";
-			int rows = DBUtils.getRowCount(con, sql);
-			if (rows > 0) {
-				ReportManager.problem(this, con, rows + " rows in " + tableName
-						+ " have seq_region_start < 1");
-				result = false;
-			} else {
-				ReportManager.correct(this, con, "All rows in " + tableName
-						+ " have seq_region_start >= 1");
-			}
-
-			if (!checkStartEnd(tableName, con)) {
-				result = false;
-			}
-
-			// ------------------------
-
-			/**
-			 * This section removed as it takes an inordinately long time for
-			 * not much useful result - can't find any combination of indices
-			 * that avoid full table scan // skip large tables as this test
-			 * takes an inordinately long time if
-			 * (tableName.equals("protein_align_feature") ||
-			 * tableName.equals("dna_align_feature") ||
-			 * tableName.equals("repeat_feature")) { continue; }
-			 * logger.info("Checking " + tableName + " for end > length"); sql =
-			 * "SELECT COUNT(*) FROM " + tableName + " f, seq_region s WHERE
-			 * f.seq_region_id = s.seq_region_id AND f.seq_region_end >
-			 * s.length"; rows = DBUtils.getRowCount(con, sql); if (rows > 0) {
-			 * ReportManager.problem(this, con, rows + " rows in " + tableName +
-			 * " have seq_region_end > length in seq_region_table"); result =
-			 * false; } else { ReportManager.correct(this, con, "All rows in " +
-			 * tableName + " have sensible lengths"); }
-			 */
+                        result &= checkStart(dbre, tableName);
+                        result &= checkStartEnd(dbre, tableName);
+                        result &= checkLength(dbre, tableName);
 
 		} // foreach table
 
@@ -112,6 +86,43 @@ public class FeatureCoords extends SingleDatabaseTestCase {
 
 	} // run
 
+
+        protected boolean checkLength(DatabaseRegistryEntry dbre, String tableName) {
+                SqlTemplate t = DBUtils.getSqlTemplate(dbre);
+                boolean result = true;
+                DefaultMapRowMapper<Integer, Integer> mapper = new DefaultMapRowMapper<Integer, Integer>(Integer.class, Integer.class);
+                String featureSQL = "SELECT seq_region_id, max(seq_region_start) from " + tableName + " group by seq_region_id ";
+                String lengthSQL = "SELECT length from seq_region where seq_region_id = ?";
+                String nameSQL = "SELECT name from seq_region where seq_region_id = ?";
+                Map<Integer, Integer> featureResults = t.queryForMap(featureSQL, mapper);
+
+                for (Map.Entry<Integer, Integer> entry : featureResults.entrySet()) {
+                        Integer max = entry.getValue();
+                        Integer region = entry.getKey();
+                        Integer length = t.queryForDefaultObject(lengthSQL, Integer.class, region);
+                        String name = t.queryForDefaultObject(nameSQL, String.class, region);
+
+                        if (max > length) {
+                                ReportManager.problem(this, dbre.getConnection(), "Table " + tableName + " has features which are longer than " + name);
+                                result = false;
+                        }
+                }
+                return result;
+
+        }
+
+
+        protected boolean checkStart(DatabaseRegistryEntry dbre, String tableName) {
+                String sql = "SELECT COUNT(*) FROM " + tableName + " WHERE seq_region_start < 1";
+                int rows = DBUtils.getRowCount(dbre.getConnection(), sql);
+                if (rows > 0) {
+                        ReportManager.problem(this, dbre.getConnection(), rows + " rows in " + tableName + " have seq_region_start < 1");
+                        return false;
+                 } else {
+                        return true;
+                 }
+
+        }
 	/**
 	 * Subroutine to carry out a check on whether the start is after the end.
 	 * This is to allow EG to skip this check for circular molecules
@@ -120,24 +131,15 @@ public class FeatureCoords extends SingleDatabaseTestCase {
 	 * @param con
 	 * @return
 	 */
-	protected boolean checkStartEnd(String tableName, Connection con) {
-		String sql;
-		int rows;
-		boolean result = true;
-		// ------------------------
-		logger.fine("Checking " + tableName + " for start > end");
-		sql = "SELECT COUNT(*) FROM " + tableName
-				+ " WHERE seq_region_start > seq_region_end";
-		rows = DBUtils.getRowCount(con, sql);
+	protected boolean checkStartEnd(DatabaseRegistryEntry dbre, String tableName) {
+		String sql = "SELECT COUNT(*) FROM " + tableName + " WHERE seq_region_start > seq_region_end";
+		int rows = DBUtils.getRowCount(dbre.getConnection(), sql);;
 		if (rows > 0) {
-			ReportManager.problem(this, con, rows + " rows in " + tableName
-					+ " have seq_region_start > seq_region_end");
-			result = false;
+			ReportManager.problem(this, dbre.getConnection(), rows + " rows in " + tableName + " have seq_region_start > seq_region_end");
+			return false;
 		} else {
-			ReportManager.correct(this, con, "All rows in " + tableName
-					+ " have seq_region_start < seq_region_end");
+                        return true;
 		}
-		return result;
 	}
 
 } // FeatureCoords
