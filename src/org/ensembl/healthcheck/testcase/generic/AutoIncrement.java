db3d3dca83d239ecb1dc4e347d574d5ae7526b00
Magali Ruffier <mr6@ebi.ac.uk>
2012-08-23 10:23:36 +0000

    updated the list of columns which require auto_increment

--- src/org/ensembl/healthcheck/testcase/generic/AutoIncrement.java
+++ src/org/ensembl/healthcheck/testcase/generic/AutoIncrement.java
@@ -34,12 +34,12 @@ import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 
 public class AutoIncrement extends SingleDatabaseTestCase {
 
-	String[] columns = { "alt_allele.alt_allele_id", "analysis.analysis_id", "assembly_exception.assembly_exception_id", "attrib_type.attrib_type_id", "coord_system.coord_system_id",
-			"density_feature.density_feature_id", "density_type.density_type_id", "ditag.ditag_id", "ditag_feature.ditag_feature_id", "dna_align_feature.dna_align_feature_id", "exon.exon_id",
-			"gene.gene_id", "karyotype.karyotype_id", "map.map_id", "mapping_session.mapping_session_id", "marker.marker_id", "marker_feature.marker_feature_id", "marker_synonym.marker_synonym_id",
-			"meta.meta_id", "misc_feature.misc_feature_id", "misc_set.misc_set_id", "object_xref.object_xref_id", "peptide_archive.peptide_archive_id", "prediction_exon.prediction_exon_id",
+	String[] columns = { "alt_allele.alt_allele_id", "analysis.analysis_id", "assembly_exception.assembly_exception_id", "attrib_type.attrib_type_id", "coord_system.coord_system_id", "data_file.data_file_id",  
+			"density_feature.density_feature_id", "density_type.density_type_id", "ditag.ditag_id", "ditag_feature.ditag_feature_id", "dna_align_feature.dna_align_feature_id", "exon.exon_id", "external_db.external_db_id", 
+			"gene.gene_id", "intron_supporting_evidence.intron_supporting_evidence_id", "karyotype.karyotype_id", "map.map_id", "mapping_session.mapping_session_id", "marker.marker_id", "marker_feature.marker_feature_id", "marker_synonym.marker_synonym_id",
+			"meta.meta_id", "misc_feature.misc_feature_id", "misc_set.misc_set_id", "object_xref.object_xref_id", "operon.operon_id", "peptide_archive.peptide_archive_id", "prediction_exon.prediction_exon_id",
 			"prediction_transcript.prediction_transcript_id", "protein_align_feature.protein_align_feature_id", "protein_feature.protein_feature_id", "qtl.qtl_id", "qtl_synonym.qtl_synonym_id",
-			"repeat_consensus.repeat_consensus_id", "repeat_feature.repeat_feature_id", "seq_region.seq_region_id", "simple_feature.simple_feature_id", "splicing_event.splicing_event_id",
+			"repeat_consensus.repeat_consensus_id", "repeat_feature.repeat_feature_id", "seq_region.seq_region_id", "seq_region_synonym.seq_region_synonym_id", "simple_feature.simple_feature_id", "splicing_event.splicing_event_id",
 			"transcript.transcript_id", "translation.translation_id", "unmapped_object.unmapped_object_id", "unmapped_reason.unmapped_reason_id", "xref.xref_id" };
 
 	/**
