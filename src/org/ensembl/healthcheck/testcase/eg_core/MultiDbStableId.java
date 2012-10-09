348b486789454f45a746a050500415cf1dc0ad57
Dan Staines <dstaines@ebi.ac.uk>
2012-10-09 14:58:13 +0000

    exclude otherfeatures

--- src/org/ensembl/healthcheck/testcase/eg_core/MultiDbStableId.java
+++ src/org/ensembl/healthcheck/testcase/eg_core/MultiDbStableId.java
@@ -30,6 +30,7 @@ public class MultiDbStableId extends MultiDatabaseTestCase {
 	private final static String STABLE_ID = "select o.stable_id from %obj% o";
 	private final static String[] OBJ_TYPES = { "gene", "transcript",
 			"translation", "exon" };
+	private final int MAX_REPORTS = 10;
 
 	@Override
 	public boolean run(DatabaseRegistry dbr) {
@@ -39,36 +40,56 @@ public class MultiDbStableId extends MultiDatabaseTestCase {
 			Map<String, Collection<String>> stableIds = CollectionUtils
 					.createHashMap();
 			for (DatabaseRegistryEntry coreDb : dbr.getAll(DatabaseType.CORE)) {
-				ReportManager.info(this, coreDb.getConnection(), "Checking "
-						+ objectType + " stable ID for " + coreDb.getName());
-				ConnectionBasedSqlTemplateImpl template = new ConnectionBasedSqlTemplateImpl(
-						coreDb.getConnection());
-				int checked = 0;
-				int dups = 0;
-				for (String stableId : template.queryForDefaultObjectList(
-						query, String.class)) {
-					checked++;
-					Collection<String> dbs = stableIds.get(stableId);
-					if (dbs == null) {
-						dbs = CollectionUtils.createArrayList(1);
-						stableIds.put(stableId, dbs);
-					} else {
-						dups++;
-						ReportManager
-								.problem(
-										this,
-										coreDb.getConnection(),
-										"The "
-												+ objectType
-												+ " stable ID has been found in the following core databases :"
-												+ StringUtils.join(dbs, ", "));
-						result = false;
+				if (!coreDb.getName().contains(
+						DatabaseType.OTHERFEATURES.getName())) {
+					ReportManager.info(this, coreDb.getConnection(),
+							"Checking " + objectType + " stable ID for "
+									+ coreDb.getName());
+					ConnectionBasedSqlTemplateImpl template = new ConnectionBasedSqlTemplateImpl(
+							coreDb.getConnection());
+					int checked = 0;
+					int dups = 0;
+					for (String stableId : template.queryForDefaultObjectList(
+							query, String.class)) {
+						checked++;
+						Collection<String> dbs = stableIds.get(stableId);
+						if (dbs == null) {
+							dbs = CollectionUtils.createArrayList(1);
+							stableIds.put(stableId, dbs);
+						} else {
+							dups++;
+							if (dups <= MAX_REPORTS) {
+								ReportManager
+										.problem(
+												this,
+												coreDb.getConnection(),
+												"The "
+														+ objectType
+														+ " stable ID "
+														+ stableId
+														+ " has been found in the following core databases :"
+														+ StringUtils.join(dbs,
+																", "));
+								if (dups == MAX_REPORTS) {
+									ReportManager
+											.problem(
+													this,
+													coreDb.getConnection(),
+													MAX_REPORTS
+															+ " duplications have been found for  "
+															+ objectType
+															+ " stable ID in this database - no more will be reported");
+								}
+							}
+							result = false;
+						}
+						dbs.add(coreDb.getName());
 					}
-					dbs.add(coreDb.getName());
+					ReportManager.info(this, coreDb.getConnection(), "Checked "
+							+ checked + " " + objectType + " stable IDs for "
+							+ coreDb.getName() + ": found " + dups
+							+ " duplicates");
 				}
-				ReportManager.info(this, coreDb.getConnection(), "Checked "
-						+ checked + " " + objectType + " stable IDs for "
-						+ coreDb.getName() + ": found " + dups + " duplicates");
 			}
 		}
 		return result;
