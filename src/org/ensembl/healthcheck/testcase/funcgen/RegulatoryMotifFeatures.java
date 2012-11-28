b4ea6cdbcb58184d57f6dbe24a732cbaf9f2d083
Nathan Johnson <njohnson@ebi.ac.uk>
2012-11-28 17:04:45 +0000

    corrected queries and now ignores archived sets

--- src/org/ensembl/healthcheck/testcase/funcgen/RegulatoryMotifFeatures.java
+++ src/org/ensembl/healthcheck/testcase/funcgen/RegulatoryMotifFeatures.java
@@ -60,33 +60,35 @@ public class RegulatoryMotifFeatures extends SingleDatabaseTestCase {
 	public boolean run(DatabaseRegistryEntry dbre) {
 
 		boolean result = true;
-
 		Connection con = dbre.getConnection();
 
-
-		int regMFs = DBUtils.getRowCount(con, "SELECT COUNT(distinct attribute_feature_id) from regulatory_attribute where attribute_feature_table='motif'");
-
-		//Nested join now accounts for focus_max_length
-		int fmaxLength = 2000;  // Should add this as attribute to FuncgenSingleDatabaseTestCase?
     
-    //This first test needs tweaking to take account of out of bounds?
-    //Don't we already do that with fmaxLength
+    // NOTE: SQL strings unbroken to allow easy copying into MySQL client
 
+    // Restricts to current i.e. non-archive fset which have a v[0-9]+ suffix
+    // This has to be a distnct count here as MF reg attrs are non-redundant across AFs within an RF
+    // Hence the 2nd count would always be higher
+    // Need change this to iterate over vell type build to capture differences within cell lines, which maybe currently masked
+    // by the cell type wide distinct count.
+
+		int regMFs = DBUtils.getRowCount(con, "SELECT count(distinct attribute_feature_id) from regulatory_attribute ra join regulatory_feature rf on ra.regulatory_feature_id=rf.regulatory_feature_id and attribute_feature_table='motif' join feature_set fs on rf.feature_set_id=fs.feature_set_id and fs.name not rlike '.*_v[0-9]+'");
+    //"SELECT COUNT(distinct attribute_feature_id) from regulatory_attribute where attribute_feature_table='motif'"
+
+
+		int fmaxLength = 2000;  // Accounts for potential out of bounds MFs from demoted TFs
+  
 		int regAMFs = DBUtils.getRowCount
-			( con, "select count(distinct amf.motif_feature_id) from " + 
-			  "associated_motif_feature amf, annotated_feature af, regulatory_attribute ra " + 
-			  "where af.annotated_feature_id=amf.annotated_feature_id and " + 
-			  "ra.attribute_feature_id=af.annotated_feature_id and " + 
-			  "ra.attribute_feature_table='annotated' and " + 
-			  "(af.seq_region_end - af.seq_region_start +1) <= " + fmaxLength);
+        ( con, "select count(distinct amf.motif_feature_id) from associated_motif_feature amf, annotated_feature af, regulatory_attribute ra, regulatory_feature rf, feature_set fs where af.annotated_feature_id=amf.annotated_feature_id and ra.attribute_feature_id=af.annotated_feature_id and ra.attribute_feature_table='annotated' and (af.seq_region_end - af.seq_region_start +1) <= " + fmaxLength +" and ra.regulatory_feature_id=rf.regulatory_feature_id and rf.feature_set_id=fs.feature_set_id and fs.name not rlike '.*_v[0-9]+'");
 
-		
+    
+    
 		if(regMFs != regAMFs){
         // This incorporates mfs for afs < 2000bp where the af has been integrated into the rf, but the mf hasn't for some reason?
-
+        // Or when  we have deleted an af with but not an associated mf that were both supporting an rf  
+        
         ReportManager.problem
-				( this,  con, "The number of motif features associated to regulatory features (" + regMFs +
-				  ") does not correspond to the number of motif features within its associated annotated features (" 
+				( this,  con, "The number of total non-distinct motif features associated to regulatory features (" + regMFs +
+				  ") does not correspond to the number of distinct motif features within its associated annotated features (" 
 				  + regAMFs + ") which are less than " + fmaxLength + " bp\n" +
 				  "USEFUL SQL:\nALTER table regulatory_attribute add index `attribute_id_type`(attribute_feature_table, attribute_feature_id);\n" +
 				  "insert ignore into regulatory_attribute select ra.regulatory_feature_id, amf.motif_feature_id, 'motif' from " + 
@@ -96,35 +98,23 @@ public class RegulatoryMotifFeatures extends SingleDatabaseTestCase {
 				  "ra.attribute_feature_table='annotated' and (af.seq_region_end - af.seq_region_start +1) <= " + 
 				  fmaxLength + " and ra1.attribute_feature_id is NULL;\nALTER table regulatory_attribute drop index `attribute_id_type`;"
 				  );
-			//This now takes seconds with the new temporary index
+	
 			
-			result = false;
+        result = false;
 		}
 
-
-    //The above only catches MFs that have been missed or when we have deleted an af with but not an associated mf that were both supporting an rf
-
-    
-    //Need to add this one
-    //Out of core region query.
-		//select mf.motif_feature_id, mf.seq_region_start, mf.seq_region_end, rf.seq_region_start, rf.seq_region_end, rf.regulatory_feature_id from feature_set fs, regulatory_feature rf, regulatory_attribute ra, motif_feature mf where fs.name!='RegulatoryFeatures:MultiCell' and fs.feature_set_id=rf.feature_set_id and rf.regulatory_feature_id=ra.regulatory_feature_id and ra.attribute_feature_table='motif' and ra.attribute_feature_id=mf.motif_feature_id and (mf.seq_region_start<rf.seq_region_start OR mf.seq_region_end>rf.seq_region_end) 
-    
-
+   
 		int outOfBoundMFs = DBUtils.getRowCount 
         (con, 
-         "SELECT count(mf.motif_feature_id) FROM feature_set fs, regulatory_feature rf, regulatory_attribute ra, motif_feature mf " + 
-         "WHERE fs.feature_set_id=rf.feature_set_id and rf.regulatory_feature_id=ra.regulatory_feature_id and ra.attribute_feature_table='motif'" + 
-         " and ra.attribute_feature_id=mf.motif_feature_id and (mf.seq_region_start<rf.seq_region_start OR mf.seq_region_end>rf.seq_region_end)");
+         "SELECT count(mf.motif_feature_id) FROM feature_set fs, regulatory_feature rf, regulatory_attribute ra, motif_feature mf WHERE fs.feature_set_id=rf.feature_set_id and rf.regulatory_feature_id=ra.regulatory_feature_id and ra.attribute_feature_table='motif' and ra.attribute_feature_id=mf.motif_feature_id and (mf.seq_region_start<rf.seq_region_start OR mf.seq_region_end>rf.seq_region_end) and fs.name not rlike '.*_v[0-9]+'");
+
 
     if(outOfBoundMFs != 0){
         
         ReportManager.problem
             ( this,  con,
               "Found " + outOfBoundMFs + " MotifFeatures which lie outside the core region. USEFUL SQL:\n" +
-              "SELECT mf.motif_feature_id, mf.seq_region_start, mf.seq_region_end, rf.regulatory_feature_id, rf.seq_region_start, rf.seq_region_end, concat(rf.stable_id, 11 0) " +
-              "FROM feature_set fs, regulatory_feature rf, regulatory_attribute ra, motif_feature mf " + 
-              "WHERE fs.feature_set_id=rf.feature_set_id and rf.regulatory_feature_id=ra.regulatory_feature_id and ra.attribute_feature_table='motif'" + 
-              " and ra.attribute_feature_id=mf.motif_feature_id and (mf.seq_region_start<rf.seq_region_start OR mf.seq_region_end>rf.seq_region_end)");
+              "SELECT mf.motif_feature_id, mf.seq_region_start, mf.seq_region_end, rf.regulatory_feature_id, rf.seq_region_start, rf.seq_region_end, lpad(rf.stable_id, 11, 0) FROM feature_set fs, regulatory_feature rf, regulatory_attribute ra, motif_feature mf WHERE fs.feature_set_id=rf.feature_set_id and rf.regulatory_feature_id=ra.regulatory_feature_id and ra.attribute_feature_table='motif' and ra.attribute_feature_id=mf.motif_feature_id and (mf.seq_region_start<rf.seq_region_start OR mf.seq_region_end>rf.seq_region_end) and fs.name not rlike '.*_v[0-9]+'");
               
               result = false;
     }
