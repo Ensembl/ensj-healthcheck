189a3474aba01698c68d72058e59ae4909f2f319
Michael Nuhn <mnuhn@ebi.ac.uk>
2012-12-06 14:59:10 +0000

    Use checksums for this, because it is a large table.

--- src/org/ensembl/healthcheck/testcase/eg_compara/ControlledTableNcbiTaxaName.java
+++ src/org/ensembl/healthcheck/testcase/eg_compara/ControlledTableNcbiTaxaName.java
@@ -1,7 +1,12 @@
 package org.ensembl.healthcheck.testcase.eg_compara;
 
 public class ControlledTableNcbiTaxaName extends AbstractControlledTable {
+	
 	@Override protected String getControlledTableName() {
 		return "ncbi_taxa_name";	
+	}
+	
+	@Override protected ComparisonStrategy getComparisonStrategy() {
+		return ComparisonStrategy.Checksum;
 	}
 }
--- src/org/ensembl/healthcheck/testcase/eg_compara/ControlledTableNcbiTaxaNode.java
+++ src/org/ensembl/healthcheck/testcase/eg_compara/ControlledTableNcbiTaxaNode.java
@@ -1,7 +1,13 @@
 package org.ensembl.healthcheck.testcase.eg_compara;
 
 public class ControlledTableNcbiTaxaNode extends AbstractControlledTable {
+	
 	@Override protected String getControlledTableName() {
 		return "ncbi_taxa_node";	
 	}
+	
+	@Override protected ComparisonStrategy getComparisonStrategy() {
+		return ComparisonStrategy.Checksum;
+	}
+
 }
