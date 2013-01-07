8eb87fa6d928b93ed4074a49dcce7f16990438fb
Magali Ruffier <mr6@ebi.ac.uk>
2013-01-07 14:28:54 +0000

    added test on species.url should be identical in species and production database, and should match pattern Homo_sapiens

--- src/org/ensembl/healthcheck/testcase/generic/ProductionSpeciesAlias.java
+++ src/org/ensembl/healthcheck/testcase/generic/ProductionSpeciesAlias.java
@@ -3,6 +3,7 @@ package org.ensembl.healthcheck.testcase.generic;
 import java.sql.Connection;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.ensembl.healthcheck.DatabaseRegistryEntry;
@@ -13,6 +14,9 @@ import org.ensembl.healthcheck.Team;
 import org.ensembl.healthcheck.Species;
 import org.ensembl.healthcheck.testcase.Priority;
 import org.ensembl.healthcheck.util.DBUtils;
+import org.ensembl.healthcheck.util.SqlTemplate;
+import java.util.regex.Matcher;
+
 
 public class ProductionSpeciesAlias extends SingleDatabaseTestCase {
   
@@ -52,31 +56,57 @@ public class ProductionSpeciesAlias extends SingleDatabaseTestCase {
           "AND s.db_name = '" + species + "' AND " +
           "s.is_current = 1 AND sa.is_current = 1");
 
-    Collection<String> dbOnly = (Collection<String>)CollectionUtils.subtract(dbAliases, productionAliases);
+// Looking for aliases which have been added directly into the species database
+// These should always be added to the production database, then synced across using the populate_species_meta script
+    result &= checkHasAlias(dbre, dbAliases, productionAliases, "production");
 
-    if (!dbOnly.isEmpty()) {
-      for (String key: dbOnly) {
-        String msg = String.format("Species alias '%s' is not in the production database, did you forget to add it?");
-        ReportManager.problem(this, con, msg);
-      }
-      result = false;
-    } else {
-      ReportManager.correct(this, con, "All aliases in the core database are also in the production database");
-    }
+// Looking for aliases which are missing from the species database
+// This means the populate_species_meta script has not been run since the entry was added to the production database
+// The populate_species_meta script is located in ensembl/misc-scripts/production_database/scripts
+    result &= checkHasAlias(dbre, productionAliases, dbAliases, "species");
 
-    Collection<String> prodOnly = (Collection<String>)CollectionUtils.subtract(productionAliases, dbAliases);
-
-    if (!prodOnly.isEmpty()) {
-      for (String key: prodOnly) {
-        String msg = String.format("Species alias '%s' is not in the core database, did you forget to run the populate_species_meta script?");
-        ReportManager.problem(this, con, msg);
-      }
-      result = false;
-    } else {
-      ReportManager.correct(this, con, "All aliases in the production database are also in the core database");
-    }
+    result &= checkUrl(dbre, prodDbre, species);
 
     return result;
-  }
+
+    }
+
+    private <T extends CharSequence> boolean checkHasAlias(DatabaseRegistryEntry dbre, Collection<T> core, Collection<T> toRemove, String type) {
+      Collection<String> dbOnly = (Collection<String>)CollectionUtils.subtract(core, toRemove);
+      if (dbOnly.isEmpty()) {
+        return true;
+      } else {
+        for (String key: dbOnly) {
+          String msg = String.format("Species alias '%s' is not in the %s database", key, type);
+          ReportManager.problem(this, dbre.getConnection(), msg);
+        }
+        return false;
+      }
+    }
+
+// Looking for species URL name
+// Should be both in the production and the core databases
+// Should start with a capital letter and have underscores between the names
+    private <T extends CharSequence> boolean checkUrl(DatabaseRegistryEntry dbre, DatabaseRegistryEntry prodDbre, String species) {
+      SqlTemplate t = DBUtils.getSqlTemplate(dbre);
+      SqlTemplate prodt = DBUtils.getSqlTemplate(prodDbre);
+      String sql = "SELECT meta_value FROM meta WHERE meta_key = 'species.url'";
+      String prodSql = "SELECT url_name FROM species WHERE db_name = ?"; 
+      String url = t.queryForDefaultObject(sql, String.class);
+      String prodUrl = prodt.queryForDefaultObject(prodSql, String.class, species);
+      if (url.equals(prodUrl)) {
+        if (url.matches("^[A-Z]{1}[a-z]*(_[a-z]*){1,2}")) {
+          ReportManager.correct(this, dbre.getConnection(), "species.url " + url + " is the same in both databases and is in the correct format");
+          return true;
+        } else {
+          ReportManager.problem(this, dbre.getConnection(), "species.url " + url + " is not in the correct format. Should start with a capital letter and have underscores to separate names");
+          return false;
+        }
+      } else {
+        ReportManager.problem(this, dbre.getConnection(), "species.url " + url + " in database does not match " + prodUrl + " in the production database");
+        return false;
+      }
+    }
+
 
 }
