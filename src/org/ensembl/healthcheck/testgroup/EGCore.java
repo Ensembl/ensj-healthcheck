27ded2d9bfccfbdd2191255ee0bb687b575c7300
Dan Staines <dstaines@ebi.ac.uk>
2013-02-26 10:24:52 +0000

    removed some production HCs we don't need to use

--- src/org/ensembl/healthcheck/testgroup/EGCore.java
+++ src/org/ensembl/healthcheck/testgroup/EGCore.java
@@ -1,14 +1,10 @@
 package org.ensembl.healthcheck.testgroup;
 
 import org.ensembl.healthcheck.GroupOfTests;
-import org.ensembl.healthcheck.testcase.eg_core.SeqRegionsConsistentWithComparaMaster;
 import org.ensembl.healthcheck.testcase.generic.*;
 
 import org.ensembl.healthcheck.testcase.generic.AnalysisTypes;
-import org.ensembl.healthcheck.testcase.generic.CheckDeclarations;
-import org.ensembl.healthcheck.testcase.generic.ProductionAnalysisLogicName;
 import org.ensembl.healthcheck.testcase.generic.ProductionBiotypes;
-import org.ensembl.healthcheck.testcase.generic.ProductionMeta;
 import org.ensembl.healthcheck.testcase.generic.TranscriptNames;
 
 /**
@@ -21,25 +17,20 @@ import org.ensembl.healthcheck.testcase.generic.TranscriptNames;
 public class EGCore extends GroupOfTests {
 
 	public EGCore() {
-		
+
 		setDescription("Supergroup of tests for core databases from Ensembl Genomes.");
-		
-		addTest(
-			EGCoreGeneModel.class,
-			EGCoreMeta.class,
-			EGCoreAnnotation.class,
-			EGCoreCompare.class,
-			EGCommon.class,
-			EGCoreMulti.class,
-			AnalysisTypes.class,
-			CheckDeclarations.class,
-			//ProductionAnalysisLogicName.class,
-			ProductionBiotypes.class,
-			//ProductionMeta.class,
-			TranscriptNames.class,
-			ControlledCoreTables.class,
-            AnalysisLogicName.class,
-            SeqRegionsConsistentWithComparaMaster.class
-		);		
+
+		addTest(EGCoreGeneModel.class, EGCoreMeta.class,
+				EGCoreAnnotation.class, EGCoreCompare.class, EGCommon.class,
+				EGCoreMulti.class,
+				AnalysisTypes.class,
+				// CheckDeclarations.class,
+				// ProductionAnalysisLogicName.class,
+				ProductionBiotypes.class,
+				// ProductionMeta.class,
+				TranscriptNames.class, ControlledCoreTables.class,
+				AnalysisLogicName.class
+		// SeqRegionsConsistentWithComparaMaster.class
+		);
 	}
 }
