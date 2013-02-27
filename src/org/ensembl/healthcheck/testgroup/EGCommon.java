676b6dd3dcc505880830c2ceb2705cfef7c1ab1e
Michael Nuhn <mnuhn@ebi.ac.uk>
2013-02-27 14:15:32 +0000

    Added NoRepeatFeatures test

--- src/org/ensembl/healthcheck/testgroup/EGCommon.java
+++ src/org/ensembl/healthcheck/testgroup/EGCommon.java
@@ -1,6 +1,7 @@
 package org.ensembl.healthcheck.testgroup;
 
 import org.ensembl.healthcheck.GroupOfTests;
+import org.ensembl.healthcheck.testcase.eg_core.NoRepeatFeatures;
 import org.ensembl.healthcheck.testcase.eg_core.SeqRegionLength;
 import org.ensembl.healthcheck.testcase.generic.MySQLStorageEngine;
 
@@ -9,7 +10,8 @@ public class EGCommon extends GroupOfTests {
 	public EGCommon() {
 		addTest(
 			MySQLStorageEngine.class,
-			SeqRegionLength.class
+			SeqRegionLength.class,
+			NoRepeatFeatures.class
 		);
 	}
 
