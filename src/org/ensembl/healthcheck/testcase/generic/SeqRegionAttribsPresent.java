dea77a747d0eccedec944ffe19dc4da162a2c7f8
Andrew Yates <ayates@ebi.ac.uk>
2013-04-30 15:07:49 +0000

    Adding a new check which asserts if _rna_edit attributes are correctly formatted

--- src/org/ensembl/healthcheck/testcase/generic/SeqRegionAttribsPresent.java
+++ src/org/ensembl/healthcheck/testcase/generic/SeqRegionAttribsPresent.java
@@ -18,14 +18,12 @@
 package org.ensembl.healthcheck.testcase.generic;
 
 import java.sql.Connection;
-import java.util.Collection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
+import java.util.Scanner;
+import java.util.Set;
 
 import org.ensembl.healthcheck.DatabaseRegistryEntry;
 import org.ensembl.healthcheck.DatabaseType;
@@ -33,13 +31,14 @@ import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.Team;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 import org.ensembl.healthcheck.util.DBUtils;
-import org.ensembl.healthcheck.util.Utils;
+import org.ensembl.healthcheck.util.RowMapper;
 import org.ensembl.healthcheck.util.SqlTemplate;
-import org.ensembl.healthcheck.testcase.AbstractTemplatedTestCase;
 
 
 /**
- * Check that certain seq_regions that have known, protein_coding genes have the GeneNo_knwCod attribute associated with them.
+ * Check that certain seq_regions that have known, protein_coding genes 
+ * have the coding_cnt attribute associated with them. Also ensure
+ * that _rna_edit attributes represent substitutions
  */
 public class SeqRegionAttribsPresent extends SingleDatabaseTestCase {
 
@@ -51,9 +50,9 @@ public class SeqRegionAttribsPresent extends SingleDatabaseTestCase {
 		addToGroup("release");
 		addToGroup("post-compara-handover");
 		
-		setDescription("Check that certain seq_regions that have protein_coding genes have the GeneNo_knwCod attribute associated with them.");
-		setEffect("Website gene counts will be wrong");
-		setFix("Re-run density generation pipeline");
+		setDescription("Check that certain seq_regions that have protein_coding genes have the coding_cnt attribute associated with them. Also ensure that _rna_edit attributes represent substitutions");
+		setEffect("Website gene counts will be wrong and API will fail to load");
+		setFix("Re-run density generation pipeline or edit the database to remove the offending attribute");
 		setTeamResponsible(Team.RELEASE_COORDINATOR);
 	}
 
@@ -78,37 +77,121 @@ public class SeqRegionAttribsPresent extends SingleDatabaseTestCase {
 	 * @return true if the test passes.
 	 */
 	public boolean run(final DatabaseRegistryEntry dbre) {
-
 		boolean result = true;
-
-		Connection con = dbre.getConnection();
-		String code;
-		if (dbre.getType() == DatabaseType.SANGER_VEGA) {
-			code = "'KnwnPCCount'";
-		} else {
-			code = "'coding_cnt'";
-		}
-
-                SqlTemplate t = DBUtils.getSqlTemplate(dbre);
-                String sql = "select distinct g.seq_region_id from gene g where g.biotype = 'protein_coding' and g.seq_region_id not in (select distinct g.seq_region_id from gene g, seq_region_attrib sa, attrib_type at where g.seq_region_id = sa.seq_region_id and sa.attrib_type_id = at.attrib_type_id and at.code in ('LRG', 'non_ref'))" ;
-                List<String> toplevel = t.queryForDefaultObjectList(sql, String.class);
-
-                sql = "select distinct g.seq_region_id from gene g, seq_region_attrib sa, attrib_type at where g.seq_region_id = sa.seq_region_id and sa.attrib_type_id = at.attrib_type_id and code = " + code ;
-                List<String> known = t.queryForDefaultObjectList(sql, String.class);
-
-                Set<String> missing = new HashSet<String>(toplevel);
-                missing.removeAll(known);
-
-		if (missing.isEmpty()) {
-                        ReportManager.correct(this, con, "All seq_regions with protein_coding genes have a coding_cnt attribute associated with them");
-                } else {
-                        String msg = String.format("%s regions with protein_coding genes do not have the coding_cnt attribute associated", missing.size());
-                       	ReportManager.problem(this, con, msg);
-        		result = false;
-                }
+		result &= checkCodingCountAttributes(dbre);
+		result &= checkRnaEditAttributes(dbre);
 		return result;
-
 	} // run
+	
+	private boolean checkCodingCountAttributes(final DatabaseRegistryEntry dbre) {
+	  boolean result = true;
+
+    Connection con = dbre.getConnection();
+    String code = (dbre.getType() == DatabaseType.SANGER_VEGA) ? "KnwnPCCount" : "coding_cnt";
+
+    SqlTemplate t = DBUtils.getSqlTemplate(dbre);
+    String sql = "select distinct g.seq_region_id from gene g where g.biotype = ? and g.seq_region_id not in (select distinct g.seq_region_id from gene g, seq_region_attrib sa, attrib_type at where g.seq_region_id = sa.seq_region_id and sa.attrib_type_id = at.attrib_type_id and at.code in (?,?))" ;
+    List<String> toplevel = t.queryForDefaultObjectList(sql, String.class, "protein_coding", "LRG", "non_ref");
+
+    sql = "select distinct g.seq_region_id from gene g, seq_region_attrib sa, attrib_type at where g.seq_region_id = sa.seq_region_id and sa.attrib_type_id = at.attrib_type_id and code =? ";
+    List<String> known = t.queryForDefaultObjectList(sql, String.class, code);
+
+    Set<String> missing = new HashSet<String>(toplevel);
+    missing.removeAll(known);
+
+    if (missing.isEmpty()) {
+      ReportManager.correct(this, con, "All seq_regions with protein_coding genes have a coding_cnt attribute associated with them");
+    } else {
+      String msg = String.format("%s regions with protein_coding genes do not have the coding_cnt attribute associated", missing.size());
+      ReportManager.problem(this, con, msg);
+      result = false;
+    }
+    
+    return result;
+	}
+	
+	/**
+	 * Check that any _rna_edit attribute represents a substitution rather
+	 * than an insertion or deletion
+	 */
+	private boolean checkRnaEditAttributes(final DatabaseRegistryEntry dbre) {
+    boolean ok = true;
+    
+    RowMapper<Attrib> mapper = new RowMapper<Attrib>() {
+      public Attrib mapRow(ResultSet rs, int row) throws SQLException {
+        return new Attrib(rs.getString(1), rs.getLong(2), rs.getString(3));
+      }
+    };
+    
+    String sql = "select sr.name, sr.seq_region_id, sra.value "
+        + "from seq_region sr join seq_region_attrib sra using (seq_region_id) "
+        + "join attrib_type at using (attrib_type_id) where at.code =?";
+
+    List<Attrib> attributes = getSqlTemplate(dbre).queryForList(sql, mapper, "_rna_edit");
+    
+    for (Attrib a : attributes) {
+      if (!a.isOk()) {
+        ReportManager.warning(this, dbre.getConnection(), a.toString());
+        ok = false;
+      }
+    }
+    if (!ok) {
+      ReportManager.problem(this, dbre.getConnection(),
+        "Detected sequence regions with incorrectly formatted _rna_edit attributes. Check warnings");
+    }
+    
+    return ok;
+  }
+
+  /**
+   * Only to be used in this class
+   */
+  private static class Attrib {
+    private final String seqRegionName;
+    private final Long seqRegionId;
+    private final String attribute;
+
+    private final Integer editStart;
+    private final Integer editEnd;
+    private final String editString;
+
+    public Attrib(final String seqRegionName, final Long seqRegionId,
+        final String attribute) {
+      this.seqRegionName = seqRegionName;
+      this.seqRegionId = seqRegionId;
+      this.attribute = attribute;
+      Scanner sc = new Scanner(attribute).useDelimiter("\\s+");
+      editStart = sc.nextInt();
+      editEnd = sc.nextInt();
+      editString = sc.next();
+      sc.close();
+    }
+
+    public boolean isOk() {
+      return editLength() == editStringLength();
+    }
+
+    public int editLength() {
+      return ((editEnd - editStart) + 1);
+    }
+
+    public int editStringLength() {
+      return (editString != null) ? editString.length() : 0;
+    }
+
+    public String editType() {
+      int editStringLength = editStringLength();
+      return (editStringLength == 0) ? "a deletion" : "an insertion";
+    }
+
+    public String toString() {
+      return "Sequence Region " + seqRegionName + " (" + seqRegionId
+          + ") has an incorrectly formatted _rna_edit attribute '" + attribute
+          + "'. Edit length was " + editLength()
+          + " but insert string length was " + editStringLength()
+          + ". Mostly likely " + editType();
+    }
+  }
 
 	// -----------------------------------------------------------------
 
