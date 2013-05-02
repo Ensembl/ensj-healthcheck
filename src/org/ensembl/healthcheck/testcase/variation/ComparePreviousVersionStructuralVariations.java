e4bb1d32cbb365d6a0f09a1dc59263f33dbf7f9b
Laurent Gil <lgil@ebi.ac.uk>
2013-05-02 13:22:41 +0000

    Compare the number of structural variants by studies

---
+++ src/org/ensembl/healthcheck/testcase/variation/ComparePreviousVersionStructuralVariations.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2003 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.ensembl.healthcheck.testcase.variation;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.Team;
+import org.ensembl.healthcheck.testcase.generic.ComparePreviousVersionBase;
+
+/**
+ * Compare the number of variations between the current database and the database on the secondary server.
+ */
+
+public class ComparePreviousVersionStructuralVariations extends ComparePreviousVersionBase {
+
+	/**
+	 * Create a new testcase.
+	 */
+	public ComparePreviousVersionStructuralVariations() {
+
+		addToGroup("variation");
+		addToGroup("variation-release");
+		setDescription("Compare the number of structural variations in the current database with those from the equivalent database on the secondary server");
+		setTeamResponsible(Team.VARIATION);
+
+	}
+
+	protected Map getCounts(DatabaseRegistryEntry dbre) {
+
+		Map<String, Integer> counts = new HashMap<String, Integer>();
+		Connection con = dbre.getConnection();
+
+		try {
+			
+			// Count variations by source
+			counts.putAll(getCountsBySQL(dbre, "SELECT st.name, COUNT(*) FROM structural_variation sv JOIN study st ON (st.study_id = sv.study_id) GROUP BY st.name"));
+			// Count total number of variations
+			counts.putAll(getCountsBySQL(dbre, "SELECT 'all structural variations', COUNT(*) FROM structural_variation"));
+
+		} catch (Exception e) {
+			ReportManager.problem(this, con, "HealthCheck caused an exception: " + e.getMessage());
+		}
+		
+		return counts;
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected String entityDescription() {
+
+		return "number of variations from";
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected double threshold() {
+
+		return 1;
+
+	}
+
+        // ------------------------------------------------------------------------
+
+        protected double minimum() {
+
+                return 0;
+
+        }
+
+	// ------------------------------------------------------------------------
+
+} // ComparePreviousVersionVariations
