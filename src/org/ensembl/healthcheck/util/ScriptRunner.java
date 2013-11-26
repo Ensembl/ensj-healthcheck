ecee54307994aff1f0c1c0692d63de7109742ad2
Andrew Yates <ayates@ebi.ac.uk>
2013-11-26 09:43:33 +0000

    The Ensembl license has been changed from Apache-like to Apache 2.0

--- src/org/ensembl/healthcheck/util/ScriptRunner.java
+++ src/org/ensembl/healthcheck/util/ScriptRunner.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ensembl.healthcheck.util;
 
 // Downloaded from:
@@ -11,22 +27,6 @@ package org.ensembl.healthcheck.util;
  * from the iBATIS Apache project. Only removed dependency on Resource class
  * and a constructor 
  */
-/*
- *  Copyright 2004 Clinton Begin
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.PrintWriter;
