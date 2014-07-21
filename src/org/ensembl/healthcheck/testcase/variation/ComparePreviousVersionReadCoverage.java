5e47c42b8621478ebd90ae23d811d2ffde42cb2b
Anja Thormann <anja@ebi.ac.uk>
2014-07-21 11:38:53 +0100

    Add health check for read coverage data
    Compare read counts for individuals between two releases.

---
+++ src/org/ensembl/healthcheck/testcase/variation/ComparePreviousVersionReadCoverage.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (C) 2003 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.ensembl.healthcheck.testcase.variation;
+
+import java.util.Map;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.Team;
+import org.ensembl.healthcheck.testcase.generic.ComparePreviousVersionBase;
+
+/**
+ * Compare the number of reads per individual between the current database and the database on the secondary server.
+ */
+
+public class ComparePreviousVersionReadCoverage extends ComparePreviousVersionBase {
+
+	/**
+	 * Create a new testcase.
+	 */
+	public ComparePreviousVersionReadCoverage() {
+
+		addToGroup("variation-release");
+		setDescription("Compare the number of reads for each individual with read coverage in the current database with those from the equivalent database on the secondary server");
+		setTeamResponsible(Team.VARIATION);
+
+	}
+
+	protected Map getCounts(DatabaseRegistryEntry dbre) {
+		return getCountsBySQL(dbre, "SELECT i.name, COUNT(*) FROM read_coverage rc JOIN individual i ON (i.individual_id = rc.individual_id) GROUP BY i.name");
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected String entityDescription() {
+
+		return "number of reads for individual";
+
+	}
+
+	// ------------------------------------------------------------------------
+
+	protected double threshold() {
+
+		return 1;
+
+	}
+
+        // ------------------------------------------------------------------------
+
+        protected double minimum() {
+
+                return 0;
+
+        }
+
+	// ------------------------------------------------------------------------
+
+} 
