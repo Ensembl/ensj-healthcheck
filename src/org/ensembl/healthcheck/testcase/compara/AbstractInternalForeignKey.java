15635343234f323dedc4c9ef3f392e8cab754d1d
Matthieu Muffato <muffato@ebi.ac.uk>
2014-09-17 11:18:55 +0100

    Simplified the code down to a single method

--- src/org/ensembl/healthcheck/testcase/compara/AbstractInternalForeignKey.java
+++ src/org/ensembl/healthcheck/testcase/compara/AbstractInternalForeignKey.java
@@ -35,84 +35,6 @@ public abstract class AbstractInternalForeignKey extends SingleDatabaseTestCase 
 
 
 	/**
-	 * Verify foreign-key relations.
-	 * 
-	 * @param con
-	 *            A connection to the database to be tested. Should already be
-	 *            open.
-	 * @param table
-	 *            The table in which to check
-	 * @param col1
-	 *            First column in table to check.
-	 * @param col2
-	 *            Second column in table to check.
-	 * @param col1_can_be_null
-	 *            Whether NULLs in the first column are allowed
-	 * @param oneWayOnly
-	 *            If true, only a "left join" is performed. Otherwise,
-	 *            both directions are checked
-	 * @return The number of "orphans"
-	 */
-	public int countOrphansSameTable(Connection con, String table, String col1,
-			String col2, boolean col1_can_be_null, boolean oneWayOnly) {
-
-		if (con == null) {
-			logger.severe("countOrphans: Database connection is null");
-		}
-
-		int resultLeft, resultRight;
-
-		String sql = " FROM " + table + " hc_table1 LEFT JOIN " + table + " hc_table2 ON "
-				+ "hc_table1." + col1 + " = hc_table2." + col2 + " WHERE "
-				+ "hc_table2." + col2 + " IS NULL";
-		if (col1_can_be_null) {
-			sql = sql + " AND hc_table1." + col1 + " IS NOT NULL";
-		}
-
-		resultLeft = DBUtils.getRowCount(con, "SELECT COUNT(*)" + sql);
-
-		logger.finest("Left: " + resultLeft);
-
-		if (resultLeft > 0) {
-			String[] values = DBUtils.getColumnValues(con, "SELECT hc_table1"
-					+ "." + col1 + sql + " LIMIT 20");
-			for (int i = 0; i < values.length; i++) {
-				ReportManager.info(this, con, table + "." + col1 + " "
-						+ values[i] + " is not linked.");
-			}
-		}
-
-		if (!oneWayOnly) {
-			// and the other way ... (a right join?)
-			sql = " FROM " + table + "hc_table2 LEFT JOIN " + table + " hc_table1 ON "
-					+ "hc_table2." + col2 + " = hc_table1." + col1 + " WHERE "
-					+ "hc_table1." + col1 + " IS NULL";
-
-			resultRight = DBUtils.getRowCount(con, "SELECT COUNT(*)" + sql);
-
-			if (resultRight > 0) {
-				String[] values = DBUtils.getColumnValues(con, "SELECT hc_table2"
-						+ "." + col2 + sql + " LIMIT 20");
-				for (int i = 0; i < values.length; i++) {
-					ReportManager.info(this, con, table + "." + col2 + " "
-							+ values[i] + " is not linked.");
-				}
-			}
-
-			logger.finest("Right: " + resultRight);
-
-		} else {
-			resultRight = 0;
-		}
-
-		// logger.finest("Left: " + resultLeft + " Right: " + resultRight);
-
-		return resultLeft + resultRight;
-
-	} // countOrphansSameTable
-
-
-	/**
 	 * Verify foreign-key relations, and fills ReportManager with useful sql if
 	 * necessary.
 	 * 
@@ -131,37 +53,39 @@ public abstract class AbstractInternalForeignKey extends SingleDatabaseTestCase 
 	 */
 	public boolean checkForOrphansSameTable(Connection con, String table, String col1, String col2, boolean col1_can_be_null) {
 
-		int orphans = 0;
-		boolean result = true;
-
-		orphans = countOrphansSameTable(con, table, col1, col2, col1_can_be_null, true);
-
-		String useful_sql = "SELECT hc_table1." + col1 + " FROM " + table
-				+ " hc_table1 LEFT JOIN " + table + " hc_table2 ON hc_table1." + col1 + " = "
-				+ "hc_table2." + col2 + " WHERE hc_table2." + col2
-				+ " IS NULL";
-		if (col1_can_be_null) {
-			useful_sql = useful_sql + " AND hc_table1." + col1 + " IS NOT NULL";
+		if (con == null) {
+			logger.severe("countOrphans: Database connection is null");
+			return false;
 		}
+
+		String sql = String.format(" FROM %s hc_table1 LEFT JOIN %s hc_table2 ON hc_table1.%s = hc_table2.%s WHERE hc_table2.%s IS NULL", table, table, col1, col2, col2);
+		if (col1_can_be_null) {
+			sql = sql + String.format(" AND hc_table1.%s IS NOT NULL", col1);
+		}
+
+		int orphans = DBUtils.getRowCount(con, "SELECT COUNT(*)" + sql);
+
+		logger.finest("Orphans: " + orphans);
 
 		if (orphans > 0) {
-			ReportManager.problem(this, con, "FAILED " + table + " : " + col1
-					+ " -> " + col2 + " using FK "
-					+ " relationships");
-			ReportManager.problem(this, con, "FAILURE DETAILS: " + orphans
-					+ " " + table + " entries are not linked from " + col1 + " to " + col2);
-			ReportManager.problem(this, con, "USEFUL SQL: " + useful_sql);
-			result = false;
+			String[] values = DBUtils.getColumnValues(con, "SELECT hc_table1." + col1 + sql + " LIMIT 20");
+			for (String this_value : values) {
+				ReportManager.info(this, con, table + "." + col1 + " " + this_value + " is not linked.");
+			}
+
+			ReportManager.problem(this, con, "FAILED " + table + " : " + col1 + " -> " + col2 + " using FK " + " relationships");
+			ReportManager.problem(this, con, "FAILURE DETAILS: " + orphans + " " + table + " entries are not linked from " + col1 + " to " + col2);
+			ReportManager.problem(this, con, "USEFUL SQL: SELECT hc_table1.*" + sql);
+			return false;
+
 		} else if (orphans < 0) {
-			ReportManager.problem(this, con, "TEST NOT COMPLETED " + table
-					+ " : " + col1 + " -> " + col2 + " using FK "
-					+ ", look at the StackTrace if any");
-			result = false;
+			ReportManager.problem(this, con, "TEST NOT COMPLETED " + table + " : " + col1 + " -> " + col2 + " using FK, look at the StackTrace if any");
+			return false;
 		}
 
-		return result;
+		return true;
 
-	} // checkForOrphans
+	} // checkForOrphansSameTable
 
 
 } // AbstractInternalForeignKey
