337a56c2fad430eeb6446232131cd05cbe0c8d3e
Magali Ruffier <mr6@ebi.ac.uk>
2014-09-19 15:44:46 +0100

    ENSCORESW-1035, ENSCORESW-1034: more checks on required attributes for human and mouse

--- src/org/ensembl/healthcheck/testcase/generic/GencodeAttributes.java
+++ src/org/ensembl/healthcheck/testcase/generic/GencodeAttributes.java
@@ -27,6 +27,8 @@ import org.ensembl.healthcheck.Team;
 import org.ensembl.healthcheck.Species;
 import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 import org.ensembl.healthcheck.util.DBUtils;
+import org.ensembl.healthcheck.util.SqlTemplate;
+
 
 /**
  * Check that gencode basic attributes are present
@@ -80,6 +82,18 @@ public class GencodeAttributes extends SingleDatabaseTestCase {
       return result;
     }
 
+    result &= gencodeAttrib(con);
+    result &= refseqAttrib(con);
+    result &= apprisAttrib(dbre);
+    result &= tslAttrib(dbre);
+
+    return result;
+
+  }
+
+  protected boolean gencodeAttrib (Connection con) {
+
+    boolean result = true;
     int gencodeGenes = DBUtils.getRowCount(con, "SELECT COUNT(distinct gene_id) FROM transcript t, attrib_type at, transcript_attrib ta WHERE t.transcript_id = ta.transcript_id AND at.attrib_type_id=ta.attrib_type_id AND at.code='gencode_basic'");
 
     int rows = DBUtils.getRowCount(con, "SELECT COUNT(distinct gene_id) FROM transcript WHERE biotype NOT IN ('LRG_gene')");
@@ -90,7 +104,13 @@ public class GencodeAttributes extends SingleDatabaseTestCase {
     } else {
       ReportManager.correct(this, con, rows + " gencode basic transcript attributes found");
     }
+ 
+    return result;
+  }
 
+  protected boolean refseqAttrib (Connection con) {
+
+    boolean result = true;
     int genes = DBUtils.getRowCount(con, "SELECT COUNT(distinct g.gene_id) FROM gene g, seq_region s, coord_system cs WHERE g.seq_region_id = s.seq_region_id AND " +
                                             "s.coord_system_id = cs.coord_system_id AND cs.name = 'chromosome' AND cs.attrib = 'default_version' AND s.name NOT LIKE 'LRG%' " +
                                             "AND s.name != 'MT' AND s.seq_region_id NOT IN (SELECT seq_region_id FROM assembly_exception WHERE exc_type in ('PATCH_NOVEL', 'PATCH_FIX', 'HAP'))");
@@ -109,7 +129,55 @@ public class GencodeAttributes extends SingleDatabaseTestCase {
 
     return result;
 
-  } // run
+  }
+
+  protected boolean apprisAttrib (DatabaseRegistryEntry dbre) {
+
+    boolean result = true;
+    Connection con = dbre.getConnection();
+    SqlTemplate t = DBUtils.getSqlTemplate(dbre);
+
+    String chromosomeSql = "SELECT DISTINCT s.name FROM seq_region s, seq_region_attrib sa, attrib_type at WHERE s.seq_region_id = sa.seq_region_id AND sa.attrib_type_id = at.attrib_type_id AND code = 'karyotype_rank'";
+    String codingSql = "SELECT count(distinct g.stable_id) FROM gene g, seq_region s WHERE g.seq_region_id = s.seq_region_id AND s.name = ? AND biotype = 'protein_coding'";
+    String apprisSql = "SELECT count(distinct g.stable_id) FROM gene g, seq_region s, transcript t, transcript_attrib ta, attrib_type a WHERE g.seq_region_id = s.seq_region_id AND s.name = ? AND g.biotype = 'protein_coding' AND g.gene_id=t.gene_id AND t.transcript_id=ta.transcript_id AND ta.attrib_type_id=a.attrib_type_id AND a.code like 'appris%'"; 
+    List<String> chromosomes = t.queryForDefaultObjectList(chromosomeSql, String.class);
+
+    for (String chromosome: chromosomes) {
+      int codingCount = t.queryForDefaultObject(codingSql, Integer.class, chromosome);
+      int apprisCount = t.queryForDefaultObject(apprisSql, Integer.class, chromosome);
+      if (apprisCount < codingCount * 0.95) { 
+        ReportManager.problem(this, con, chromosome + " has " + codingCount + " protein coding genes but only " + apprisCount + " have a transcript-attrib like ‘appris%’");
+        result = false;
+      }
+    }
+
+    return result;
+  }
+
+
+  protected boolean tslAttrib (DatabaseRegistryEntry dbre) {
+
+    boolean result = true;
+
+    Connection con = dbre.getConnection();
+    SqlTemplate t = DBUtils.getSqlTemplate(dbre);
+
+    String chromosomeSql = "SELECT DISTINCT s.name FROM seq_region s, seq_region_attrib sa, attrib_type at WHERE s.seq_region_id = sa.seq_region_id AND sa.attrib_type_id = at.attrib_type_id AND s.name NOT LIKE 'LRG%' AND code = 'toplevel'";
+    String transcriptSql = "SELECT count(distinct t.stable_id) from seq_region s, transcript t WHERE t.seq_region_id = s.seq_region_id and s.name = ?";
+    String tslSql = "SELECT count(distinct t.stable_id) from seq_region s, transcript t, transcript_attrib ta, attrib_type a WHERE t.seq_region_id = s.seq_region_id AND t.transcript_id = ta.transcript_id AND ta.attrib_type_id = a.attrib_type_id AND a.code like 'tsl%' AND s.name = ?";
+    List<String> chromosomes = t.queryForDefaultObjectList(chromosomeSql, String.class);
+
+    for (String chromosome: chromosomes) {
+      int transcriptCount = t.queryForDefaultObject(transcriptSql, Integer.class, chromosome);
+      int tslCount = t.queryForDefaultObject(tslSql, Integer.class, chromosome);
+      if (tslCount < transcriptCount * 0.95) {
+        ReportManager.problem(this, con, chromosome + " has " + transcriptCount + " transcripts but only " + tslCount + " have a transcript-attrib like ‘tsl%’");
+        result = false;
+      }
+    }
+
+    return result;
+  }
 
   // ----------------------------------------------------------------------
 
