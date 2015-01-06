1c2cdc953e290355de2bce0b1d05dae6e0922b96
Magali Ruffier <mr6@ebi.ac.uk>
2015-01-06 12:19:44 +0000

    include new type of gene count attrib_types, with codes like '%_cnt_%'

--- src/org/ensembl/healthcheck/testcase/generic/SeqRegionAttribAcrossSpecies.java
+++ src/org/ensembl/healthcheck/testcase/generic/SeqRegionAttribAcrossSpecies.java
@@ -53,7 +53,7 @@ public class SeqRegionAttribAcrossSpecies extends MultiDatabaseTestCase {
 	public boolean run(DatabaseRegistry dbr) {
 
 		return checkSQLAcrossSpecies(
-                              "SELECT at.code, COUNT(*) FROM seq_region_attrib sa, attrib_type at WHERE at.attrib_type_id=sa.attrib_type_id AND at.code != 'SNPCount' AND at.code NOT LIKE '%_cnt' AND at.code NOT LIKE '%_acnt' group by at.code",
+                              "SELECT at.code, COUNT(*) FROM seq_region_attrib sa, attrib_type at WHERE at.attrib_type_id=sa.attrib_type_id AND at.code != 'SNPCount' AND at.code NOT LIKE '%_cnt%' AND at.code NOT LIKE '%_acnt%' group by at.code",
 				dbr, types, false);
 
 	} // run
