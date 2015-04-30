abfa64994582631ab08c3d066f668e036a5d3e89
Matthieu Muffato <muffato@ebi.ac.uk>
2015-04-30 09:34:13 +0100

    Use queryForMap() and recent-ish Java features
    Yes, we can iterate over a Map :)

--- src/org/ensembl/healthcheck/testcase/compara/CheckSpeciesSetTag.java
+++ src/org/ensembl/healthcheck/testcase/compara/CheckSpeciesSetTag.java
@@ -35,6 +35,7 @@ import org.ensembl.healthcheck.Species;
 import org.ensembl.healthcheck.Team;
 import org.ensembl.healthcheck.testcase.compara.AbstractComparaTestCase;
 import org.ensembl.healthcheck.util.DBUtils;
+import org.ensembl.healthcheck.util.DefaultMapRowMapper;
 
 /**
  * An EnsEMBL Healthcheck test case that looks for broken foreign-key
@@ -67,75 +68,51 @@ public class CheckSpeciesSetTag extends AbstractComparaTestCase {
 
 		boolean result = true;
 
-		DatabaseRegistryEntry[] allSecondaryComparaDBs = DBUtils
-				.getSecondaryDatabaseRegistry().getAll(DatabaseType.COMPARA);
+		DatabaseRegistryEntry[] allSecondaryComparaDBs = DBUtils.getSecondaryDatabaseRegistry().getAll(DatabaseType.COMPARA);
 
-			// ... check that we have one name tag for every MSA
-			result &= checkNameTagForMultipleAlignments(comparaDbre);
+		// ... check that we have one name tag for every MSA
+		result &= checkNameTagForMultipleAlignments(comparaDbre);
 
-			if (allSecondaryComparaDBs.length == 0) {
-				result = false;
-				ReportManager.problem(this,
-						comparaDbre.getConnection(),
-						"Cannot find the compara database in the secondary server. This check expects to find a previous version of the compara database for checking that all the *named* species_sets are still present in the current database.");
-			}
-			for (int j = 0; j < allSecondaryComparaDBs.length; j++) {
-				// Check vs previous compara DB.
-				result &= checkSetOfSpeciesSets(comparaDbre,
-						allSecondaryComparaDBs[j]);
-			}
+		if (allSecondaryComparaDBs.length == 0) {
+			result = false;
+			ReportManager.problem(this,
+					comparaDbre.getConnection(),
+					"Cannot find the compara database in the secondary server. This check expects to find a previous version of the compara database for checking that all the *named* species_sets are still present in the current database.");
+		}
+
+		for (int j = 0; j < allSecondaryComparaDBs.length; j++) {
+			// Check vs previous compara DB.
+			result &= checkSetOfSpeciesSets(comparaDbre, allSecondaryComparaDBs[j]);
+		}
 
 		return result;
 	}
 
-	public boolean checkSetOfSpeciesSets(
-			DatabaseRegistryEntry primaryComparaDbre,
-			DatabaseRegistryEntry secondaryComparaDbre) {
+	public boolean checkSetOfSpeciesSets(DatabaseRegistryEntry primaryComparaDbre, DatabaseRegistryEntry secondaryComparaDbre) {
 
 		boolean result = true;
 		Connection con1 = primaryComparaDbre.getConnection();
 		Connection con2 = secondaryComparaDbre.getConnection();
-		HashMap<String,Integer> primarySets = new HashMap<String,Integer>();
-		HashMap<String,Integer> secondarySets = new HashMap<String,Integer>();
 
 		// Get list of species_set sets in the secondary server
 		String sql = "SELECT value, count(*) FROM species_set_tag WHERE tag = 'name' GROUP BY value";
-		try {
-			Statement stmt1 = con1.createStatement();
-			ResultSet rs1 = stmt1.executeQuery(sql);
-			while (rs1.next()) {
-				primarySets.put(rs1.getString(1), rs1.getInt(2));
-			}
-			rs1.close();
-			stmt1.close();
+		Map<String,Integer> primarySets   = DBUtils.getSqlTemplate(con1).queryForMap(sql, new DefaultMapRowMapper<String, Integer>(String.class, Integer.class));
+		Map<String,Integer> secondarySets = DBUtils.getSqlTemplate(con2).queryForMap(sql, new DefaultMapRowMapper<String, Integer>(String.class, Integer.class));
 
-			Statement stmt2 = con2.createStatement();
-			ResultSet rs2 = stmt2.executeQuery(sql);
-			while (rs2.next()) {
-				secondarySets.put(rs2.getString(1), rs2.getInt(2));
-			}
-			rs2.close();
-			stmt2.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		Iterator<String> it = secondarySets.keySet().iterator();
-		while (it.hasNext()) {
-			String next = it.next();
-			Integer primaryValue = primarySets.get(next);
-			Integer secondaryValue = secondarySets.get(next);
-			if (primaryValue == null) {
-				ReportManager.problem(this, con1, String.format("Species set '%s' is missing (it appears %d time(s) in %s", next, secondaryValue, DBUtils.getShortDatabaseName(con2)));
+		for (Map.Entry<String, Integer> key_value : secondarySets.entrySet()) {
+			String key = key_value.getKey();
+			Integer primaryValue = primarySets.get(key);
+			Integer secondaryValue = key_value.getValue();
+			if (!primarySets.containsKey(key)) {
+				ReportManager.problem(this, con1, String.format("Species set '%s' is missing (it appears %d time(s) in %s", key, secondaryValue, DBUtils.getShortDatabaseName(con2)));
 				result = false;
 			} else if (primaryValue < secondaryValue) {
-				ReportManager.problem(this, con1, String.format("Species set '%s' is present only %d times instead of %d as in %s", next, primaryValue, secondaryValue, DBUtils.getShortDatabaseName(con2)));
+				ReportManager.problem(this, con1, String.format("Species set '%s' is present only %d times instead of %d as in %s", key, primaryValue, secondaryValue, DBUtils.getShortDatabaseName(con2)));
 				result = false;
 			}
 		}
-		it = primarySets.keySet().iterator();
-		while (it.hasNext()) {
-			String next = it.next();
+		for (Map.Entry<String, Integer> key_value : primarySets.entrySet()) {
+			String next = key_value.getKey();
 			if (!secondarySets.containsKey(next)) {
 				ReportManager.problem(this, con1, String.format("Species set '%s' is new (compared to %s)", next, DBUtils.getShortDatabaseName(con2)));
 				result = false;
@@ -150,8 +127,6 @@ public class CheckSpeciesSetTag extends AbstractComparaTestCase {
 		boolean result = true;
 
 		Connection con = dbre.getConnection();
-		HashMap<Integer,String> allSetsWithAName = new HashMap<Integer,String>();
-		HashMap<Integer,String> allSetsForMultipleAlignments = new HashMap<Integer,String>();
 
 		if (tableHasRows(con, "species_set_tag")) {
 
@@ -160,38 +135,14 @@ public class CheckSpeciesSetTag extends AbstractComparaTestCase {
 
 			// Find all the species_set_ids for multiple alignments
 			String sql2 = "SELECT species_set_id, name FROM method_link_species_set JOIN method_link USING (method_link_id) WHERE"
-					+ " class LIKE '%multiple_alignment%' OR class LIKE '%tree_alignment%' OR class LIKE '%ancestral_alignment%'";
+				+ " class LIKE '%multiple_alignment%' OR class LIKE '%tree_alignment%' OR class LIKE '%ancestral_alignment%'";
 
-			try {
-				Statement stmt1 = con.createStatement();
-				ResultSet rs1 = stmt1.executeQuery(sql1);
-				while (rs1.next()) {
-					allSetsWithAName.put(rs1.getInt(1), rs1.getString(2));
-				}
-				rs1.close();
-				stmt1.close();
+			Map<Integer, String> allSetsWithAName = DBUtils.getSqlTemplate(con).queryForMap(sql1, new DefaultMapRowMapper<Integer, String>(Integer.class, String.class));
+			Map<Integer, String> allSetsForMultipleAlignments = DBUtils.getSqlTemplate(con).queryForMap(sql2, new DefaultMapRowMapper<Integer, String>(Integer.class, String.class));
 
-				Statement stmt2 = con.createStatement();
-				ResultSet rs2 = stmt2.executeQuery(sql2);
-				while (rs2.next()) {
-					allSetsForMultipleAlignments.put(rs2.getInt(1),
-							rs2.getString(2));
-				}
-				rs2.close();
-				stmt2.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-			Iterator<Integer> it = allSetsForMultipleAlignments.keySet().iterator();
-			while (it.hasNext()) {
-				Integer next = it.next();
-				String setName = allSetsWithAName.get(next);
-				String multipleAlignmentName = allSetsForMultipleAlignments.get(next);
-				if (setName == null) {
-					ReportManager.problem(this, con,
-							"There is no name entry in species_set_tag for MSA \""
-									+ multipleAlignmentName + "\".");
+			for (Map.Entry<Integer, String> key_value : allSetsForMultipleAlignments.entrySet()) {
+				if (!allSetsWithAName.containsKey(key_value.getKey())) {
+					ReportManager.problem(this, con, "There is no name entry in species_set_tag for MSA \"" + key_value.getValue()+ "\".");
 					result = false;
 				}
 			}
@@ -202,7 +153,6 @@ public class CheckSpeciesSetTag extends AbstractComparaTestCase {
 		}
 
 		return result;
-
 	}
 
 } // CheckSpeciesSetTag
