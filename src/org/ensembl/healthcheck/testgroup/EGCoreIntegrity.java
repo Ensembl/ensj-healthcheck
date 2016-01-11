a710b79d5f3faabfd8d363a106e7868b0681abf7
Dan Staines <dstaines@ebi.ac.uk>
2016-01-11 13:45:19 +0000

    Removed EGCompareCoreSchema as it detects minor, unimportant differences that cannot be easily resolved

--- src/org/ensembl/healthcheck/testgroup/EGCoreIntegrity.java
+++ src/org/ensembl/healthcheck/testgroup/EGCoreIntegrity.java
@@ -2,14 +2,11 @@ package org.ensembl.healthcheck.testgroup;
 
 import org.ensembl.healthcheck.GroupOfTests;
 import org.ensembl.healthcheck.testcase.eg_core.CircularAwareFeatureCoords;
-import org.ensembl.healthcheck.testcase.eg_core.ControlledTableAttribType;
-import org.ensembl.healthcheck.testcase.eg_core.ControlledTableExternalDb;
 import org.ensembl.healthcheck.testcase.eg_core.DbDisplayNameUniProt;
 import org.ensembl.healthcheck.testcase.eg_core.DeprecatedEgMeta;
 import org.ensembl.healthcheck.testcase.eg_core.DuplicateMetaKeys;
 import org.ensembl.healthcheck.testcase.eg_core.DuplicateTopLevel;
 import org.ensembl.healthcheck.testcase.eg_core.DuplicateXref;
-import org.ensembl.healthcheck.testcase.eg_core.EGCompareCoreSchema;
 import org.ensembl.healthcheck.testcase.eg_core.ExonBoundary;
 import org.ensembl.healthcheck.testcase.eg_core.GeneBuildStartDate;
 import org.ensembl.healthcheck.testcase.eg_core.GeneDescriptionUniProtSource;
@@ -85,7 +82,7 @@ public class EGCoreIntegrity extends GroupOfTests {
 	public EGCoreIntegrity() {
 		addTest(AssemblyExceptions.class, AssemblyMultipleOverlap.class,
 				AssemblySeqregion.class, CanonicalTranscriptCoding.class,
-				CircularAwareFeatureCoords.class, EGCompareCoreSchema.class,
+				CircularAwareFeatureCoords.class,
 				CoreForeignKeys.class, DuplicateAssembly.class,
 				DuplicateTopLevel.class, ExonRank.class, ExonStrandOrder.class,
 				ExonTranscriptStartEnd.class, GeneCoordSystem.class,
