996f4d175524904b8d0dc623be1c70ec831d3058
Thomas Juettemann <juettemann@ebi.ac.uk>
2016-08-17 14:24:45 +0100

    Removed InputSubset, ObjectXref, added MirnaTargetFeatures

--- src/org/ensembl/healthcheck/testcase/funcgen/FuncgenAnalysisDescription.java
+++ src/org/ensembl/healthcheck/testcase/funcgen/FuncgenAnalysisDescription.java
@@ -1,13 +1,13 @@
 /*
  * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
  * Copyright [2016] EMBL-European Bioinformatics Institute
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,13 +17,13 @@
 
 /*
  * Copyright (C) 2003 EBI, GRL
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
@@ -42,7 +42,7 @@ import org.ensembl.healthcheck.testcase.generic.AnalysisDescription;
 
 public class FuncgenAnalysisDescription extends AnalysisDescription {
 
-	String[] types = {"feature_set", "probe_feature", "object_xref", "result_set", "input_subset"}; //unmapped_object? 
+	String[] types = {"feature_set", "probe_feature", "result_set", "mirna_target_feature"}; //unmapped_object?
 	//Will these types be available in parent class?
 
 	/**
@@ -53,14 +53,14 @@ public class FuncgenAnalysisDescription extends AnalysisDescription {
 		addToGroup("funcgen-release");
 		setDescription("Check that all of certain types of objects have analysis_descriptions; also check that displayable field is set.");
 		setTeamResponsible(Team.FUNCGEN);
-		
+
 	}
-	
+
 	// ------------------------------------------------------------------------------
 	protected String[] tableNames() {
 
 		return types;
 
 	}
-	
+
 } // FuncgenAnalysisDescription
