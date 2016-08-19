4dd864080c3c61b7d72ed86e2fa5d6f51dea40b0
Thomas Juettemann <juettemann@ebi.ac.uk>
2016-08-19 14:56:33 +0100

    Removed mirna_target_feature as they dont have an analysis attached

--- src/org/ensembl/healthcheck/testcase/funcgen/FuncgenAnalysisDescription.java
+++ src/org/ensembl/healthcheck/testcase/funcgen/FuncgenAnalysisDescription.java
@@ -42,7 +42,7 @@ import org.ensembl.healthcheck.testcase.generic.AnalysisDescription;
 
 public class FuncgenAnalysisDescription extends AnalysisDescription {
 
-	String[] types = {"feature_set", "probe_feature", "result_set", "mirna_target_feature"}; //unmapped_object?
+	String[] types = {"feature_set", "probe_feature", "result_set" }; //unmapped_object?
 	//Will these types be available in parent class?
 
 	/**
