fff73f06d3869ec2d813144a0b19fb8d1d62cc97
Dan Staines <dstaines@ebi.ac.uk>
2017-02-01 14:25:55 +0000

    cleaned up formatting

--- src/org/ensembl/healthcheck/testcase/generic/TranslationStartEnd.java
+++ src/org/ensembl/healthcheck/testcase/generic/TranslationStartEnd.java
@@ -15,7 +15,6 @@
  * limitations under the License.
  */
 
-
 package org.ensembl.healthcheck.testcase.generic;
 
 import java.sql.Connection;
@@ -28,80 +27,84 @@ import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
 import org.ensembl.healthcheck.util.DBUtils;
 
 /**
- * Check that if the start and end of translation is on the same exon, that start < end. Also check that translation ends aren't
- * beyond exon ends.
+ * Check that if the start and end of translation is on the same exon, that
+ * start < end. Also check that translation ends aren't beyond exon ends.
  */
 public class TranslationStartEnd extends SingleDatabaseTestCase {
 
-	/**
-	 * Creates a new instance of CheckTranslationStartEnd
-	 */
-	public TranslationStartEnd() {
-		
-		setDescription("Check that if the start and end of translation is on the same exon, that start < end. Also check that translation ends aren't beyond exon ends.");
-		setTeamResponsible(Team.GENEBUILD);
-	}
+    /**
+     * Creates a new instance of CheckTranslationStartEnd
+     */
+    public TranslationStartEnd() {
 
-	/**
-	 * This only applies to core and Vega databases.
-	 */
-	public void types() {
+        setDescription(
+                "Check that if the start and end of translation is on the same exon, that start < end. Also check that translation ends aren't beyond exon ends.");
+        setTeamResponsible(Team.GENEBUILD);
+    }
 
-		removeAppliesToType(DatabaseType.OTHERFEATURES);
-		removeAppliesToType(DatabaseType.RNASEQ);
+    /**
+     * This only applies to core and Vega databases.
+     */
+    public void types() {
 
-	}
+        removeAppliesToType(DatabaseType.OTHERFEATURES);
+        removeAppliesToType(DatabaseType.RNASEQ);
 
-	/**
-	 * Find any matching databases that have start > end.
-	 * 
-	 * @param dbre
-	 *          The database to use.
-	 * @return Result.
-	 */
-	public boolean run(DatabaseRegistryEntry dbre) {
+    }
 
-		boolean result = true;
+    /**
+     * Find any matching databases that have start > end.
+     * 
+     * @param dbre
+     *            The database to use.
+     * @return Result.
+     */
+    public boolean run(DatabaseRegistryEntry dbre) {
 
-		// check start < end
-		Connection con = dbre.getConnection();
-		int rows = DBUtils.getRowCount(con, "SELECT COUNT(translation_id) FROM translation WHERE start_exon_id = end_exon_id AND seq_start > seq_end");
-		if (rows > 0) {
-			result = false;
-			ReportManager.problem(this, con, rows + " translations have start > end");
-		} else {
-			ReportManager.correct(this, con, "No translations have start > end");
-		}
+        boolean result = true;
 
-		// check no translations overrun their exons
-		rows = DBUtils.getRowCount(con, "SELECT COUNT(*) FROM translation t, exon e WHERE t.end_exon_id=e.exon_id AND cast(e.seq_region_end as signed int)-cast(e.seq_region_start as signed int)+1 < t.seq_end");
-		if (rows > 0) {
-			result = false;
-			ReportManager.problem(this, con, rows + " translations end beyond the end of their exons");
-		} else {
-			ReportManager.correct(this, con, "No translations overrun exons");
-		}
+        // check start < end
+        Connection con = dbre.getConnection();
+        int rows = DBUtils.getRowCount(con,
+                "SELECT COUNT(translation_id) FROM translation WHERE start_exon_id = end_exon_id AND seq_start > seq_end");
+        if (rows > 0) {
+            result = false;
+            ReportManager.problem(this, con, rows + " translations have start > end");
+        } else {
+            ReportManager.correct(this, con, "No translations have start > end");
+        }
 
-                // check the start and end exon have a correct phase
-                rows = DBUtils.getRowCount(con, "SELECT COUNT(*) FROM translation t, exon e WHERE t.start_exon_id=e.exon_id AND start_exon_id <> end_exon_id and end_phase = -1");
-                if (rows > 0) {
-                        result = false;
-                        ReportManager.problem(this, con, rows + " translations have start exon with a -1 end phase");
-                } else {
-                        ReportManager.correct(this, con, "Start exons for translations have correct end phase");
-                }
+        // check no translations overrun their exons
+        rows = DBUtils.getRowCount(con,
+                "SELECT COUNT(*) FROM translation t, exon e WHERE t.end_exon_id=e.exon_id AND cast(e.seq_region_end as signed int)-cast(e.seq_region_start as signed int)+1 < t.seq_end");
+        if (rows > 0) {
+            result = false;
+            ReportManager.problem(this, con, rows + " translations end beyond the end of their exons");
+        } else {
+            ReportManager.correct(this, con, "No translations overrun exons");
+        }
 
-                rows = DBUtils.getRowCount(con, "SELECT COUNT(*) FROM translation t, exon e WHERE t.end_exon_id=e.exon_id AND start_exon_id <> end_exon_id and phase = -1");
-                if (rows > 0) {
-                        result = false;
-                        ReportManager.problem(this, con, rows + " translations have end exon with -1 phase");
-                } else {
-                        ReportManager.correct(this, con, "End exons for translations have correct phase");
-                }
+        // check the start and end exon have a correct phase
+        rows = DBUtils.getRowCount(con,
+                "SELECT COUNT(*) FROM translation t, exon e WHERE t.start_exon_id=e.exon_id AND start_exon_id <> end_exon_id and end_phase = -1");
+        if (rows > 0) {
+            result = false;
+            ReportManager.problem(this, con, rows + " translations have start exon with a -1 end phase");
+        } else {
+            ReportManager.correct(this, con, "Start exons for translations have correct end phase");
+        }
 
+        rows = DBUtils.getRowCount(con,
+                "SELECT COUNT(*) FROM translation t, exon e WHERE t.end_exon_id=e.exon_id AND start_exon_id <> end_exon_id and phase = -1");
+        if (rows > 0) {
+            result = false;
+            ReportManager.problem(this, con, rows + " translations have end exon with -1 phase");
+        } else {
+            ReportManager.correct(this, con, "End exons for translations have correct phase");
+        }
 
-		return result;
+        return result;
 
-	} // run
+    } // run
 
 } // TranslationStartEnd
