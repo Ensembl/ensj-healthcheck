354bbe1f319a45ce205daa87c1d89afdb7e155db
Ilias Lavidas <ilavidas@ebi.ac.uk>
2017-05-02 15:48:20 +0100

    remove segmentation_feature table

--- src/org/ensembl/healthcheck/testcase/EnsTestCase.java
+++ src/org/ensembl/healthcheck/testcase/EnsTestCase.java
@@ -154,7 +154,7 @@ public abstract class EnsTestCase {
 
 	private String[] funcgenFeatureTables = { "probe_feature",
 			"annotated_feature", "regulatory_feature", "external_feature",
-			"motif_feature", "mirna_target_feature", "segmentation_feature" };
+			"motif_feature", "mirna_target_feature" };
 
 	/**
 	 * Funcgen tables that have an analysis ID.
