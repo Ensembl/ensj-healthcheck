b937ddf87358347d43ce854a086d85d19fb262f5
Ilias Lavidas <ilavidas@ebi.ac.uk>
2017-11-01 14:11:25 +0000

    remove obsolete funcgen hcs

--- src/org/ensembl/healthcheck/testgroup/FuncgenRelease.java
+++ src/org/ensembl/healthcheck/testgroup/FuncgenRelease.java
@@ -86,7 +86,6 @@ public class FuncgenRelease extends GroupOfTests {
 			org.ensembl.healthcheck.testcase.funcgen.ProbeIdsUnique.class,
 			org.ensembl.healthcheck.testcase.funcgen.ProbeTranscriptMappingsUnique.class,
 			org.ensembl.healthcheck.testcase.funcgen.ProbeSetTranscriptMappingsUnique.class,
-			org.ensembl.healthcheck.testcase.funcgen.CurrentSchemaBuildUnique.class,
 			org.ensembl.healthcheck.testcase.funcgen.ExternalFeatureFilesExist.class
 		);
 
