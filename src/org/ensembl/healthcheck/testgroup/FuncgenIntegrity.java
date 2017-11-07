f989b179c27cad9efb6bd461bb24a3a59dad1d6a
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-11-07 12:04:48 +0000

    Alignments don't link to experiment ids, so the healthcheck is obsolete.

--- src/org/ensembl/healthcheck/testgroup/FuncgenIntegrity.java
+++ src/org/ensembl/healthcheck/testgroup/FuncgenIntegrity.java
@@ -7,7 +7,6 @@ public class FuncgenIntegrity extends GroupOfTests {
   public FuncgenIntegrity() {
 
       addTest(
-        org.ensembl.healthcheck.testcase.funcgen.InconsistentExperimentIds.class,
         org.ensembl.healthcheck.testcase.funcgen.BrokenFeatureSetToFeatureTypeLinks.class,
         org.ensembl.healthcheck.testcase.funcgen.SampleRegulatoryFeatureExists.class,
         org.ensembl.healthcheck.testcase.funcgen.EpigenomeLinkedToEFO.class
