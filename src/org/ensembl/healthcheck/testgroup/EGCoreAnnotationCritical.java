2b4c4509633e297374b66b694b24529c3059d748
James Allen <jallen@ebi.ac.uk>
2018-01-24 16:19:12 +0000

    Status field removed; no alternative means to determine whether a display_xref_id is appropriate, so delete HC

--- src/org/ensembl/healthcheck/testgroup/EGCoreAnnotationCritical.java
+++ src/org/ensembl/healthcheck/testgroup/EGCoreAnnotationCritical.java
@@ -19,7 +19,6 @@ package org.ensembl.healthcheck.testgroup;
 
 import org.ensembl.healthcheck.GroupOfTests;
 import org.ensembl.healthcheck.testcase.eg_core.DbDisplayNameUniProt;
-import org.ensembl.healthcheck.testcase.eg_core.DisplayXrefId;
 import org.ensembl.healthcheck.testcase.eg_core.GeneDescriptionSource;
 import org.ensembl.healthcheck.testcase.eg_core.GeneDescriptionUniProtSource;
 import org.ensembl.healthcheck.testcase.eg_core.GeneSource;
@@ -45,7 +44,6 @@ public class EGCoreAnnotationCritical extends GroupOfTests {
 			DescriptionNewlines.class, 
 			DisplayLabels.class,
 			DisplayXref.class, 
-			DisplayXrefId.class, 
 			GeneDescriptions.class,
 			GeneDescriptionSource.class, 
 			SharedDisplayXref.class,
