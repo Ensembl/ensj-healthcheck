99ffcd13d14590c7026eba6d2447449914dde723
Amonida Zadissa <amonida@ebi.ac.uk>
2018-02-20 17:48:41 +0000

    Adding back the removed files after the merge conflict

---
+++ src/org/ensembl/healthcheck/testcase/variation/VariationClasses.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * Copyright [2016-2017] EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Copyright (C) 2004 EBI, GRL
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+package org.ensembl.healthcheck.testcase.variation;
+
+import java.sql.Connection;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.DatabaseType;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.Team;
+import org.ensembl.healthcheck.testcase.SingleDatabaseTestCase;
+import org.ensembl.healthcheck.util.DBUtils;
+
+/**
+ * Sanity check variation classes
+ */
+public class VariationClasses extends SingleDatabaseTestCase {
+
+	/**
+	 * Creates a new instance of VariationClasses
+	 */
+	public VariationClasses() {
+
+		addToGroup("variation-release");
+
+		setDescription("Sanity check variation classes");
+		setTeamResponsible(Team.VARIATION);
+
+	}
+
+	// ---------------------------------------------------------------------
+
+	/**
+	 * Sanity check the variation classes.
+	 * 
+	 * @param dbre
+	 *            The database to check.
+	 * @return true if the test passed.
+	 */
+	public boolean run(DatabaseRegistryEntry dbre) {
+
+		boolean result = true;
+
+		String species = dbre.getSpecies();
+
+		Connection con = dbre.getConnection();
+
+		// at the moment we only check human
+
+		if (species == DatabaseRegistryEntry.HOMO_SAPIENS) {
+
+			try {
+
+				// and we only check that no HGMD mutation is ever classed as
+				// 'sequence_alteration'
+
+				String query = "SELECT COUNT(*) " + "FROM variation v, source s, attrib a, attrib_type t "
+						+ "WHERE t.code = 'SO_term' " + "AND a.attrib_type_id = t.attrib_type_id "
+						+ "AND a.value = 'sequence_alteration' " + "AND a.attrib_id = v.class_attrib_id "
+						+ "AND s.name = 'HGMD-PUBLIC' " + "AND s.source_id = v.source_id ";
+
+				int cnt = DBUtils.getRowCount(con, query);
+				if (cnt != 0) {
+					ReportManager.problem(this, con, cnt
+							+ " variations with name 'HGMD-PUBLIC' found that have the attrib 'sequence_alteration' ");
+					ReportManager.problem(this, con, "Useful SQL: " + query);
+					result = false;
+				}
+
+			} catch (Exception e) {
+				ReportManager.problem(this, con, "HealthCheck caused an exception: " + e.getMessage());
+				result = false;
+			}
+		}
+
+		if (result) {
+			ReportManager.correct(this, con, "Variation classes look sane");
+		}
+
+		return result;
+
+	} // run
+
+	// -----------------------------------------------------------------
+
+	/**
+	 * This only applies to variation databases.
+	 */
+	public void types() {
+
+		setAppliesToType(DatabaseType.VARIATION);
+
+	}
+
+} // VariationClasses
---
+++ src/org/ensembl/healthcheck/testgroup/EGCoreIntegrity.java
@@ -0,0 +1,136 @@
+package org.ensembl.healthcheck.testgroup;
+
+import org.ensembl.healthcheck.GroupOfTests;
+import org.ensembl.healthcheck.testcase.eg_core.AssemblyDefault;
+import org.ensembl.healthcheck.testcase.eg_core.CircularAwareFeatureCoords;
+import org.ensembl.healthcheck.testcase.eg_core.DbDisplayNameUniProt;
+import org.ensembl.healthcheck.testcase.eg_core.DeprecatedEgMeta;
+import org.ensembl.healthcheck.testcase.eg_core.DuplicateMetaKeys;
+import org.ensembl.healthcheck.testcase.eg_core.DuplicateTopLevel;
+import org.ensembl.healthcheck.testcase.eg_core.DuplicateXref;
+import org.ensembl.healthcheck.testcase.eg_core.EvidenceFreeGO;
+import org.ensembl.healthcheck.testcase.eg_core.ExonBoundary;
+import org.ensembl.healthcheck.testcase.eg_core.GeneBuildStartDate;
+import org.ensembl.healthcheck.testcase.eg_core.GeneDescriptionUniProtSource;
+import org.ensembl.healthcheck.testcase.eg_core.GeneGC;
+import org.ensembl.healthcheck.testcase.eg_core.IgiXref;
+import org.ensembl.healthcheck.testcase.eg_core.InappropriateTranslation;
+import org.ensembl.healthcheck.testcase.eg_core.IncorrectExonRank;
+import org.ensembl.healthcheck.testcase.eg_core.MetaForCompara;
+import org.ensembl.healthcheck.testcase.eg_core.PeptideTranslationAttribs;
+import org.ensembl.healthcheck.testcase.eg_core.PermittedEgMeta;
+import org.ensembl.healthcheck.testcase.eg_core.PositiveCoordinates;
+import org.ensembl.healthcheck.testcase.eg_core.ProteinCodingGene;
+import org.ensembl.healthcheck.testcase.eg_core.ProteinTranslation;
+import org.ensembl.healthcheck.testcase.eg_core.RepeatAnalysesInMeta;
+import org.ensembl.healthcheck.testcase.eg_core.RequiredEgMeta;
+import org.ensembl.healthcheck.testcase.eg_core.SampleSetting;
+import org.ensembl.healthcheck.testcase.eg_core.SeqRegionAttribForPolyploidGenome;
+import org.ensembl.healthcheck.testcase.eg_core.SeqRegionAttribForPolyploidGenomeToplevelOnly;
+import org.ensembl.healthcheck.testcase.eg_core.SeqRegionDna;
+import org.ensembl.healthcheck.testcase.eg_core.SeqRegionLength;
+import org.ensembl.healthcheck.testcase.eg_core.SeqRegionsConsistentWithComparaMaster;
+import org.ensembl.healthcheck.testcase.eg_core.TranslationAttribType;
+import org.ensembl.healthcheck.testcase.eg_core.UniProtExternalDbTypes;
+import org.ensembl.healthcheck.testcase.eg_core.ValidSeqEnd;
+import org.ensembl.healthcheck.testcase.eg_core.VersionedGenes;
+import org.ensembl.healthcheck.testcase.eg_core.VersionedTranscripts;
+import org.ensembl.healthcheck.testcase.eg_core.VersionedTranslations;
+import org.ensembl.healthcheck.testcase.eg_core.XrefDescriptionSpecialChars;
+import org.ensembl.healthcheck.testcase.generic.AnalysisDescription;
+import org.ensembl.healthcheck.testcase.generic.AnalysisLogicName;
+import org.ensembl.healthcheck.testcase.generic.AssemblyExceptionTableSeqRegionAttribute;
+import org.ensembl.healthcheck.testcase.generic.AssemblyExceptionTableIntegrity;
+import org.ensembl.healthcheck.testcase.generic.AssemblyExceptionTableSeqMapping;
+import org.ensembl.healthcheck.testcase.generic.AssemblyExceptionTableUniqueRegion;
+import org.ensembl.healthcheck.testcase.generic.AssemblyExceptionTableStartEnd;
+import org.ensembl.healthcheck.testcase.generic.AssemblyMapping;
+import org.ensembl.healthcheck.testcase.generic.AssemblySeqregion;
+import org.ensembl.healthcheck.testcase.generic.BlankCoordSystemVersions;
+import org.ensembl.healthcheck.testcase.generic.BlankEnums;
+import org.ensembl.healthcheck.testcase.generic.BlankInfoType;
+import org.ensembl.healthcheck.testcase.generic.CanonicalTranscriptCoding;
+import org.ensembl.healthcheck.testcase.generic.CoreForeignKeys;
+import org.ensembl.healthcheck.testcase.generic.DescriptionNewlines;
+import org.ensembl.healthcheck.testcase.generic.DisplayLabels;
+import org.ensembl.healthcheck.testcase.generic.DuplicateAssembly;
+import org.ensembl.healthcheck.testcase.generic.ExonRank;
+import org.ensembl.healthcheck.testcase.generic.ExonStrandOrder;
+import org.ensembl.healthcheck.testcase.generic.ExonTranscriptStartEnd;
+import org.ensembl.healthcheck.testcase.generic.GeneCoordSystem;
+import org.ensembl.healthcheck.testcase.generic.GeneDescriptions;
+import org.ensembl.healthcheck.testcase.generic.InterproDescriptions;
+import org.ensembl.healthcheck.testcase.generic.IsCurrent;
+import org.ensembl.healthcheck.testcase.generic.MetaCoord;
+import org.ensembl.healthcheck.testcase.generic.NullTranscripts;
+import org.ensembl.healthcheck.testcase.generic.ProductionAnalysisLogicName;
+import org.ensembl.healthcheck.testcase.generic.ProductionBiotypes;
+import org.ensembl.healthcheck.testcase.generic.ProductionMasterTables;
+import org.ensembl.healthcheck.testcase.generic.SeqRegionAttribsPresent;
+import org.ensembl.healthcheck.testcase.generic.SeqRegionsTopLevel;
+import org.ensembl.healthcheck.testcase.generic.StableID;
+import org.ensembl.healthcheck.testcase.generic.Strand;
+import org.ensembl.healthcheck.testcase.generic.TranscriptsTranslate;
+import org.ensembl.healthcheck.testcase.generic.TranslationStartEnd;
+import org.ensembl.healthcheck.testcase.generic.TranslationStartEndExon;
+import org.ensembl.healthcheck.testcase.generic.XrefCategories;
+import org.ensembl.healthcheck.testcase.generic.XrefHTML;
+import org.ensembl.healthcheck.testcase.generic.XrefIdentifiers;
+import org.ensembl.healthcheck.testcase.generic.XrefTypes;
+
+/**
+ * Group containing tests that should never fail for Ensembl Genomes core
+ * databases
+ * 
+ * @author dstaines
+ *
+ */
+public class EGCoreIntegrity extends GroupOfTests {
+
+	public EGCoreIntegrity() {
+		addTest(AssemblyExceptionTableIntegrity.class,
+			AssemblyExceptionTableSeqMapping.class,
+			AssemblyExceptionTableSeqRegionAttribute.class,
+			AssemblyExceptionTableUniqueRegion.class,
+			AssemblyExceptionTableStartEnd.class,
+			//AssemblyMultipleOverlap.class,
+				AssemblySeqregion.class, CanonicalTranscriptCoding.class,
+				AssemblyDefault.class,
+				CircularAwareFeatureCoords.class,
+				CoreForeignKeys.class, DuplicateAssembly.class,
+				DuplicateTopLevel.class, ExonRank.class,
+				ExonTranscriptStartEnd.class, GeneCoordSystem.class,
+				NullTranscripts.class, PeptideTranslationAttribs.class,
+				ProteinCodingGene.class, SeqRegionAttribsPresent.class,
+				SeqRegionsTopLevel.class, StableID.class, Strand.class,
+				TranscriptsTranslate.class, TranslationStartEnd.class,
+				TranslationStartEndExon.class, ProteinTranslation.class,
+				AssemblyMapping.class, ValidSeqEnd.class,
+				ExonBoundary.class, InappropriateTranslation.class,
+				DescriptionNewlines.class, DisplayLabels.class,
+				GeneDescriptions.class, PositiveCoordinates.class,
+				GeneDescriptionUniProtSource.class, DbDisplayNameUniProt.class,
+				XrefDescriptionSpecialChars.class,
+				BlankCoordSystemVersions.class, BlankEnums.class,
+				BlankInfoType.class, DuplicateXref.class, IgiXref.class,
+				InterproDescriptions.class, IsCurrent.class,
+				XrefCategories.class, XrefHTML.class, XrefIdentifiers.class,
+				XrefTypes.class, UniProtExternalDbTypes.class,
+				SeqRegionDna.class, RequiredEgMeta.class,
+				DeprecatedEgMeta.class, GeneBuildStartDate.class,
+				DuplicateMetaKeys.class, GeneGC.class, MetaCoord.class,
+				SampleSetting.class, AnalysisDescription.class,
+				MetaForCompara.class, TranslationAttribType.class,
+				PermittedEgMeta.class, RepeatAnalysesInMeta.class,
+				ProductionBiotypes.class,
+				ProductionMasterTables.class, AnalysisLogicName.class,
+				SeqRegionsConsistentWithComparaMaster.class,
+				SeqRegionAttribForPolyploidGenome.class,
+				SeqRegionAttribForPolyploidGenomeToplevelOnly.class,
+				SeqRegionLength.class, 
+				EvidenceFreeGO.class, VersionedGenes.class,
+				VersionedTranscripts.class, VersionedTranslations.class,
+				IncorrectExonRank.class);
+	}
+
+}
