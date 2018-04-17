10367353d69d234daa4b9f2f848492d6d31afa8c
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-04-17 11:30:52 +0100

    Updated the funcgen integrity group

--- src/org/ensembl/healthcheck/testgroup/FuncgenIntegrity.java
+++ src/org/ensembl/healthcheck/testgroup/FuncgenIntegrity.java
@@ -7,10 +7,32 @@ public class FuncgenIntegrity extends GroupOfTests {
   public FuncgenIntegrity() {
 
       addTest(
+        org.ensembl.healthcheck.testcase.funcgen.AlignmentHasBamFile.class,
+        org.ensembl.healthcheck.testcase.funcgen.ArraysHaveProbes.class,
         org.ensembl.healthcheck.testcase.funcgen.BrokenFeatureSetToFeatureTypeLinks.class,
+        org.ensembl.healthcheck.testcase.funcgen.CompareFuncgenSchema.class,
+        org.ensembl.healthcheck.testcase.funcgen.CurrentRegulatoryBuildHasEpigenomes.class,
+        org.ensembl.healthcheck.testcase.funcgen.EpigenomeHasSegmentationFile.class,
+        org.ensembl.healthcheck.testcase.funcgen.ExperimentHasFeatureSet.class,
+        org.ensembl.healthcheck.testcase.funcgen.ExternalFeatureFilesExist.class,
+        org.ensembl.healthcheck.testcase.funcgen.FeaturePosition.class,
+        org.ensembl.healthcheck.testcase.funcgen.FuncgenAnalysisDescription.class,
+        org.ensembl.healthcheck.testcase.funcgen.FuncgenForeignKeys.class,
+        org.ensembl.healthcheck.testcase.funcgen.FuncgenStableID.class,
+        org.ensembl.healthcheck.testcase.funcgen.MetaCoord.class,
+        org.ensembl.healthcheck.testcase.funcgen.ProbeIdsUnique.class,
+        org.ensembl.healthcheck.testcase.funcgen.ProbeSetTranscriptMappingsUnique.class,
+        org.ensembl.healthcheck.testcase.funcgen.ProbeTranscriptMappingsUnique.class,
+        org.ensembl.healthcheck.testcase.funcgen.RegulatoryFeatureIsActive.class,
         org.ensembl.healthcheck.testcase.funcgen.SampleRegulatoryFeatureExists.class,
-        org.ensembl.healthcheck.testcase.funcgen.EpigenomeLinkedToEFO.class
-        
+        org.ensembl.healthcheck.testcase.funcgen.SegmentationFileHasBigBed.class,
+        org.ensembl.healthcheck.testcase.funcgen.StableIDsUnique.class,
+        org.ensembl.healthcheck.testcase.generic.ExternalDBDisplayName.class,
+        org.ensembl.healthcheck.testcase.generic.SchemaType.class,
+        org.ensembl.healthcheck.testcase.generic.SpeciesID.class
+        // org.ensembl.healthcheck.testcase.funcgen.DuplicateProbes.class,
+        // org.ensembl.healthcheck.testcase.funcgen.DuplicateProbesFromProbeSets.class,
+        // org.ensembl.healthcheck.testcase.funcgen.EpigenomeLinkedToEFO.class
       );
   }
 }
