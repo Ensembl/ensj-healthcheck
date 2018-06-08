bfdfb56f559c653c84598680f792271b70d1d258
James Allen <jallen@ebi.ac.uk>
2018-06-08 15:04:10 +0100

    Check that homology confidence levels have been set

---
+++ src/org/ensembl/healthcheck/testcase/eg_compara/EGHighConfidence.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * Copyright [2016-2018] EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ensembl.healthcheck.testcase.eg_compara;
+
+import org.ensembl.healthcheck.DatabaseRegistryEntry;
+import org.ensembl.healthcheck.DatabaseType;
+import org.ensembl.healthcheck.ReportManager;
+import org.ensembl.healthcheck.Team;
+import org.ensembl.healthcheck.testcase.AbstractTemplatedTestCase;
+import org.ensembl.healthcheck.util.SqlTemplate;
+
+public class EGHighConfidence extends AbstractTemplatedTestCase {
+
+	public EGHighConfidence() {
+		setTeamResponsible(Team.ENSEMBL_GENOMES);
+		appliesToType(DatabaseType.COMPARA);
+		setDescription("Checks whether HighConfidenceOrthologs pipeline has been run");
+	}
+
+	@Override
+	protected boolean runTest(DatabaseRegistryEntry dbre) {
+		SqlTemplate srv = getSqlTemplate(dbre);
+		boolean result = true;
+		if(srv.queryForDefaultObject("SELECT COUNT(*) FROM homology WHERE is_high_confidence IS NOT NULL", Integer.class)==0) {
+			ReportManager.problem(this, dbre.getConnection(), "No homologies have been annotated with a confidence level");
+			result = false;
+		}
+		return result;
+	}
+
+}
