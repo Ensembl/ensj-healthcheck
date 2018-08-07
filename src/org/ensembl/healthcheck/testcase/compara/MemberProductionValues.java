a10373741b65f53a19afe3715a8009e2bc6c3d26
Mateus Patricio <mateus@ebi.ac.uk>
2018-08-07 16:10:08 +0100

    Addressing PR comments.

--- src/org/ensembl/healthcheck/testcase/compara/MemberProductionValues.java
+++ src/org/ensembl/healthcheck/testcase/compara/MemberProductionValues.java
@@ -5,12 +5,9 @@ import org.ensembl.healthcheck.DatabaseType;
 import org.ensembl.healthcheck.ReportManager;
 import org.ensembl.healthcheck.Team;
 import org.ensembl.healthcheck.testcase.AbstractTemplatedTestCase;
-import org.ensembl.healthcheck.util.SqlTemplate;
 import org.ensembl.healthcheck.util.DBUtils;
 
 public class MemberProductionValues extends AbstractTemplatedTestCase {
-
-	protected int totalCount;
 
 	public MemberProductionValues() {
 		setTeamResponsible(Team.COMPARA);
@@ -20,52 +17,30 @@ public class MemberProductionValues extends AbstractTemplatedTestCase {
 
 	@Override
 	protected boolean runTest(DatabaseRegistryEntry dbre) {
-		SqlTemplate srv = getSqlTemplate(dbre);
 		boolean result = true;
 
-        getTotalCould(dbre);
-		result = computeMissingValues (dbre, totalCount);
+		result = computeMissingValues (dbre);
 
 		return result;
 	}
 
-    //Get count
-	protected void getTotalCould(DatabaseRegistryEntry dbre) {
-		String sql = "SELECT gene_member_id FROM gene_member_hom_stats WHERE collection = 'murinae'";
-		totalCount = DBUtils.getRowCount(dbre.getConnection(), sql);
-	}
-
-	protected boolean computeMissingValues(DatabaseRegistryEntry dbre, int expectedCount) {
+	protected boolean computeMissingValues(DatabaseRegistryEntry dbre) {
 		boolean result = true;
-		String sql = String.format(
-            "SELECT gene_member_id FROM gene_member_hom_stats WHERE "+
-                " gene_trees = 1 AND gene_gain_loss_trees = 0 AND orthologues = 0" + 
-                " AND paralogues = 0 AND homoeologues = 0");
 
-		int missingCount = DBUtils.getRowCount(dbre.getConnection(), sql);
+        //Get the total counts
+		String sql_1 = "SELECT gene_member_id FROM gene_member_hom_stats WHERE collection = 'murinae'";
+		int totalCount = DBUtils.getRowCount(dbre.getConnection(), sql_1);
 
-        // Will fail if more than 1% of values are missing
+        //Get the missing counts
+		String sql_2 = "SELECT gene_member_id FROM gene_member_hom_stats WHERE gene_trees = 1 AND gene_gain_loss_trees = 0 AND orthologues = 0 AND paralogues = 0 AND homoeologues = 0";
+		int missingCount = DBUtils.getRowCount(dbre.getConnection(), sql_2);
+
+
+        // Will fail if more than 0.1% of values are missing
 		if(missingCount > (totalCount*0.001)) {
-			ReportManager.problem(this, dbre.getConnection(), "Too many missing counts (>1%) in gene_member. Wrong homology counts in gene_member_hom_stats.");
+			ReportManager.problem(this, dbre.getConnection(), "Too many missing counts (>0.1%) in gene_member_hom_stats. Wrong homology counts.");
 			result = false;
         }
 		return result;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-//String sql = "SELECT gmhs.gene_member_id, gene_count, root_id, taxon_id FROM gene_member_hom_stats AS gmhs JOIN gene_member USING(gene_member_id) JOIN gene_tree_node ON (seq_member_id=canonical_member_id) JOIN gene_tree_root_attr USING(root_id)";
-//List<String[]> data = DBUtils.getRowValuesList(comparaCon, sql);
-//data = DBUtils.getRowValuesList(comparaCon, sql);
-//for (String[] line : data) {
-//    System.out.println(">>>" + line[0] + "<<<");
-//}
