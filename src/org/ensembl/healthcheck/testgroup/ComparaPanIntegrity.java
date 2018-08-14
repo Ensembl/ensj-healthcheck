bb157b934931d2353f32563dbc1194d5ac2ec132
Thomas Maurel <maurel@ebi.ac.uk>
2018-08-14 15:40:22 +0100

    Updated test to extend from ComparaIntegrity

--- src/org/ensembl/healthcheck/testgroup/ComparaPanIntegrity.java
+++ src/org/ensembl/healthcheck/testgroup/ComparaPanIntegrity.java
@@ -18,72 +18,24 @@
 package org.ensembl.healthcheck.testgroup;
 
 import org.ensembl.healthcheck.GroupOfTests;
-
+import org.ensembl.healthcheck.testcase.compara.CheckSynteny;
+import org.ensembl.healthcheck.testcase.compara.CheckTaxon;
+import org.ensembl.healthcheck.testcase.compara.CheckTopLevelDnaFrag;
+import org.ensembl.healthcheck.testcase.compara.MLSSTagStatsHomology;
+import org.ensembl.healthcheck.testcase.compara.MLSSTagThresholdDs;
+import org.ensembl.healthcheck.testcase.compara.ForeignKeyMasterTables;
 /**
  * Healthchecks for the genomic Compara databases
  * (and only this kind of database)
  */
-public class ComparaPanIntegrity extends GroupOfTests {
+public class ComparaPanIntegrity extends ComparaIntegrity {
 
 	public ComparaPanIntegrity() {
 
+		setDescription("Group of test for the Pan compara database");
 		addTest(
-			org.ensembl.healthcheck.testcase.compara.CheckGenomeDB.class,
-			org.ensembl.healthcheck.testcase.compara.CheckSpeciesSetTable.class,
-			org.ensembl.healthcheck.testcase.compara.CheckSpeciesSetSizeByMethod.class,
-			org.ensembl.healthcheck.testcase.compara.CheckFirstLastRelease.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ForeignKeyPanMasterTables.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagSpeciesNames.class,
-			org.ensembl.healthcheck.testcase.compara.Meta.class,
-			org.ensembl.healthcheck.testcase.compara.MetaSpeciesID.class,
-			org.ensembl.healthcheck.testcase.compara.SingleDBCollations.class,
-			org.ensembl.healthcheck.testcase.compara.CheckConservationScore.class,
-			org.ensembl.healthcheck.testcase.compara.CheckConstrainedElementTable.class,
-			org.ensembl.healthcheck.testcase.compara.CheckGenomicAlignGenomeDBs.class,
-			org.ensembl.healthcheck.testcase.compara.CheckGenomicAlignTreeTable.class,
-			org.ensembl.healthcheck.testcase.compara.CheckMLSSIDConsistencyInGenomicAlign.class,
-			org.ensembl.healthcheck.testcase.compara.CheckPairAlignerUniqueMethod.class,
-			org.ensembl.healthcheck.testcase.compara.CheckMSANames.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyConservationTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyGenomicAlignmentTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyMLSSIdGenomic.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeySyntenyTables.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagGERPMSA.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagHighCoverageMSA.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagMaxAlign.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagStatsMultipleAlignment.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagStatsPairwiseAlignment.class,
-			org.ensembl.healthcheck.testcase.compara.MLSSTagStatsSynteny.class,
-			org.ensembl.healthcheck.testcase.compara.MultipleGenomicAlignBlockIds.class,
-			org.ensembl.healthcheck.testcase.compara.CheckCAFETable.class,
-			org.ensembl.healthcheck.testcase.compara.CheckFlatProteinTrees.class,
-			org.ensembl.healthcheck.testcase.compara.CheckGeneGainLossData.class,
-			org.ensembl.healthcheck.testcase.compara.CheckHomology.class,
-			org.ensembl.healthcheck.testcase.compara.CheckJSONObjects.class,
-			org.ensembl.healthcheck.testcase.compara.CheckSequenceTable.class,
-			org.ensembl.healthcheck.testcase.compara.CheckSpeciesTreeNodeAttr.class,
-			org.ensembl.healthcheck.testcase.compara.CheckComparaStableIDs.class,
-			org.ensembl.healthcheck.testcase.compara.CheckOrthologQCThresholds.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyCAFETables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyFamilyTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyGeneAlignTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyGeneTreeTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyHomologyTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyMemberTables.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeyMLSSIdHomology.class,
-			org.ensembl.healthcheck.testcase.eg_compara.CheckEmptyLeavesTrees.class,
-			org.ensembl.healthcheck.testcase.compara.MemberProductionCounts.class,
-			org.ensembl.healthcheck.testcase.compara.ForeignKeySpeciesTreeTables.class,
-			org.ensembl.healthcheck.testcase.generic.MySQLStorageEngine.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableDnafrag.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableGenomeDb.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableMappingSession.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableMethodLink.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableMethodLinkSpeciesSet.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableNcbiTaxaName.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableNcbiTaxaNode.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableSpeciesSet.class,
-			org.ensembl.healthcheck.testcase.eg_compara.ControlledTableSpeciesSetTag.class
+			org.ensembl.healthcheck.testcase.eg_compara.ForeignKeyPanMasterTables.class
 		);
+		removeTest(CheckSynteny.class, CheckTaxon.class, CheckTopLevelDnaFrag.class, MLSSTagStatsHomology.class, MLSSTagThresholdDs.class, ForeignKeyMasterTables.class);
 	}
 }
