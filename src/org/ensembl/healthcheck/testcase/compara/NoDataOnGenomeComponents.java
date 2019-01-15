242b933d9e7dc49c090c046bb644f414e05eea30
Mateus Patricio <mateuspatricio@users.noreply.github.com>
2019-01-15 17:17:18 +0000

    bugfix: this should not run on master databases

--- src/org/ensembl/healthcheck/testcase/compara/NoDataOnGenomeComponents.java
+++ src/org/ensembl/healthcheck/testcase/compara/NoDataOnGenomeComponents.java
@@ -37,17 +37,23 @@ public class NoDataOnGenomeComponents extends AbstractComparaTestCase {
 
 	public boolean run(DatabaseRegistryEntry dbre) {
 		Connection con = dbre.getConnection();
+		
+		boolean is_master_db = isMasterDB(con);
 
 		boolean result = true;
+		
+		if (!is_master_db) {
 
-		// The only MLSS that is allowed to have component GenomeDBs is protein-trees (401)
-		result &= checkCountIsZero(con, "genome_db JOIN species_set USING (genome_db_id) JOIN method_link_species_set USING (species_set_id)", "genome_component IS NOT NULL AND method_link_id != 401");
+			// The only MLSS that is allowed to have component GenomeDBs is protein-trees (401)
+			result &= checkCountIsZero(con, "genome_db JOIN species_set USING (genome_db_id) JOIN method_link_species_set USING (species_set_id)", "genome_component IS NOT NULL AND method_link_id != 401");
 
-		// All the alignments, syntenies, genes etc should be attached to the principal GenomeDBs
-		String[] tables = {"genomic_align", "dnafrag_region", "constrained_element", "gene_member", "seq_member"};
-		for (String t : tables) {
-			result &= checkCountIsZero(con, "genome_db JOIN dnafrag USING (genome_db_id) JOIN " + t + " USING (dnafrag_id)", "genome_component IS NOT NULL");
+			// All the alignments, syntenies, genes etc should be attached to the principal GenomeDBs
+			String[] tables = {"genomic_align", "dnafrag_region", "constrained_element", "gene_member", "seq_member"};
+			for (String t : tables) {
+				result &= checkCountIsZero(con, "genome_db JOIN dnafrag USING (genome_db_id) JOIN " + t + " USING (dnafrag_id)", "genome_component IS NOT NULL");
+			}
 		}
+		
 		return result;
 	}
 
