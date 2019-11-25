7f37293f5ab1ee67c7d280db4bb6116b153613d6
Matthieu Muffato <muffato@ebi.ac.uk>
2019-11-25 09:30:01 +0000

    Added another FK constraint

--- src/org/ensembl/healthcheck/testcase/compara/ForeignKeyGeneAlignTables.java
+++ src/org/ensembl/healthcheck/testcase/compara/ForeignKeyGeneAlignTables.java
@@ -47,6 +47,7 @@ public class ForeignKeyGeneAlignTables extends AbstractComparaTestCase {
 		result &= checkForOrphans(con, "gene_align_member", "seq_member_id", "seq_member", "seq_member_id");
 		result &= checkOptionalRelation(con, "gene_tree_root", "gene_align_id", "gene_align", "gene_align_id");
 		result &= checkForOrphansWithConstraint(con, "gene_tree_root_attr", "mcoffee_scores_gene_align_id", "gene_align", "gene_align_id", "mcoffee_scores_gene_align_id IS NOT NULL");
+		result &= checkForOrphansWithConstraint(con, "gene_tree_root_tag", "value", "gene_align", "gene_align_id", "tag = 'filtered_gene_align_id'");
 
 		return result;
 	}
